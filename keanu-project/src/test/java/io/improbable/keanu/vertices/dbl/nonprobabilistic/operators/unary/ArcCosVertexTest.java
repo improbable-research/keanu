6b924612d33411c93f22eac747c929004ab74e13
George Nash <georgenash@improbable.io>
2018-04-12 10:40:20 +0100

    Cos and ArcCos

---
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/ArcCosVertexTest.java
@@ -0,0 +1,42 @@
+package io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.unary;
+
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
+import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArcCosVertexTest {
+
+    @Test
+    public void arcCosOpIsCalculatedCorrectly() {
+        ConstantDoubleVertex x = new ConstantDoubleVertex(0.5);
+        ArcCosVertex aCos = new ArcCosVertex(x);
+
+        assertEquals(Math.acos(0.5), aCos.getValue(), 0.00001);
+    }
+
+    @Test
+    public void arcCosOpIsCalculatedCorrectlyWithValue() {
+        ArcCosVertex aCosWithValue = new ArcCosVertex(0.5);
+
+        assertEquals(Math.acos(0.5), aCosWithValue.getValue(), 0.00001);
+    }
+
+    @Test
+    public void arcCosDualNumberIsCalculatedCorrectly() {
+        DoubleVertex uniform = new UniformVertex(0, 10);
+        uniform.setValue(5.0);
+
+        DoubleVertex pow = new PowerVertex(uniform, 3); //dPow = 3 * 5^2
+        ArcCosVertex aCos = new ArcCosVertex(pow);
+
+        double dArcCos = aCos.getDualNumber().getInfinitesimal().getInfinitesimals().get(uniform.getId()); //dArcCos = π / 2 - asin(5^3) * 3 * 5^2
+        double expected = Math.PI / 2 - Math.asin(Math.pow(uniform.getValue(), 3)) * (3 * Math.pow(uniform.getValue(), 3 - 1));
+
+        assertEquals(expected, dArcCos, 0.0001);
+    }
+
+}
