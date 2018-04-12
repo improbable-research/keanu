5232bc5987e1b945b3e23175106475fd0906120c
George Nash <georgenash@improbable.io>
2018-04-12 11:12:34 +0100

    Wire up laplace pdf

--- keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/Laplace.java
+++ keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/Laplace.java
@@ -1,4 +1,36 @@
 package io.improbable.keanu.distributions.continuous;
 
 public class Laplace {
+
+    public static double sample(double mu, double beta) {
+        return 0;
+    }
+
+    public static double pdf(double mu, double beta, double x) {
+        return 0;
+    }
+
+    public static Diff dPdf(double mu, double beta, double x) {
+        return null;
+    }
+
+    public static double logPdf(double mu, double beta, double x) {
+        return 0;
+    }
+
+    public static Diff dlnPdf(double mu, double beta, double x) {
+        return null;
+    }
+
+    public static class Diff {
+        public final double dPdmu;
+        public final double dPdbeta;
+        public final double dPdx;
+
+        public Diff(double dPdmu, double dPdbeta, double dPdx) {
+            this.dPdmu = dPdmu;
+            this.dPdbeta = dPdbeta;
+            this.dPdx = dPdx;
+        }
+    }
 }
--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/LaplaceDistribution.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/LaplaceDistribution.java
@@ -1,5 +1,6 @@
 package io.improbable.keanu.vertices.dbl.probabilistic;
 
+import io.improbable.keanu.distributions.continuous.Laplace;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 
@@ -23,25 +24,27 @@ public class LaplaceDistribution extends ProbabilisticDouble {
 
     @Override
     public double density(Double value) {
-        return 0;
+        return Laplace.pdf(mu.getValue(), beta.getValue(), value);
     }
 
     public double logDensity(Double value) {
-        return 0;
+        return Laplace.logPdf(mu.getValue(), beta.getValue(), value);
     }
 
     @Override
     public Map<String, Double> dDensityAtValue() {
+        Laplace.Diff diff = Laplace.dPdf(mu.getValue(), beta.getValue(), getValue());
         return null;
     }
 
     @Override
     public Map<String, Double> dlnDensityAtValue() {
+        Laplace.Diff diff = Laplace.dlnPdf(mu.getValue(), beta.getValue(), getValue());
         return null;
     }
 
     @Override
     public Double sample() {
-        return null;
+        return Laplace.sample(mu.getValue(), beta.getValue());
     }
 }
