89d4cf3f24fe2837070c02f8a7e3a351d2fcce85
George Nash <georgenash@improbable.io>
2018-04-16 10:41:43 +0100

    Add infer hyper param test to Laplace

--- keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/probabilistic/LaplaceVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/probabilistic/LaplaceVertexTest.java
@@ -1,7 +1,9 @@
 package io.improbable.keanu.vertices.dbl.probabilistic;
 
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,13 @@ public class LaplaceVertexTest {
     private static final double DELTA = 0.0001;
 
     private final Logger log = LoggerFactory.getLogger(LaplaceVertexTest.class);
+
+    private Random random;
+
+    @Before
+    public void setup() {
+        random = new Random(1);
+    }
 
     @Test
     public void laplaceSampleMethodMatchesDensityMethod() {
@@ -112,5 +121,28 @@ public class LaplaceVertexTest {
     public void diffLnDensityIsSameAsLogOfDiffDensity() {
         LaplaceVertex l = new LaplaceVertex(new ConstantDoubleVertex(0.0), new ConstantDoubleVertex(1.0));
         ProbabilisticDoubleContract.diffLnDensityIsSameAsLogOfDiffDensity(l, 0.5, 0.001);
+    }
+
+
+    @Test
+    public void inferHyperParamsFromSamples() {
+
+        double trueMu = 0.0;
+        double trueBeta = 1.0;
+
+        List<DoubleVertex> muBeta = new ArrayList<>();
+        muBeta.add(new ConstantDoubleVertex(trueMu));
+        muBeta.add(new ConstantDoubleVertex(trueBeta));
+
+        List<DoubleVertex> latentMuBeta = new ArrayList<>();
+        latentMuBeta.add(new SmoothUniformVertex(0.01, 10.0));
+        latentMuBeta.add(new SmoothUniformVertex(0.01, 10.0));
+
+        VertexVariationalMAPTest.inferHyperParamsFromSamples(
+                hyperParams -> new LaplaceVertex(hyperParams.get(0), hyperParams.get(1), random),
+                muBeta,
+                latentMuBeta,
+                1000
+        );
     }
 }
