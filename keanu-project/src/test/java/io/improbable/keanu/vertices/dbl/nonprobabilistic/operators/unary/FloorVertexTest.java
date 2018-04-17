fa2a833a4d56544f2759df461818103da69b616a
George Nash <georgenash@improbable.io>
2018-04-17 17:27:04 +0100

    floor and ceil unary op's|

---
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/FloorVertexTest.java
@@ -0,0 +1,38 @@
+package io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.unary;
+
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.binary.PowerVertex;
+import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FloorVertexTest {
+
+    @Test
+    public void canComputeFloorOperation() {
+        DoubleVertex A = new ConstantDoubleVertex(8.12);
+        FloorVertex floor = new FloorVertex(A);
+
+        assertEquals(Math.floor(8.12), floor.getValue(), 0.0001);
+    }
+
+    @Test
+    public void canComputeFloorOperationWithValue() {
+        FloorVertex floor = new FloorVertex(8.12);
+
+        assertEquals(Math.floor(8.12), floor.getValue(), 0.0001);
+    }
+
+    @Test
+    public void floorVertexDoesNotChangeDerivativeCalculations() {
+        DoubleVertex A = new UniformVertex(0.0, 10.0);
+        A.setValue(5.2);
+        FloorVertex floorVertex = new FloorVertex(A);
+        PowerVertex powerVertex = new PowerVertex(floorVertex, 2.0);
+
+        assertEquals(2 * Math.pow(5.0, 2 - 1), powerVertex.getDualNumber().getInfinitesimal().getInfinitesimals().get(A.getId()), 0.000);
+    }
+
+}
