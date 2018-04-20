6203c226a0895c41908bf04d863780b836f191d6
Caleb <caleb@improbable.io>
2018-04-20 18:44:17 +0100

    add failing propagating through probabilistic test

--- keanu-project/src/test/java/io/improbable/keanu/vertices/SetAndCascadeTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/SetAndCascadeTest.java
@@ -17,37 +17,72 @@ public class SetAndCascadeTest {
     public void doesNotDoUnnecessaryOperations() {
 
         Random random = new Random(1);
+        AtomicInteger n = new AtomicInteger(0);
         DoubleVertex start = new GaussianVertex(0, 1, random);
 
-        DoubleVertex end = start;
-
-        AtomicInteger n = new AtomicInteger(0);
-
         int links = 20;
-        for (int i = 0; i < links; i++) {
-
-            DoubleVertex left = new DoubleUnaryOpLambda<>(end, (a) -> {
-                n.incrementAndGet();
-                return a;
-            });
-
-            DoubleVertex right = new DoubleUnaryOpLambda<>(end, (a) -> {
-                n.incrementAndGet();
-                return a;
-            });
-
-            end = new DoubleBinaryOpLambda<>(left, right, (a, b) -> {
-                n.incrementAndGet();
-                return a + b;
-            });
-        }
+        DoubleVertex end = addLinks(start, n, links);
 
         start.setAndCascade(2.0);
 
         //Calculates the correct answer
-        assertEquals(end.getValue(), Math.pow(2, links + 1), 0.0);
+        assertEquals(Math.pow(2, links + 1), end.getValue(), 0.0);
 
         //Does the right amount of work
         assertEquals(3 * links, n.get());
+    }
+
+    @Test
+    public void doesNotPropagateThroughProbabilisticVertices() {
+        Random random = new Random(1);
+        AtomicInteger n = new AtomicInteger(0);
+        DoubleVertex start = new GaussianVertex(0, 1, random);
+
+        DoubleVertex end = addLinks(start, n, 1);
+
+        start.setAndCascade(2.0);
+
+        System.out.println("");
+        System.out.println("");
+        DoubleVertex nextLayerStart = new GaussianVertex(end, 1, random);
+
+        DoubleVertex secondLayerEnd = addLinks(nextLayerStart, n, 1);
+
+        start.setAndCascade(3.0);
+
+        assertEquals(6.0, end.getValue(), 0.0);
+        assertEquals(3, n.get());
+    }
+
+    private DoubleVertex addLinks(DoubleVertex end, AtomicInteger n, int links) {
+
+
+        long id;
+
+        for (int i = 0; i < links; i++) {
+
+            final long leftId = Vertex.idGenerator.get();
+            DoubleVertex left = new DoubleUnaryOpLambda<>(end, (a) -> {
+                n.incrementAndGet();
+                System.out.println("left " + leftId + " " + n.get());
+                return a;
+            });
+
+            final long rightId = Vertex.idGenerator.get();
+            DoubleVertex right = new DoubleUnaryOpLambda<>(end, (a) -> {
+                n.incrementAndGet();
+                System.out.println("right " + rightId + " " + n.get());
+                return a;
+            });
+
+            final long centerId = Vertex.idGenerator.get();
+            end = new DoubleBinaryOpLambda<>(left, right, (a, b) -> {
+                n.incrementAndGet();
+                System.out.println("center " + centerId + " " + n.get());
+                return a + b;
+            });
+        }
+
+        return end;
     }
 }
