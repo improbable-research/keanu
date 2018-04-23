b8a5d5c9f967a01918ef194db8124f6fa4bd1b21
Justin Taylor <justintaylor@improbable.io>
2018-04-23 13:45:02 -0400

    Bug dropping var t and using var random for x in student t's f(x)

--- keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/StudentT.java
+++ keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/StudentT.java
@@ -12,18 +12,34 @@ public class StudentT {
 	 * ARL-TR-2168 March 2000
 	 * 5.1.23 page 36
 	 */
-	public static double sample(int t, Random random) {
-		assert( t >= 1 );
-		return Guassian.sample( 0., 1., random) / sqrt( ChiSquared.sample(t, random) / t );
+	public static double sample(int v, Random random) {
+		assert( v > 0 );
+		return Guassian.sample( 0., 1., random) / sqrt( ChiSquared.sample(v, random) / v );
 		
 	}
 	
-	public static double pdf(int t, int v) {
+	public static double sample(int v, double mu, double sigma, Random random) {
+		assert( v > 0 );
+		return Guassian.sample( mu, sigma, random) / sqrt( ChiSquared.sample(v, random) / v );
+		
+	}
+	
+	public static double pdf(int v, Random random) {
 		final double halfVPlusOne = ((double) v + 1.) / 2.;
 		final double halfV = (double) v / 2.;
 		final double numerator = gamma(halfVPlusOne);
 		final double denominator = sqrt((double) v * PI) * gamma(halfV);
-		final double multiplier = pow(1. + pow((double) t, 2), -halfVPlusOne);
+		final double multiplier = pow(1. + pow((double) random, 2), -halfVPlusOne);
+		
+		return (numerator / denominator) * multiplier;
+	}
+	
+	public static double pdf(int v, double mu, double sigma, Random random) {
+		final double halfVPlusOne = ((double) v + 1.) / 2.;
+		final double halfV = (double) v / 2.;
+		final double numerator = gamma(halfVPlusOne);
+		final double denominator = sqrt((double) v * PI) * gamma(halfV);
+		final double multiplier = pow(1. + pow((double) random, 2), -halfVPlusOne);
 		
 		return (numerator / denominator) * multiplier;
 	}
--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/StudentTVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/StudentTVertex.java
@@ -9,32 +9,31 @@ import java.util.Random;
 
 public class StudentTVertex extends ProbabilisticDouble {
 	
-	private final DoubleVertex t;
 	private final DoubleVertex v;
 	private final Random random;
 	
-	public StudentTVertex(DoubleVertex t, DoubleVertex v, Random random) {
-		this.t = t;
+	public StudentTVertex(DoubleVertex v, Random random) {
 		this.v = v;
 		this.random = random;
 		setValue(sample());
-		setParents(t, v);
+		setParents(v);
 	}
 	
-	public StudentTVertex(DoubleVertex t, DoubleVertex v) { this(t, v, new Random()); }
+	public StudentTVertex(DoubleVertex v) { this(v, new Random()); }
 
-	public StudentTVertex(int t, int v, Random random) {
-		this(new ConstantDoubleVertex(t), new ConstantDoubleVertex(v), random);
+	public StudentTVertex(int v, Random random) {
+		this(new ConstantDoubleVertex(v), random);
 	}
 
-	public StudentTVertex(int t, int v) {
-		this(new ConstantDoubleVertex(t), new ConstantDoubleVertex(v), new Random());
+	public StudentTVertex(int v) {
+		this(new ConstantDoubleVertex(v), new Random());
 	}
-	
-	public DoubleVertex getT() { return t; }
 	
 	public DoubleVertex getV() { return v; }
 	
 	@Override
-	public double density(Double value) { return StudentT.pdf(t.getValue(), v.getValue(), value); }
+	public double density(Double value) { return StudentT.pdf(v.getValue(), value); }
+	
+	@Override
+	public Double sample() { return StudentT.sample(v.getValue(), random); }
 }
