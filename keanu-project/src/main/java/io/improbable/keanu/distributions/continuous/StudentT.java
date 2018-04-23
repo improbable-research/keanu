ddaad8f725f52d48b6f31f4bc5272be808e1346d
Justin Taylor <justintaylor@improbable.io>
2018-04-23 15:29:54 -0400

    Add mu and sigma, undo last commit

--- keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/StudentT.java
+++ keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/StudentT.java
@@ -12,35 +12,29 @@ public class StudentT {
 	 * ARL-TR-2168 March 2000
 	 * 5.1.23 page 36
 	 */
-	public static double sample(int v, Random random) {
-		assert( v > 0 );
+	public static double sample(double v, Random random) {
+		assert( v > 0. );
 		return Guassian.sample( 0., 1., random) / sqrt( ChiSquared.sample(v, random) / v );
 		
 	}
 	
-	public static double sample(int v, double mu, double sigma, Random random) {
-		assert( v > 0 );
+	public static double sample(double v, double mu, double sigma, Random random) {
+		assert( v > 0. );
 		return Guassian.sample( mu, sigma, random) / sqrt( ChiSquared.sample(v, random) / v );
 		
 	}
 	
-	public static double pdf(int v, Random random) {
-		final double halfVPlusOne = ((double) v + 1.) / 2.;
-		final double halfV = (double) v / 2.;
+	public static double pdf(double v, double x) {
+		final double halfVPlusOne = (v + 1.) / 2.;
+		final double halfV = v / 2.;
 		final double numerator = gamma(halfVPlusOne);
-		final double denominator = sqrt((double) v * PI) * gamma(halfV);
-		final double multiplier = pow(1. + pow((double) random, 2), -halfVPlusOne);
+		final double denominator = sqrt(v * PI) * gamma(halfV);
+		final double multiplier = pow(1. + pow((x, 2), -halfVPlusOne);
 		
 		return (numerator / denominator) * multiplier;
 	}
 	
-	public static double pdf(int v, double mu, double sigma, Random random) {
-		final double halfVPlusOne = ((double) v + 1.) / 2.;
-		final double halfV = (double) v / 2.;
-		final double numerator = gamma(halfVPlusOne);
-		final double denominator = sqrt((double) v * PI) * gamma(halfV);
-		final double multiplier = pow(1. + pow((double) random, 2), -halfVPlusOne);
-		
-		return (numerator / denominator) * multiplier;
+	public static double pdf(double v, double mu, double sigma, double x) {
+		return 0.;
 	}
 }
--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/StudentTVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/StudentTVertex.java
@@ -10,30 +10,52 @@ import java.util.Random;
 public class StudentTVertex extends ProbabilisticDouble {
 	
 	private final DoubleVertex v;
+	private final DoubleVertex mu;
+	private final DoubleVertex sigma;
 	private final Random random;
 	
-	public StudentTVertex(DoubleVertex v, Random random) {
+	public StudentTVertex(DoubleVertex v, DoubleVertex mu, DoubleVertex sigma, Random random) {
 		this.v = v;
+		this.mu = mu;
+		this.sigma = sigma;
 		this.random = random;
 		setValue(sample());
-		setParents(v);
+		setParents(v, mu, sigma);
+	}
+	
+	public StudentTVertex(DoubleVertex v, Random random) {
+		this(v, 0., 1., random);
 	}
 	
 	public StudentTVertex(DoubleVertex v) { this(v, new Random()); }
-
-	public StudentTVertex(int v, Random random) {
+	
+	public StudentTVertex(DoubleVertex v, DoubleVertex mu, DoubleVertex sigma) { this(v, mu, sigma, new Random()); }
+	
+	public StudentTVertex(double v, Random random) {
 		this(new ConstantDoubleVertex(v), random);
 	}
-
-	public StudentTVertex(int v) {
+	
+	public StudentTVertex(double v) {
 		this(new ConstantDoubleVertex(v), new Random());
+	}
+	
+	public StudentTVertex(double v, double mu, double sigma, Random random) {
+		this(new ConstantDoubleVertex(v), new ConstantDoubleVertex(mu), new ConstantDoubleVertex(sigma), random);
+	}
+	
+	public StudentTVertex(double vdouble mu, double sigma, ) {
+		this(new ConstantDoubleVertex(v), new ConstantDoubleVertex(mu), new ConstantDoubleVertex(sigma), new Random());
 	}
 	
 	public DoubleVertex getV() { return v; }
 	
-	@Override
-	public double density(Double value) { return StudentT.pdf(v.getValue(), value); }
+	public DoubleVertex getMu() { return mu; }
+	
+	public DoubleVertex getSigma() { return sigma; }
 	
 	@Override
-	public Double sample() { return StudentT.sample(v.getValue(), random); }
+	public double density(Double value) { return StudentT.pdf(v.getValue(), mu.getValue(), sigma.getValue(), value); }
+	
+	@Override
+	public Double sample() { return StudentT.sample(v.getValue(), mu.getValue(), sigma.getValue(), random); }
 }
