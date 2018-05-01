c7fe00075b315e958ca93c211ba08e790f25666f
George Nash <georgenash@improbable.io>
2018-05-01 11:25:59 +0100

    Sonar issues

--- keanu-project/src/main/java/io/improbable/keanu/util/csv/pojo/PublicFieldMatcher.java
+++ keanu-project/src/main/java/io/improbable/keanu/util/csv/pojo/PublicFieldMatcher.java
@@ -1,8 +1,5 @@
 package io.improbable.keanu.util.csv.pojo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Optional;
