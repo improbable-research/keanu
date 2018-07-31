702703ecb77568f35e557b8ac56f854898a5e51a
George Nash <georgenash@improbable.io>
2018-07-31 11:47:43 +0100

    logprob and dLogProb

--- keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/Dirichlet.java
+++ keanu-project/src/main/java/io/improbable/keanu/distributions/continuous/Dirichlet.java
@@ -5,35 +5,48 @@ import io.improbable.keanu.distributions.dual.Diffs;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
 
+import static io.improbable.keanu.distributions.dual.Diffs.C;
+import static io.improbable.keanu.distributions.dual.Diffs.X;
+
 public class Dirichlet implements ContinuousDistribution {
 
     private final DoubleTensor concentration;
-    private final int categories;
 
     public static ContinuousDistribution withParameters(DoubleTensor concentration) {
-        return new Dirichlet(concentration, (int) concentration.getLength());
+        return new Dirichlet(concentration);
     }
 
-    private Dirichlet(DoubleTensor concentration, int categories) {
+    private Dirichlet(DoubleTensor concentration) {
         this.concentration = concentration;
-        this.categories = categories;
     }
 
     @Override
     public DoubleTensor sample(int[] shape, KeanuRandom random) {
-        ContinuousDistribution gamma = Gamma.withParameters(concentration, DoubleTensor.ones(concentration.getShape()), DoubleTensor.zeros(concentration.getShape()));
-        DoubleTensor gammaSamples = gamma.sample(concentration.getShape(), random);
+       final ContinuousDistribution gamma = Gamma.withParameters(
+            concentration,
+            DoubleTensor.ones(shape),
+            DoubleTensor.zeros(shape)
+        );
+        final DoubleTensor gammaSamples = gamma.sample(concentration.getShape(), random);
         return normalise(gammaSamples);
     }
 
     @Override
-    public Diffs dLogProb(DoubleTensor x) {
-        return null;
+    public DoubleTensor logProb(DoubleTensor x) {
+        final DoubleTensor sumConcentrationLogged = x.log().times(concentration.minus(1.).sum());
+        final double sumLogGammaConcentration = concentration.apply(org.apache.commons.math3.special.Gamma::gamma).log().sum();
+        final double logGammaSumConcentration = Math.log(org.apache.commons.math3.special.Gamma.gamma(concentration.sum()));
+        return sumConcentrationLogged.minus(sumLogGammaConcentration).plus(logGammaSumConcentration);
     }
 
     @Override
-    public DoubleTensor logProb(DoubleTensor x) {
-        return null;
+    public Diffs dLogProb(DoubleTensor x) {
+        final DoubleTensor dLogPdc = x.log().minus(concentration.apply(org.apache.commons.math3.special.Gamma::digamma)).plus(org.apache.commons.math3.special.Gamma.digamma(concentration.sum()));
+        final DoubleTensor dLogPdx = concentration.minus(1).div(x);
+
+        return new Diffs()
+            .put(C, dLogPdc)
+            .put(X, dLogPdx);
     }
 
     private DoubleTensor normalise(DoubleTensor gammaSamples) {
--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/DirichletVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/DirichletVertex.java
@@ -4,20 +4,17 @@ import io.improbable.keanu.distributions.continuous.Dirichlet;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
-import io.improbable.keanu.vertices.intgr.IntegerVertex;
 
 import java.util.Map;
 
 public class DirichletVertex extends ProbabilisticDouble {
 
     private final DoubleVertex concentration;
-    private final int categories;
 
     public DirichletVertex(int[] tensorShape, DoubleVertex concentration) {
         //check shape
 
         this.concentration = concentration;
-        this.categories = (int) concentration.getValue().getLength();
         setParents(concentration);
         setValue(DoubleTensor.placeHolder(tensorShape));
     }
