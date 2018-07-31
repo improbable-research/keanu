98f496a1dd08dc0e4c85cb7c496418455af8922a
George Nash <georgenash@improbable.io>
2018-07-31 11:55:47 +0100

    Dirichlet sample test

--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/DirichletVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/probabilistic/DirichletVertex.java
@@ -1,36 +1,69 @@
 package io.improbable.keanu.vertices.dbl.probabilistic;
 
 import io.improbable.keanu.distributions.continuous.Dirichlet;
+import io.improbable.keanu.distributions.dual.Diffs;
+import io.improbable.keanu.tensor.TensorShape;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.diff.PartialDerivatives;
 
 import java.util.Map;
+
+import static io.improbable.keanu.distributions.dual.Diffs.C;
+import static io.improbable.keanu.distributions.dual.Diffs.X;
+import static io.improbable.keanu.tensor.TensorShape.shapeToDesiredRankByPrependingOnes;
 
 public class DirichletVertex extends ProbabilisticDouble {
 
     private final DoubleVertex concentration;
 
     public DirichletVertex(int[] tensorShape, DoubleVertex concentration) {
-        //check shape
-
         this.concentration = concentration;
         setParents(concentration);
         setValue(DoubleTensor.placeHolder(tensorShape));
     }
 
+    public DirichletVertex(DoubleVertex concentration) {
+        this(concentration.getShape(), concentration);
+    }
+
+    public DirichletVertex(double concentration) {
+        this(new ConstantDoubleVertex(concentration));
+    }
+
     @Override
     public double logPdf(DoubleTensor value) {
-        return 0;
+        DoubleTensor concentrationValues = concentration.getValue();
+        DoubleTensor logPdfs = Dirichlet.withParameters(concentrationValues).logProb(value);
+        return logPdfs.sum();
     }
 
     @Override
     public Map<Long, DoubleTensor> dLogPdf(DoubleTensor value) {
-        return null;
+        Diffs dlnP = Dirichlet.withParameters(concentration.getValue()).dLogProb(value);
+        return convertDualNumbersToDiff(dlnP.get(C).getValue(), dlnP.get(X).getValue());
     }
 
     @Override
     public DoubleTensor sample(KeanuRandom random) {
         return Dirichlet.withParameters(concentration.getValue()).sample(getShape(), random);
+    }
+
+    private Map<Long, DoubleTensor> convertDualNumbersToDiff(DoubleTensor dLogPdc,
+                                                             DoubleTensor dLogPdx) {
+
+        PartialDerivatives dLogPdInputs = concentration.getDualNumber().getPartialDerivatives().multiplyBy(dLogPdc);
+
+        if (!this.isObserved()) {
+            dLogPdInputs.putWithRespectTo(getId(), dLogPdx.reshape(
+                shapeToDesiredRankByPrependingOnes(dLogPdx.getShape(), dLogPdx.getRank() + getValue().getRank()))
+            );
+        }
+
+        PartialDerivatives summed = dLogPdInputs.sum(true, TensorShape.dimensionRange(0, getShape().length));
+
+        return summed.asMap();
     }
 }
---
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/probabilistic/DirichletVertexTest.java
@@ -0,0 +1,21 @@
+package io.improbable.keanu.vertices.dbl.probabilistic;
+
+import io.improbable.keanu.vertices.dbl.KeanuRandom;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DirichletVertexTest {
+
+    private KeanuRandom random;
+
+    @Before
+    public void setup() {
+        random = new KeanuRandom(1);
+    }
+
+    @Test
+    public void something() {
+        
+    }
+
+}
