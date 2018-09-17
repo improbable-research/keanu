2f07a41befdce26d4a07814090069fd98e9a2215
George Nash <georgenash@improbable.io>
2018-09-17 17:42:02 +0100

    Add deleted java docs

--- keanu-project/src/main/java/io/improbable/keanu/vertices/model/ProcessModelVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/model/ProcessModelVertex.java
@@ -9,6 +9,13 @@ import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+/**
+ * A vertex whose operation is the execution of a command line process.
+ *
+ * It is able to execute this process and parse the result.
+ *
+ * It stores multiple output values and a model result vertex is required to extract a specific value.
+ */
 public class ProcessModelVertex {
 
     public static LambdaModelVertex create(Map<VertexLabel, Vertex<? extends Tensor>> inputs,
