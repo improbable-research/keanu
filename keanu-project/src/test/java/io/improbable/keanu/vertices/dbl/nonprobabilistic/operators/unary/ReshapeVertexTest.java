437e34dcf7949735c310dba24aca64841b46ee76
Chris North <christophernorth@improbable.io>
2018-09-19 14:06:04 +0100

    Updated Reshape Vertex to stretch its legs with higher dimensions (and changing dimensions) Fixed Reshape Vertex so reverse diff works correctly

--- keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/ReshapeVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/ReshapeVertexTest.java
@@ -104,21 +104,21 @@ public class ReshapeVertexTest {
 
     @Test
     public void partialCorrectlyFlowsThroughTwoReshapes() {
-        DoubleVertex A = new UniformVertex(0, 10);
-        A.setValue(DoubleTensor.create(new double[]{1, 2, 3, 4}, 2, 2));
+        DoubleVertex A = new UniformVertex(new int[]{2, 2, 2, 2}, 0, 10);
+        A.setValue(A.sample());
 
-        DoubleVertex B = new UniformVertex(0, 10);
-        B.setValue(DoubleTensor.create(new double[]{1, 2, 3, 4}, 2, 2));
+        DoubleVertex B = new UniformVertex(new int[]{2, 2, 2, 2}, 0, 10);
+        B.setValue(B.sample());
 
         DoubleVertex C = A.plus(B);
 
-        DoubleVertex D = C.reshape(4, 1);
-        DoubleVertex E = D.reshape(1, 4);
+        DoubleVertex D = C.reshape(4, 2, 2);
+        DoubleVertex E = D.reshape(4, 4);
 
         PartialDerivatives forward = E.getDualNumber().getPartialDerivatives();
         PartialDerivatives backward = Differentiator.reverseModeAutoDiff(E, ImmutableSet.of(A, B));
 
-        Assert.assertArrayEquals(new int[]{1, 4, 2, 2}, forward.withRespectTo(A).getShape());
+        Assert.assertArrayEquals(new int[]{4, 4, 2, 2, 2, 2}, forward.withRespectTo(A).getShape());
         Assert.assertArrayEquals(forward.withRespectTo(A).asFlatDoubleArray(), backward.withRespectTo(A).asFlatDoubleArray(), 1e-6);
     }
 
