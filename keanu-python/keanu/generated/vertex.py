ea5f2966cfb4ebc9fd4c9cc5b29c5d0b332843b5
Joanne Hong <joannehong@improbable.io>
2018-09-28 15:44:05 +0100

    Enforce ordering of vertices in generated code

--- keanu-python/keanu/generated/vertex.py
+++ keanu-python/keanu/generated/vertex.py
@@ -6,34 +6,10 @@ from keanu.base import KeanuContext, Vertex
 k = KeanuContext().jvm_view()
 
 
-java_import(k, "io.improbable.keanu.vertices.dbl.nonprobabilistic.DoubleIfVertex")
+java_import(k, "io.improbable.keanu.vertices.bool.nonprobabilistic.operators.binary.compare.GreaterThanVertex")
 
-def DoubleIf(*args) -> k.DoubleIfVertex:
-    return Vertex(k.DoubleIfVertex, args)
-
-
-java_import(k, "io.improbable.keanu.vertices.intgr.probabilistic.PoissonVertex")
-
-def Poisson(*args) -> k.PoissonVertex:
-    return Vertex(k.PoissonVertex, args)
-
-
-java_import(k, "io.improbable.keanu.vertices.dbl.probabilistic.GammaVertex")
-
-def Gamma(*args) -> k.GammaVertex:
-    return Vertex(k.GammaVertex, args)
-
-
-java_import(k, "io.improbable.keanu.vertices.dbl.probabilistic.ExponentialVertex")
-
-def Exponential(*args) -> k.ExponentialVertex:
-    return Vertex(k.ExponentialVertex, args)
-
-
-java_import(k, "io.improbable.keanu.vertices.intgr.probabilistic.UniformIntVertex")
-
-def UniformInt(*args) -> k.UniformIntVertex:
-    return Vertex(k.UniformIntVertex, args)
+def GreaterThan(*args) -> k.GreaterThanVertex:
+    return Vertex(k.GreaterThanVertex, args)
 
 
 java_import(k, "io.improbable.keanu.vertices.dbl.nonprobabilistic.CastDoubleVertex")
@@ -42,13 +18,37 @@ def CastDouble(*args) -> k.CastDoubleVertex:
     return Vertex(k.CastDoubleVertex, args)
 
 
+java_import(k, "io.improbable.keanu.vertices.dbl.nonprobabilistic.DoubleIfVertex")
+
+def DoubleIf(*args) -> k.DoubleIfVertex:
+    return Vertex(k.DoubleIfVertex, args)
+
+
 java_import(k, "io.improbable.keanu.vertices.dbl.probabilistic.CauchyVertex")
 
 def Cauchy(*args) -> k.CauchyVertex:
     return Vertex(k.CauchyVertex, args)
 
 
-java_import(k, "io.improbable.keanu.vertices.bool.nonprobabilistic.operators.binary.compare.GreaterThanVertex")
+java_import(k, "io.improbable.keanu.vertices.dbl.probabilistic.ExponentialVertex")
 
-def GreaterThan(*args) -> k.GreaterThanVertex:
-    return Vertex(k.GreaterThanVertex, args)
+def Exponential(*args) -> k.ExponentialVertex:
+    return Vertex(k.ExponentialVertex, args)
+
+
+java_import(k, "io.improbable.keanu.vertices.dbl.probabilistic.GammaVertex")
+
+def Gamma(*args) -> k.GammaVertex:
+    return Vertex(k.GammaVertex, args)
+
+
+java_import(k, "io.improbable.keanu.vertices.intgr.probabilistic.PoissonVertex")
+
+def Poisson(*args) -> k.PoissonVertex:
+    return Vertex(k.PoissonVertex, args)
+
+
+java_import(k, "io.improbable.keanu.vertices.intgr.probabilistic.UniformIntVertex")
+
+def UniformInt(*args) -> k.UniformIntVertex:
+    return Vertex(k.UniformIntVertex, args)
