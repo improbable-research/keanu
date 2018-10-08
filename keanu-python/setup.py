cb7a1b2ff92fab1f899e64fade6ac39c365bb5ba
Joanne Hong <joannehong@improbable.io>
2018-10-08 20:51:05 +0100

    Add keanu eng email

--- keanu-python/setup.py
+++ keanu-python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='keanu',
       version='0.1',
       author='Improbable Worlds',
-      author_email='UNKNOWN',
+      author_email='keanu-engineering@improbable.io',
       packages=['keanu', 'keanu.generated'],
       install_requires=[
           'py4j',
