7178cabad91731f5dc947e8264ba606439bcbb05
Joanne Hong <joannehong@improbable.io>
2018-10-09 17:33:22 +0100

    Add comment for python singleton

--- keanu-python/keanu/context.py
+++ keanu-python/keanu/context.py
@@ -6,7 +6,7 @@ from py4j.java_gateway import JavaGateway, JavaObject, CallbackServerParameters
 PATH = os.path.abspath(os.path.dirname(__file__))
 CLASSPATH = os.path.join(PATH, "keanu-python-all.jar")
 
-
+# python singleton implementation https://stackoverflow.com/a/6798042/741789
 class Singleton(type):
     _instances = {}
 
