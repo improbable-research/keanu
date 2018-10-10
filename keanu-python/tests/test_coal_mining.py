879298a8d09a930d14a9f0c824d270de4868db14
Michael Lewin <michaellewin@improbable.io>
2018-10-10 14:26:55 +0100

    putting code back into test_coal_mining until it breaks

--- keanu-python/tests/test_coal_mining.py
+++ keanu-python/tests/test_coal_mining.py
@@ -12,8 +12,8 @@ def test_coalmining():
     data = pd.read_csv(FILE, names=["year", "count"]).set_index("year")
     start_year, end_year = (data.index.min(), data.index.max())
 
-    # with kn.Model() as m:
-    #     m.switchpoint = kn.UniformInt(int(start_year), int(end_year + 1))
+    with kn.Model() as m:
+        m.switchpoint = kn.UniformInt(int(start_year), int(end_year + 1))
 
     #     m.early_rate = kn.Exponential(1.0)
     #     m.late_rate = kn.Exponential(1.0)
