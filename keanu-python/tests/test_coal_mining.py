5a552e83c56f24b23f2331cb2eb3d68cacaef5e7
Michael Lewin <michaellewin@improbable.io>
2018-10-10 11:57:48 +0100

    added simple temp tests to debug AppVeyor hanging

--- keanu-python/tests/test_coal_mining.py
+++ keanu-python/tests/test_coal_mining.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import keanu as kn
 import numpy as np
+def test_temp_pass():
+    assert True
+
+def test_temp_fail():
+    assert False
 
 def test_coalmining():
     FILE = "data/coal-mining-disaster-data.csv"
