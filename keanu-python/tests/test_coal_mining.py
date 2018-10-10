f9da994acb115c6a33a75f508e02686e5d80f359
Michael Lewin <michaellewin@improbable.io>
2018-10-10 15:07:47 +0100

    add the DoubleIf back into test

--- keanu-python/tests/test_coal_mining.py
+++ keanu-python/tests/test_coal_mining.py
@@ -19,7 +19,7 @@ def test_coalmining():
         m.late_rate = kn.Exponential(1.0)
 
         m.years = np.array(data.index)
-    #     m.rates = kn.DoubleIf([1, 1], m.switchpoint > m.years, m.early_rate, m.late_rate)
+        m.rates = kn.DoubleIf([1, 1], m.switchpoint > m.years, m.early_rate, m.late_rate)
     #     m.disasters = kn.Poisson(m.rates)
 
     # m.disasters.observe(data.values)
