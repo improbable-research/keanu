226f7efe3906e31d36d5530cfafba55febbd8322
Michael Lewin <michaellewin@improbable.io>
2018-10-11 16:32:27 +0100

    added logging to test_appveyor_hanging

--- keanu-python/tests/test_appveyor_hanging.py
+++ keanu-python/tests/test_appveyor_hanging.py
@@ -2,7 +2,15 @@ import keanu as kn
 import numpy as np
 
 
-def test_appveyor_freezing():
+def test_appveyor_hanging():
+    print("test_appveyor_hanging")
     values = np.array([[1., 1.]])
+    print("values", values)
     const = kn.Const(values)
+    print("const vertex shape", [i for i in const.getShape()])
+    print("const vertex value", [i for i in const.getValue().asFlatArray()])
     exponential = kn.Exponential(const)
+
+
+if __name__ == "__main__":
+    test_appveyor_hanging()
