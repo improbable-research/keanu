a959a69710a6467451229be0a23fdf9cf830fda1
Michael Lewin <michaellewin@improbable.io>
2018-10-12 15:16:49 +0100

    add logging and stdout flushes to test

--- keanu-python/tests/test_appveyor_hanging.py
+++ keanu-python/tests/test_appveyor_hanging.py
@@ -1,14 +1,23 @@
 import keanu as kn
 import numpy as np
+import sys
+import logging
 
 
 def test_appveyor_hanging():
     print("test_appveyor_hanging")
+    logging.warn("test_appveyor_hanging")
+    sys.stdout.flush()
     values = np.array([[1., 1.]])
     print("values", values)
+    logging.warn("values %s" % values)
+    sys.stdout.flush()
     const = kn.Const(values)
     print("const vertex shape", [i for i in const.getShape()])
+    logging.warn("const vertex shape %s" % [i for i in const.getShape()])
     print("const vertex value", [i for i in const.getValue().asFlatArray()])
+    logging.warn("const vertex value %s" % [i for i in const.getValue().asFlatArray()])
+    sys.stdout.flush()
     exponential = kn.Exponential(const)
 
 
