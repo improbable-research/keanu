7659366cb1d04f8ad56c83faafd0cb1c2aa6ed47
George Nash <georgenash@improbable.io>
2018-10-15 17:07:42 +0100

    Fix bash script syntax

--- deployment/buildAndDeploy.sh
+++ deployment/buildAndDeploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'feature/travisRelease' ]]
+if [[ $TRAVIS_BRANCH == 'feature/travisRelease' ]]; then
   ./gradlew uploadArchives -PnexusUser=$NEXUS_USER -PnexusPassword=$NEXUS_PASSWORD -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=../deployment/secret-keys-keanu.gpg
 else
   ./gradlew check
 fi
 
-if [[ ! -z $SONAR_TOKEN ]]; then sonar-scanner; fi
+if [[ ! -z $SONAR_TOKEN ]]; then
+  sonar-scanner
+fi
