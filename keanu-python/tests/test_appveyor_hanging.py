bba84d0d27a0a37bef5a48aacd91cd09138e1a61
Michael Lewin <michaellewin@improbable.io>
2018-10-16 12:02:12 +0100

    fixed DeprecationWarning: The 'warn' function is deprecated, use 'warning' instead

--- keanu-python/tests/test_appveyor_hanging.py
+++ keanu-python/tests/test_appveyor_hanging.py
@@ -6,17 +6,17 @@ import logging
 
 def test_appveyor_hanging():
     print("test_appveyor_hanging")
-    logging.warn("test_appveyor_hanging")
+    logging.warning("test_appveyor_hanging")
     sys.stdout.flush()
     values = np.array([[1., 1.]])
     print("values", values)
-    logging.warn("values %s" % values)
+    logging.warning("values %s" % values)
     sys.stdout.flush()
     const = kn.Const(values)
     print("const vertex shape", [i for i in const.getShape()])
-    logging.warn("const vertex shape %s" % [i for i in const.getShape()])
+    logging.warning("const vertex shape %s" % [i for i in const.getShape()])
     print("const vertex value", [i for i in const.getValue().asFlatArray()])
-    logging.warn("const vertex value %s" % [i for i in const.getValue().asFlatArray()])
+    logging.warning("const vertex value %s" % [i for i in const.getValue().asFlatArray()])
     sys.stdout.flush()
     exponential = kn.Exponential(const)
 
