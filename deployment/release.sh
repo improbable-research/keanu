b17553db8e0e539f3f8f20465597e542522c18e1
George Nash <georgenash@improbable.io>
2018-10-16 16:13:24 +0100

    Print out current version when prompting for release version

--- deployment/release.sh
+++ deployment/release.sh
@@ -1,5 +1,8 @@
+echo "Current version is: "
+git describe --abbrev=0
+
 #  version="USER INPUT"
-read -p "Enter version number: " version
+read -p "Enter version number to release: " version
 
 git checkout master
 git flow release start $version
