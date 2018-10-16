478221a58cf14530092149452d10b790bf9fc550
Michael Lewin <michaellewin@improbable.io>
2018-10-16 18:05:44 +0100

    removed Model#getNet from the interface

--- keanu-project/src/main/java/io/improbable/keanu/model/BayesianModel.java
+++ keanu-project/src/main/java/io/improbable/keanu/model/BayesianModel.java
@@ -1,17 +1,10 @@
 package io.improbable.keanu.model;
 
-import io.improbable.keanu.network.BayesianNetwork;
-
 abstract public class BayesianModel<INPUT, OUTPUT> implements Model<INPUT, OUTPUT> {
     private final ModelGraph<INPUT, OUTPUT> modelGraph;
 
     protected BayesianModel(ModelGraph<INPUT, OUTPUT> modelGraph) {
         this.modelGraph = modelGraph;
-    }
-
-    @Override
-    public BayesianNetwork getNet() {
-        return modelGraph.getNet();
     }
 
     @Override
--- keanu-project/src/main/java/io/improbable/keanu/model/Model.java
+++ keanu-project/src/main/java/io/improbable/keanu/model/Model.java
@@ -4,8 +4,6 @@ import io.improbable.keanu.network.BayesianNetwork;
 
 public interface Model<INPUT, OUTPUT> {
 
-    BayesianNetwork getNet();
-
     OUTPUT predict(INPUT input);
 
 }
