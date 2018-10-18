16c0f731a087725bf470c026380d32f0a1401e32
Michael Lewin <michaellewin@improbable.io>
2018-10-18 14:30:43 +0100

    support for operator overloads with numpy arrays

--- keanu-python/tests/test_vertex_operations.py
+++ keanu-python/tests/test_vertex_operations.py
@@ -4,98 +4,64 @@ import pytest
 from tests.keanu_assert import tensors_equal
 
 
-@pytest.fixture
-def jvm_view():
-    from py4j.java_gateway import java_import
-    jvm_view = kn.KeanuContext().jvm_view()
-    java_import(jvm_view, "io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex")
-    return jvm_view
-
-
-def test_can_do_vertex_greater_than_vertex(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = kn.Const(np.array([15., 15.]))
-    result = v1 > v2
+@pytest.mark.parametrize("lhs, rhs, expected_result", [
+    (kn.Const(np.array([10., 20.])), kn.Const(np.array([15., 15.])), np.array([[False], [True]])),
+    (kn.Const(np.array([10., 20.])),          np.array([15., 15.]) , np.array([[False], [True]])),
+    (         np.array([10., 20.]) , kn.Const(np.array([15., 15.])), np.array([[False], [True]])),
+    (kn.Const(np.array([10., 20.])),                         15.   , np.array([[False], [True]])),
+    (                   10.        , kn.Const(np.array([15.,  5.])), np.array([[False], [True]])),
+])
+def test_can_do_greater_than(lhs, rhs, expected_result):
+    result = lhs > rhs
     assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[False], [True]]))
+    assert tensors_equal(result.getValue(), expected_result)
 
-def test_can_do_vertex_greater_than_np_array(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = np.array([15., 15.])
-    result = v1 > v2
+
+@pytest.mark.parametrize("lhs, rhs, expected_result", [
+    (kn.Const(np.array([10., 20.])), kn.Const(np.array([15., 15.])), np.array([[True], [False]])),
+    (kn.Const(np.array([10., 20.])),          np.array([15., 15.]) , np.array([[True], [False]])),
+    (         np.array([10., 20.]) , kn.Const(np.array([15., 15.])), np.array([[True], [False]])),
+    (kn.Const(np.array([10., 20.])),                         15.   , np.array([[True], [False]])),
+    (                   10.        , kn.Const(np.array([15.,  5.])), np.array([[True], [False]])),
+])
+def test_can_do_less_than(lhs, rhs, expected_result):
+    result = lhs < rhs
     assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[False], [True]]))
+    assert tensors_equal(result.getValue(), expected_result)
 
-def test_can_do_vertex_greater_than_number(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = 15.
-    result = v1 > v2
+
+@pytest.mark.parametrize("lhs, rhs, expected_result", [
+    (kn.Const(np.array([10., 20.])), kn.Const(np.array([1., 2.])), np.array([[11], [22]])),
+    (kn.Const(np.array([10., 20.])),          np.array([1., 2.]) , np.array([[11], [22]])),
+    (         np.array([10., 20.]) , kn.Const(np.array([1., 2.])), np.array([[11], [22]])),
+    (kn.Const(np.array([10., 20.])),                        2.   , np.array([[12], [22]])),
+    (                   10.        , kn.Const(np.array([1., 2.])), np.array([[11], [12]])),
+])
+def test_can_do_addition(lhs, rhs, expected_result):
+    result = lhs + rhs
     assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[False], [True]]))
+    assert tensors_equal(result.getValue(), expected_result)
 
-
-def test_can_do_vertex_plus_vertex(jvm_view):
-    v1 = kn.Const(np.array([1., 2.]))
-    v2 = kn.Const(np.array([10., 20.]))
-    result = v1 + v2
+@pytest.mark.parametrize("lhs, rhs, expected_result", [
+    (kn.Const(np.array([10., 20.])), kn.Const(np.array([1., 2.])), np.array([[9], [18]])),
+    (kn.Const(np.array([10., 20.])),          np.array([1., 2.]) , np.array([[9], [18]])),
+    (         np.array([10., 20.]) , kn.Const(np.array([1., 2.])), np.array([[9], [18]])),
+    (kn.Const(np.array([10., 20.])),                        2.   , np.array([[8], [18]])),
+    (                   10.        , kn.Const(np.array([1., 2.])), np.array([[9], [ 8]])),
+])
+def test_can_do_subtraction(lhs, rhs, expected_result):
+    result = lhs - rhs
     assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[11], [22]]))
+    assert tensors_equal(result.getValue(), expected_result)
 
-def test_can_do_vertex_plus_np_array(jvm_view):
-    v1 = kn.Const(np.array([1., 2.]))
-    v2 = np.array([10., 20.])
-    result = v1 + v2
+@pytest.mark.parametrize("lhs, rhs, expected_result", [
+    (kn.Const(np.array([3., 2.])), kn.Const(np.array([5., 7.])), np.array([[15], [14]])),
+    (kn.Const(np.array([3., 2.])),          np.array([5., 7.]) , np.array([[15], [14]])),
+    (         np.array([3., 2.]) , kn.Const(np.array([5., 7.])), np.array([[15], [14]])),
+    (kn.Const(np.array([3., 2.])),                    5.       , np.array([[15], [10]])),
+    (                   3.,        kn.Const(np.array([5., 7.])), np.array([[15], [21]])),
+])
+def test_can_do_multiplication(lhs, rhs, expected_result):
+    result = lhs * rhs
     assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[11], [22]]))
-
-def test_can_do_vertex_plus_number(jvm_view):
-    v1 = kn.Const(np.array([1., 2.]))
-    v2 = 10.
-    result = v1 + v2
-    assert type(result) == kn.Vertex
-    print(result.getValue())
-    assert tensors_equal(result.getValue(), np.array([[11], [12]]))
-
-
-def test_can_do_vertex_minus_vertex(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = kn.Const(np.array([1., 2.]))
-    result = v1 - v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[9], [18]]))
-
-def test_can_do_vertex_minus_np_array(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = np.array([1., 2.])
-    result = v1 - v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[9], [18]]))
-
-def test_can_do_vertex_minus_number(jvm_view):
-    v1 = kn.Const(np.array([10., 20.]))
-    v2 = 1.
-    result = v1 - v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[9], [19]]))
-
-
-def test_can_do_vertex_times_vertex(jvm_view):
-    v1 = kn.Const(np.array([3., 2.]))
-    v2 = kn.Const(np.array([5., 7.]))
-    result = v1 * v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[15], [14]]))
-
-def test_can_do_vertex_times_np_array(jvm_view):
-    v1 = kn.Const(np.array([3., 2.]))
-    v2 = np.array([5., 7.])
-    result = v1 * v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[15], [14]]))
-
-def test_can_do_vertex_times_number(jvm_view):
-    v1 = kn.Const(np.array([3., 2.]))
-    v2 = 5.
-    result = v1 * v2
-    assert type(result) == kn.Vertex
-    assert tensors_equal(result.getValue(), np.array([[15], [10]]))
+    assert tensors_equal(result.getValue(), expected_result)
