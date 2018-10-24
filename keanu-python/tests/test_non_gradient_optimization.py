340017153e4ebea134bba5bd7cfb33600617e924
Joanne Hong <joannehong@improbable.io>
2018-10-24 17:40:48 +0100

    Use size instead of len on Java lists.

--- keanu-python/tests/test_non_gradient_optimization.py
+++ keanu-python/tests/test_non_gradient_optimization.py
@@ -24,7 +24,7 @@ def test_non_gradient_op_bayes_net(model):
 
 def test_non_gradient_op_vertex(model):
     non_gradient_optimizer = kn.NonGradientOptimizer(model.a)
-    assert len(non_gradient_optimizer.net.getLatentVertices()) == 2
+    assert non_gradient_optimizer.net.getLatentVertices().size() == 2
 
 
 def test_non_gradient_op_throws_with_invalid_net_param():
