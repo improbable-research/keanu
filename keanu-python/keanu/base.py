c1af8b06c59badaf9f07c3c34d4c5727269d371b
Michael Lewin <michaellewin@improbable.io>
2018-10-25 18:00:14 +0100

    better error message when the keanu jar is missing from the classpath
    also better __repr__ method for JavaObjectWrapper

--- keanu-python/keanu/base.py
+++ keanu-python/keanu/base.py
@@ -1,12 +1,23 @@
 class JavaObjectWrapper:
     def __init__(self, ctor, *args):
-        self._val = ctor(*args)
+        self._ctor = ctor
         self._args = args
+        try:
+            self._val = ctor(*args)
+        except TypeError as e:
+            raise EnvironmentError(
+                "Cannot call %s - this may be because the keanu jar has not been loaded into py4j's JVM's classpath" 
+                % JavaObjectWrapper.print_signature(ctor, args)
+            ) from e        
         self._class = self.unwrap().getClass().getSimpleName()
 
     def __repr__(self):
-        args = [str(arg) for arg in self._args]
-        return "[{0} => {1}: ({2})]".format(self._class, type(self), ",".join(args))
+        return "[{0} => {1}: {2}]".format(self._class, type(self), JavaObjectWrapper.print_signature(self._ctor, self._args))
+
+    @staticmethod
+    def print_signature(_ctor, _args):
+        args = [str(arg) for arg in _args]
+        return "{0}({1})".format(_ctor._fqn, ",".join(args))
 
     def __getattr__(self, k):
         if k in self.__dict__:
