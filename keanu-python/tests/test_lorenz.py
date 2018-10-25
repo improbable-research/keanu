fc0922608e156c46d4b442c958664cf3faf3450d
Michael Lewin <michaellewin@improbable.io>
2018-10-25 15:18:19 +0100

    use snake case throughout test_lorenz

--- keanu-python/tests/test_lorenz.py
+++ keanu-python/tests/test_lorenz.py
@@ -7,8 +7,8 @@ from lorenz_model import LorenzModel
 
 
 convergedError = 0.01
-windowSize = 8
-maxWindows = 100
+window_size = 8
+max_windows = 100
 
 sigma = 10.
 beta = 2.66667
@@ -19,61 +19,61 @@ time_step = 0.01
 def test_lorenz():
     error = math.inf
     window = 0
-    priorMu = (3.,3.,3.)
+    prior_mu = (3.,3.,3.)
 
     model = LorenzModel(sigma, beta, rho, time_step)
-    observed = list(model.runModel(windowSize * maxWindows))
+    observed = list(model.runModel(window_size * max_windows))
 
-    while error > convergedError and window < maxWindows:
-        xt0 = kn.Gaussian(priorMu[0], 1.0)
-        yt0 = kn.Gaussian(priorMu[1], 1.0)
-        zt0 = kn.Gaussian(priorMu[2], 1.0)
-        graphTimeSteps = list(buildGraph((xt0, yt0, zt0)))
-        xt0.setAndCascade(priorMu[0])
-        yt0.setAndCascade(priorMu[1])
-        zt0.setAndCascade(priorMu[2])
-        applyObservations(graphTimeSteps, window, observed)
+    while error > convergedError and window < max_windows:
+        xt0 = kn.Gaussian(prior_mu[0], 1.0)
+        yt0 = kn.Gaussian(prior_mu[1], 1.0)
+        zt0 = kn.Gaussian(prior_mu[2], 1.0)
+        graph_time_steps = list(buildGraph((xt0, yt0, zt0)))
+        xt0.setAndCascade(prior_mu[0])
+        yt0.setAndCascade(prior_mu[1])
+        zt0.setAndCascade(prior_mu[2])
+        apply_observations(graph_time_steps, window, observed)
         
         optimizer = kn.GradientOptimizer(xt0)
         optimizer.max_a_posteriori()
-        posterior = getTimeSliceValues(graphTimeSteps, windowSize - 1)
+        posterior = get_time_slice_values(graph_time_steps, window_size - 1)
         
-        postT = (window + 1) * (windowSize - 1)
-        actualAtPostT = observed[postT]
+        post_t = (window + 1) * (window_size - 1)
+        actual_at_post_t = observed[post_t]
         
         error = math.sqrt(
-                    (actualAtPostT.x - posterior[0]) ** 2 +
-                    (actualAtPostT.y - posterior[1]) ** 2 +
-                    (actualAtPostT.z - posterior[2]) ** 2
+                    (actual_at_post_t.x - posterior[0]) ** 2 +
+                    (actual_at_post_t.y - posterior[1]) ** 2 +
+                    (actual_at_post_t.z - posterior[2]) ** 2
                 )
-        priorMu = (posterior[0], posterior[1], posterior[2])
+        prior_mu = (posterior[0], posterior[1], posterior[2])
         window += 1
         
     assert error <= convergedError
 
 def add_time(current):
-    rhov = Const(rho)
+    rho_v = Const(rho)
     (xt, yt, zt) = current
 
     x_tplus1 = xt * Const(1. - time_step * sigma) + (yt * Const(time_step * sigma))
-    y_tplus1 = yt * Const(1. - time_step) + (xt * (rhov - zt) * Const(time_step))
+    y_tplus1 = yt * Const(1. - time_step) + (xt * (rho_v - zt) * Const(time_step))
     z_tplus1 = zt * Const(1. - time_step * beta) + (xt * yt * Const(time_step))
     return (x_tplus1, y_tplus1, z_tplus1) 
 
 def buildGraph(initial):
     (x, y, z) = initial
     yield (x, y, z)
-    for _ in range(windowSize):
+    for _ in range(window_size):
         (x, y, z) = add_time((x, y, z))
         yield (x, y, z)
     
-def applyObservations(graphTimeSteps, window, observed):
-    for (idx, timeSlice) in enumerate(graphTimeSteps):
-        t = window * (windowSize - 1) + idx
-        xt = timeSlice[0]
-        observedXt = kn.Gaussian(xt, 1.0)
-        observedXt.observe(observed[t].x)
+def apply_observations(graph_time_steps, window, observed):
+    for (idx, time_slice) in enumerate(graph_time_steps):
+        t = window * (window_size - 1) + idx
+        xt = time_slice[0]
+        observed_xt = kn.Gaussian(xt, 1.0)
+        observed_xt.observe(observed[t].x)
 
-def getTimeSliceValues(timeSteps, time):
-    slice = timeSteps[time]
+def get_time_slice_values(time_steps, time):
+    slice = time_steps[time]
     return list(map(lambda v : v.getValue(), slice))
