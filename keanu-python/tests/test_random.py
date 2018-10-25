35b1fc0194bfd018b3b7ec2371652c08e0c67693
Joanne Hong <joannehong@improbable.io>
2018-10-25 13:58:10 +0100

    Add more assertions to keanu random tests

--- keanu-python/tests/test_random.py
+++ keanu-python/tests/test_random.py
@@ -6,9 +6,11 @@ def test_default_keanu_random():
     random = keanu_random.next_double()
 
     assert type(random) == float
+    assert 0 <= random < 1
 
 def test_seeded_keanu_random():
     keanu_random = kn.KeanuRandom(1)
     random = keanu_random.next_double()
 
     assert type(random) == float
+    assert random == 0.1129943035738381
