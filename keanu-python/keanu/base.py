df1fc3d26fa10810dedb6de6b523ba5464a64f15
Joanne Hong <joannehong@improbable.io>
2018-10-25 14:22:36 +0100

    Move name conversion to utils and write a doctest for it

--- keanu-python/keanu/base.py
+++ keanu-python/keanu/base.py
@@ -1,8 +1,5 @@
-import re
 import warnings
-
-first_cap_re = re.compile('(.)([A-Z][a-z]+)')
-all_cap_re = re.compile('([a-z0-9])([A-Z])')
+from keanu.utils import get_java_name, get_python_name
 
 class JavaObjectWrapper:
     def __init__(self, val):
@@ -13,26 +10,16 @@ class JavaObjectWrapper:
         return "[{0} => {1}]".format(self._class, type(self))
 
     def __getattr__(self, k):
-        python_name = JavaObjectWrapper.__get_python_name(k)
+        python_name = get_python_name(k)
         if python_name in self.__class__.__dict__:
             return getattr(self, python_name)
 
-        java_name = JavaObjectWrapper.__get_java_name(k)
+        java_name = get_java_name(k)
         warnings.warn("\"{}\" is not implemented so Java API was called directly instead".format(k))
         return self.unwrap().__getattr__(java_name)
 
     def unwrap(self):
         return self._val
-
-    @staticmethod
-    def __get_java_name(name):
-       first, *rest = name.split('_')
-       return first + ''.join(word.capitalize() for word in rest)
-
-    @staticmethod
-    def __get_python_name(name):
-        s1 = first_cap_re.sub(r'\1_\2', name)
-        return all_cap_re.sub(r'\1_\2', s1).lower()
 
 
 class JavaCtor(JavaObjectWrapper):
