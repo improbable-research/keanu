155eb0a69fe18ef619dd1a4b60ab269f92aa8519
George Nash <georgenash@improbable.io>
2018-10-25 14:59:42 +0100

    Default method for single vertex sampling

--- keanu-project/src/main/java/io/improbable/keanu/algorithms/PosteriorSamplingAlgorithm.java
+++ keanu-project/src/main/java/io/improbable/keanu/algorithms/PosteriorSamplingAlgorithm.java
@@ -3,9 +3,18 @@ package io.improbable.keanu.algorithms;
 import io.improbable.keanu.network.BayesianNetwork;
 import io.improbable.keanu.vertices.Vertex;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public interface PosteriorSamplingAlgorithm {
+
+    default NetworkSamples getPosteriorSamples(BayesianNetwork bayesianNetwork,
+                                               Vertex vertexToSampleFrom,
+                                               int sampleCount) {
+        return getPosteriorSamples(bayesianNetwork, Collections.singletonList(vertexToSampleFrom), sampleCount);
+    }
 
     NetworkSamples getPosteriorSamples(BayesianNetwork bayesNet,
                                        List<? extends Vertex> verticesToSampleFrom,
