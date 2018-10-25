f84b580e78bce8aaf22a446d64890ed043ebaec7
CharlieCrisp <charliecrisp@improbable.io>
2018-10-25 16:15:57 +0100

    spotless apply

--- keanu-project/src/test/java/io/improbable/keanu/algorithms/particlefiltering/ParticleFilteringTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/algorithms/particlefiltering/ParticleFilteringTest.java
@@ -9,8 +9,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 import static org.junit.Assert.assertTrue;
 
 public class ParticleFilteringTest {
