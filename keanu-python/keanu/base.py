49f65fa9cfd0587da2c075e61971e788f12679a2
Joanne Hong <joannehong@improbable.io>
2018-10-26 14:49:04 +0100

    Make AttributeError more specific

--- keanu-python/keanu/base.py
+++ keanu-python/keanu/base.py
@@ -1,5 +1,5 @@
 import warnings
-from keanu.utils import get_java_name
+from keanu.utils import get_java_name, get_python_name
 
 class JavaObjectWrapper:
     def __init__(self, val):
@@ -13,7 +13,12 @@ class JavaObjectWrapper:
         java_name = get_java_name(k)
 
         if not k.islower() and k == java_name:
-            raise AttributeError("{} has no attribute {}".format(self, k))
+            python_name = get_python_name(k)
+
+            if python_name in self.__class__.__dict__:
+                raise AttributeError("{} has no attribute {}. Did you mean {}?".format(self.__class__, k, python_name))
+
+            raise AttributeError("{} has no attribute {}".format(self.__class__, k))
 
         warnings.warn("\"{}\" is not implemented so Java API was called directly instead".format(k))
         return self.unwrap().__getattr__(java_name)
--- keanu-python/keanu/utils.py
+++ keanu-python/keanu/utils.py
@@ -1,3 +1,8 @@
+import re
+
+first_cap_re = re.compile('(.)([A-Z][a-z]+)')
+all_cap_re = re.compile('([a-z0-9])([A-Z])')
+
 def get_java_name(name):
     """
     >>> get_java_name("snake_case_name")
@@ -7,3 +12,13 @@ def get_java_name(name):
     """
     first, *rest = name.split('_')
     return first + ''.join(word.capitalize() for word in rest)
+
+def get_python_name(name):
+    """
+    >>> get_python_name("camelCaseName")
+    'camel_case_name'
+    >>> get_python_name("snake_case_name")
+    'snake_case_name'
+    """
+    s1 = first_cap_re.sub(r'\1_\2', name)
+    return all_cap_re.sub(r'\1_\2', s1).lower()
