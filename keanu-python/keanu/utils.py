e499478ca2d3e2d6d79603fcc4675ac78e1cfd03
Joanne Hong <joannehong@improbable.io>
2018-10-26 13:25:29 +0100

    cleanup __getattr__ for wrapper

--- keanu-python/keanu/utils.py
+++ keanu-python/keanu/utils.py
@@ -1,8 +1,3 @@
-import re
-
-first_cap_re = re.compile('(.)([A-Z][a-z]+)')
-all_cap_re = re.compile('([a-z0-9])([A-Z])')
-
 def get_java_name(name):
     """
     >>> get_java_name("snake_case_name")
@@ -12,19 +7,3 @@ def get_java_name(name):
     """
     first, *rest = name.split('_')
     return first + ''.join(word.capitalize() for word in rest)
-
-def is_python_name(name):
-    """
-    >>> is_python_name("snake_case_name")
-    True
-    >>> is_python_name("snakecasename")
-    True
-    >>> is_python_name("camelCaseName")
-    False
-    >>> is_python_name("camelCaseNAME")
-    False
-    """
-    s1 = first_cap_re.sub(r'\1_\2', name)
-    python_name = all_cap_re.sub(r'\1_\2', s1).lower()
-
-    return python_name == name
