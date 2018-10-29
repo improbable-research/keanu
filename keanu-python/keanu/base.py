360692884f9a34385d8de86a0f70495acaf82d6b
Joanne Hong <joannehong@improbable.io>
2018-10-29 14:50:42 +0000

    Rename case conversion methods

--- keanu-python/keanu/base.py
+++ keanu-python/keanu/base.py
@@ -1,5 +1,5 @@
 import warnings
-from keanu.case_conversion import _get_java_name, _get_python_name
+from keanu.case_conversion import _get_camel_case_name, _get_snake_case_name
 
 class JavaObjectWrapper:
     def __init__(self, val):
@@ -10,7 +10,7 @@ class JavaObjectWrapper:
         return "[{0} => {1}]".format(self._class, type(self))
 
     def __getattr__(self, k):
-        python_name = _get_python_name(k)
+        python_name = _get_snake_case_name(k)
 
         if k != python_name:
             if python_name in self.__class__.__dict__:
@@ -19,7 +19,7 @@ class JavaObjectWrapper:
             raise AttributeError("{} has no attribute {}".format(self.__class__, k))
 
         warnings.warn("\"{}\" is not implemented so Java API was called directly instead".format(k))
-        return self.unwrap().__getattr__(_get_java_name(k))
+        return self.unwrap().__getattr__(_get_camel_case_name(k))
 
     def unwrap(self):
         return self._val
--- keanu-python/keanu/case_conversion.py
+++ keanu-python/keanu/case_conversion.py
@@ -3,21 +3,21 @@ import re
 first_cap_re = re.compile('(.)([A-Z][a-z]+)')
 all_cap_re = re.compile('([a-z0-9])([A-Z])')
 
-def _get_java_name(name):
+def _get_camel_case_name(name):
     """
-    >>> _get_java_name("snake_case_name")
+    >>> _get_camel_case_name("snake_case_name")
     'snakeCaseName'
-    >>> _get_java_name("camelCaseName")
+    >>> _get_camel_case_name("camelCaseName")
     'camelCaseName'
     """
     first, *rest = name.split('_')
     return first + ''.join(word.capitalize() for word in rest)
 
-def _get_python_name(name):
+def _get_snake_case_name(name):
     """
-    >>> _get_python_name("camelCaseName")
+    >>> _get_snake_case_name("camelCaseName")
     'camel_case_name'
-    >>> _get_python_name("snake_case_name")
+    >>> _get_snake_case_name("snake_case_name")
     'snake_case_name'
     """
     s1 = first_cap_re.sub(r'\1_\2', name)
