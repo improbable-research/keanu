3104e9e891e50d8d6bbdff21fe6578e83e0f3120
CharlieCrisp <charliecrisp@improbable.io>
2018-11-06 16:09:02 +0000

    Use constructorDoc.tags

--- codegen/src/main/java/io/improbable/keanu/codegen/python/ParamStringProcessor.java
+++ codegen/src/main/java/io/improbable/keanu/codegen/python/ParamStringProcessor.java
@@ -1,6 +1,7 @@
 package io.improbable.keanu.codegen.python;
 
 import com.sun.javadoc.ConstructorDoc;
+import com.sun.javadoc.Tag;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,17 +11,11 @@ import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 
 class ParamStringProcessor {
     static Map<String, String> getNameToCommentMapping(ConstructorDoc constructorDoc) {
-        String rawComment = constructorDoc.getRawCommentText();
-        String[] rawCommentLines = rawComment.split("\\r?\\n");
         Map<String, String> nameToCommentMapping = new HashMap<>();
-        for (String commentLine : rawCommentLines) {
-            if (!commentLine.contains("@param")) {
-                continue;
-            }
-            commentLine = commentLine.replaceFirst("[ ]{2,}", " ");
-            String[] splitComment = commentLine.split(" ", 4);
-            String snakeCaseParamName = UPPER_CAMEL.to(LOWER_UNDERSCORE, splitComment[2]);
-            String paramComment = splitComment[3];
+        Tag[] params = constructorDoc.tags("@param");
+        for (Tag param: params) {
+            String snakeCaseParamName = UPPER_CAMEL.to(LOWER_UNDERSCORE, param.name());
+            String paramComment = param.text();
             nameToCommentMapping.put(snakeCaseParamName, paramComment);
         }
         return nameToCommentMapping;
