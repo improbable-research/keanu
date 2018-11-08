ae7a9e5ad31d5f88b8b0d6d86723c4e67d934815
Joanne Hong <joannehong@improbable.io>
2018-11-08 11:41:18 +0000

    Mypy now detects some imcompatible types

--- keanu-python/keanu/vartypes.py
+++ keanu-python/keanu/vartypes.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from typing import Any, Iterable, Union, Tuple
 from keanu.base import JavaObjectWrapper
+from keanu.vertex.base import VertexOps
 
 int_types = (int, np.integer)
 
@@ -15,10 +16,10 @@ pandas_types = (pd.Series, pd.DataFrame)
 
 numpy_types = (np.ndarray, )
 
-const_arg_types = numpy_types + pandas_types + primitive_types
+const_arg_types = (int, float, bool, np.integer, np.float_, np.bool_, pd.Series, pd.DataFrame, np.ndarray)#numpy_types + pandas_types + primitive_types
 
 mypy_shape_types = Iterable[Union[int, float, bool, np.integer, np.float_, np.bool_]]
 
 mypy_const_arg_types = Union[int, float, bool, np.integer, np.float_, np.bool_, pd.Series, pd.DataFrame, np.ndarray]
 
-mypy_vertex_arg_types = Union[int, float, bool, np.integer, np.float_, np.bool_, pd.Series, pd.DataFrame, np.ndarray, JavaObjectWrapper]
+mypy_vertex_arg_types = Union[int, float, bool, np.integer, np.float_, np.bool_, pd.Series, pd.DataFrame, np.ndarray, JavaObjectWrapper, VertexOps]
