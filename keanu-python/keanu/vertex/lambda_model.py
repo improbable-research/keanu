898da6057dc9ea355298f9ca93e933f009a45b02
Michael Lewin <michaellewin@improbable.io>
2018-11-08 18:15:36 +0000

    added a default update_values method

--- keanu-python/keanu/vertex/lambda_model.py
+++ keanu-python/keanu/vertex/lambda_model.py
@@ -3,14 +3,20 @@ from keanu.vertex import Vertex
 from keanu.vertex.vertex_label import VertexLabel
 from keanu.tensor import Tensor
 from keanu.functional import Consumer, Function
+from keanu.vertex.base import JavaObjectWrapper
 from py4j.java_gateway import java_import
 
 
 context = KeanuContext()
 java_import(context.jvm_view(), "io.improbable.keanu.vertices.model.LambdaModelVertex")
 
+def _identity_update(vertices):
+    vertex_out = vertices[VertexLabel("out").unwrap()]
+    value_map = {VertexLabel("out").unwrap() : JavaObjectWrapper(vertex_out.getValue()) }
+    return context.to_java_map(value_map)
+
 class LambdaModel(Vertex):
-    def __init__(self, inputs: map, executor: Consumer, update_value: Function) -> Vertex:
+    def __init__(self, inputs: map, executor: Consumer, update_value: Function = Function(_identity_update)) -> Vertex:
         self.map = map
         self.executor = executor
         self.update_value = update_value
@@ -21,3 +27,5 @@ class LambdaModel(Vertex):
         label = VertexLabel(label)
         result = self.unwrap().getDoubleModelOutputVertex(label.unwrap())
         return Vertex(result)
+
+
--- keanu-python/tests/test_lambdas.py
+++ keanu-python/tests/test_lambdas.py
@@ -12,18 +12,12 @@ def plus_one(vertices):
     input_vertex = vertices["in"]
     vertices[VertexLabel("out").unwrap()] = (Vertex(input_vertex) + 1.).unwrap()
 
-def get_values(vertices):
-    vertex_out = vertices[VertexLabel("out").unwrap()]
-    value_map = {VertexLabel("out").unwrap() : JavaObjectWrapper(vertex_out.getValue()) }
-    return context.to_java_map(value_map)
-
 def test_you_can_create_a_lambda_model_vertex():
     v_in = Gaussian(1., 1.)
     inputs = context.to_java_map({ "in": v_in })
 
     executor = Consumer(plus_one)
-    update_values = Function(get_values)
-    model = LambdaModel(inputs, executor, update_values)
+    model = LambdaModel(inputs, executor)
     v_out = model.get_double_model_output_vertex("out")
 
     v_in.set_value(1.)
