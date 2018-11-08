0b2cbe2f8eedddf77157e9402e4df761a3486a93
Joanne Hong <joannehong@improbable.io>
2018-11-08 17:15:29 +0000

    Add float type to numpy type stubs. float_ is not the actual type in ndarray.

--- keanu-python/keanu/vartypes.py
+++ keanu-python/keanu/vartypes.py
@@ -1,20 +1,20 @@
 from typing import Iterable, Union, Type, Any, TYPE_CHECKING
-from numpy import ndarray, integer, float_, bool_
+import numpy as np
 from pandas import Series, DataFrame
 from keanu.base import JavaObjectWrapper
-from keanu.vertex.base import VertexOps
+#from keanu.vertex.base import VertexOps
 
-int_types = Union[int, integer]
-float_types = Union[int, float_]
-bool_types = Union[bool, bool_]
+int_types = Union[int, np.integer]
+float_types = Union[int, np.float]
+bool_types = Union[bool, np.bool_]
 
 primitive_types = Union[int_types, float_types, bool_types]
 
 pandas_types = Union[Series, DataFrame]
-numpy_types = Union[ndarray]
+numpy_types = Union[np.ndarray]
 
 const_arg_types = Union[primitive_types, pandas_types, numpy_types]
-vertex_arg_types = Union[const_arg_types, JavaObjectWrapper, VertexOps]
+vertex_arg_types = Union[const_arg_types, JavaObjectWrapper]
 shape_types = Iterable[primitive_types]
 
 runtime_numpy_types : Type[Any]
@@ -26,10 +26,10 @@ runtime_bool_types : Type[Any]
 runtime_const_arg_types : Type[Any]
 
 if not TYPE_CHECKING:
-    runtime_numpy_types = numpy_types.__args__
+    runtime_numpy_types = numpy_types
     runtime_pandas_types = pandas_types.__args__
     runtime_primitive_types = primitive_types.__args__
     runtime_int_types = int_types.__args__
     runtime_float_types = float_types.__args__
     runtime_bool_types = bool_types.__args__
-    runtime_const_arg_types = const_arg_Types.__args__
+    runtime_const_arg_types = const_arg_types.__args__
