e94915abf8165beea4178c4cbfb99eda0d6fc899
Chris North <christophernorth@improbable.io>
2018-11-18 18:23:52 +0000

    Annotated Unary Op Vertices we support

--- keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerAbsVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerAbsVertex.java
@@ -2,15 +2,17 @@ package io.improbable.keanu.vertices.intgr.nonprobabilistic.operators.unary;
 
 
 import io.improbable.keanu.tensor.intgr.IntegerTensor;
+import io.improbable.keanu.vertices.LoadParentVertex;
+import io.improbable.keanu.vertices.SaveableVertex;
 import io.improbable.keanu.vertices.intgr.IntegerVertex;
 
-public class IntegerAbsVertex extends IntegerUnaryOpVertex {
+public class IntegerAbsVertex extends IntegerUnaryOpVertex implements SaveableVertex {
 
     /**
      * Takes the absolute value of a vertex
      * @param inputVertex the vertex
      */
-    public IntegerAbsVertex(IntegerVertex inputVertex) {
+    public IntegerAbsVertex(@LoadParentVertex(INPUT_NAME) IntegerVertex inputVertex) {
         super(inputVertex.getShape(), inputVertex);
     }
 
--- keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerSumVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerSumVertex.java
@@ -1,16 +1,18 @@
 package io.improbable.keanu.vertices.intgr.nonprobabilistic.operators.unary;
 
 import io.improbable.keanu.tensor.intgr.IntegerTensor;
+import io.improbable.keanu.vertices.LoadParentVertex;
+import io.improbable.keanu.vertices.SaveableVertex;
 import io.improbable.keanu.vertices.intgr.IntegerVertex;
 
-public class IntegerSumVertex extends IntegerUnaryOpVertex {
+public class IntegerSumVertex extends IntegerUnaryOpVertex implements SaveableVertex {
 
     /**
      * Performs a sum across each value stored in a vertex
      *
      * @param inputVertex the vertex to have its values summed
      */
-    public IntegerSumVertex(IntegerVertex inputVertex) {
+    public IntegerSumVertex(@LoadParentVertex(INPUT_NAME) IntegerVertex inputVertex) {
         super(inputVertex.getShape(), inputVertex);
     }
 
