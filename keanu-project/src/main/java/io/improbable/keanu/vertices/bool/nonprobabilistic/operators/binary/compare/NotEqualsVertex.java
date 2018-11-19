3a128b19345a26357a7b221e406eca7c48649c91
Chris North <christophernorth@improbable.io>
2018-11-19 15:38:54 +0000

    Annotate the Boolean Operators

--- keanu-project/src/main/java/io/improbable/keanu/vertices/bool/nonprobabilistic/operators/binary/compare/NotEqualsVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/bool/nonprobabilistic/operators/binary/compare/NotEqualsVertex.java
@@ -3,13 +3,15 @@ package io.improbable.keanu.vertices.bool.nonprobabilistic.operators.binary.comp
 import io.improbable.keanu.annotation.ExportVertexToPythonBindings;
 import io.improbable.keanu.tensor.Tensor;
 import io.improbable.keanu.tensor.bool.BooleanTensor;
+import io.improbable.keanu.vertices.LoadParentVertex;
+import io.improbable.keanu.vertices.SaveableVertex;
 import io.improbable.keanu.vertices.Vertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.binary.BoolBinaryOpVertex;
 
-public class NotEqualsVertex<A extends Tensor, B extends Tensor> extends BoolBinaryOpVertex<A, B> {
+public class NotEqualsVertex<A extends Tensor, B extends Tensor> extends BoolBinaryOpVertex<A, B> implements SaveableVertex {
 
     @ExportVertexToPythonBindings
-    public NotEqualsVertex(Vertex<A> a, Vertex<B> b) {
+    public NotEqualsVertex(@LoadParentVertex(A_NAME) Vertex<A> a, @LoadParentVertex(B_NAME) Vertex<B> b) {
         super(a, b);
     }
 
