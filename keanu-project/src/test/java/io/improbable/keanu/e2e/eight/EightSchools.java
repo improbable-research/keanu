f9f688b25ba2a22683ce500e9ee028ad05ad1d9c
Caleb <caleb@improbable.io>
2018-11-19 15:05:50 +0000

    refactor NUTS to use sampler with progress bar

--- keanu-project/src/test/java/io/improbable/keanu/e2e/eight/EightSchools.java
+++ keanu-project/src/test/java/io/improbable/keanu/e2e/eight/EightSchools.java
@@ -7,12 +7,13 @@ import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.ConstantVertex;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
-import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.HalfCauchyVertex;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 
+@Slf4j
 public class EightSchools {
 
     public static void main(String[] args) {
@@ -28,7 +29,9 @@ public class EightSchools {
         double[] sigma = new double[]{15., 10., 16., 11., 9., 11., 10., 18.};
 
         DoubleVertex mu = new GaussianVertex(0, 5);
+        mu.setValue(0);
         DoubleVertex tau = new HalfCauchyVertex(5);
+        tau.setValue(3);
         DoubleVertex theta_tilde = new GaussianVertex(new long[]{1, J}, 0, 1);
         DoubleVertex theta = mu.plus(tau.multiply(theta_tilde));
         DoubleVertex y = new GaussianVertex(theta, ConstantVertex.of(sigma));
@@ -40,7 +43,7 @@ public class EightSchools {
 
         NUTS sampler = NUTS.builder()
             .adaptEnabled(false)
-            .stepSize(0.3)
+            .initialStepSize(0.3)
             .random(random)
             .build();
 
@@ -55,10 +58,10 @@ public class EightSchools {
         DoubleTensor theta_mean = samples.getDoubleTensorSamples(theta).getAverages();
         DoubleTensor theta_tilde_mean = samples.getDoubleTensorSamples(theta_tilde).getAverages();
 
-        System.out.println("Average value for tau : " + tau_mean);
-        System.out.println("Average value for theta: " + theta_mean);
-        System.out.println("Average value for theta_tilde: " + theta_tilde_mean);
-        System.out.println("Average value for mu: " + mu_mean);
+        log.info("Average value for tau : " + tau_mean);
+        log.info("Average value for theta: " + theta_mean);
+        log.info("Average value for theta_tilde: " + theta_tilde_mean);
+        log.info("Average value for mu: " + mu_mean);
     }
 
 }
