413b0a9f1a09b27e7607efb5707583f35675da88
Wesley Williams <wesley@improbable.io>
2018-11-20 14:37:49 +0000

    Remove saveValue from Nonsaveable

--- keanu-project/src/main/java/io/improbable/keanu/vertices/NonSaveableVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/NonSaveableVertex.java
@@ -6,7 +6,4 @@ public interface NonSaveableVertex {
     default void save(NetworkWriter netWriter) {
         throw new IllegalArgumentException("Trying to save a vertex that isn't Saveable");
     }
-    default void saveValue(NetworkWriter netWriter) {
-        throw new IllegalArgumentException("Trying to save a vertex that isn't Saveable");
-    }
 }
