5b952162b21ff469a1a1d0be563d355bc22a9355
Michael Lewin <michaellewin@improbable.io>
2018-11-20 15:23:18 +0000

    rewrote StatisticsCalculator so it uses the raw data instead of SummaryStatistics.
    This is because I realised I will need the raw data to calculate the residuals.

--- keanu-project/src/main/java/io/improbable/keanu/tensor/StatisticsCalculator.java
+++ keanu-project/src/main/java/io/improbable/keanu/tensor/StatisticsCalculator.java
@@ -1,80 +1,47 @@
 package io.improbable.keanu.tensor;
 
-import com.google.common.base.Preconditions;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
-import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
 public class StatisticsCalculator {
 
-    private final SummaryStatistics xStats;
-    private final SummaryStatistics yStats;
-    private final SummaryStatistics xyStats;
+    private final DoubleTensor xData;
+    private final DoubleTensor yData;
 
-    private StatisticsCalculator(SummaryStatistics xStats, SummaryStatistics yStats, SummaryStatistics xyStats) {
-        Preconditions.checkArgument(xStats.getN() == yStats.getN(),
-            "xStats and yStats must have the same size",
-            xStats.getN(),
-            yStats.getN());
-        Preconditions.checkArgument(xStats.getN() == xyStats.getN(),
-            "xStats and xyStats must have the same size",
-            xStats.getN(),
-            xyStats.getN());
-        this.xStats = xStats;
-        this.yStats = yStats;
-        this.xyStats = xyStats;
-
+    public StatisticsCalculator(DoubleTensor xData, DoubleTensor yData) {
+        TensorShapeValidation.checkAllShapesMatch(xData.getShape(), yData.getShape());
+        this.xData = xData;
+        this.yData = yData;
     }
 
     public static StatisticsCalculator forData(DoubleTensor xData, DoubleTensor yData) {
-        SummaryStatistics xStats = createStatistics(xData);
-        SummaryStatistics yStats = createStatistics(yData);
-        SummaryStatistics xyStats = createCombinedStatistics(xData, yData);
-        return new StatisticsCalculator(xStats, yStats, xyStats);
-    }
-
-    private static SummaryStatistics createStatistics(DoubleTensor data) {
-        SummaryStatistics stats = new SummaryStatistics();
-        double[] values = data.asFlatDoubleArray();
-        for (int i = 0; i < values.length; i++) {
-            stats.addValue(values[i]);
-        }
-        return stats;
-    }
-
-    private static SummaryStatistics createCombinedStatistics(DoubleTensor xData, DoubleTensor yData) {
-        TensorShapeValidation.checkAllShapesMatch(xData.getShape(), yData.getShape());
-        SummaryStatistics stats = new SummaryStatistics();
-        double[] x = xData.asFlatDoubleArray();
-        double[] y = yData.asFlatDoubleArray();
-        for (int i = 0; i < x.length; i++) {
-            stats.addValue(x[i] * y[i]);
-        }
-        return stats;
-    }
-
-    public double calculateMeanSquaredError() {
-        return 0.;
+        return new StatisticsCalculator(xData, yData);
     }
 
     public long size() {
-        return xStats.getN();
+        return xData.getLength();
     }
 
     public double xMean() {
-        return xStats.getMean();
+        return xData.average();
     }
 
     public double yMean() {
-        return yStats.getMean();
+        return yData.average();
     }
 
-    public double estimatedGradient() {
-        double s_xy = xyStats.getSum() - (xStats.getSum() * yStats.getSum() / size());
-        double s_xx = xStats.getSecondMoment();
+    public double estimateGradient() {
+        double sumX = xData.sum();
+        double sumY = yData.sum();
+        double sumXX = xData.times(xData).sum();
+        double sumXY = xData.times(yData).sum();
+
+        double s_xx = sumXX - (sumX * sumX / size());
+        double s_xy = sumXY - (sumX * sumY / size());
+
         return s_xy / s_xx;
     }
 
     public double estimateIntercept() {
-        return yMean() - estimatedGradient() * xMean();
+        return yMean() - estimateGradient() * xMean();
     }
 }
--- keanu-project/src/test/java/io/improbable/keanu/tensor/StatisticsCalculatorTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/tensor/StatisticsCalculatorTest.java
@@ -1,6 +1,7 @@
 package io.improbable.keanu.tensor;
 
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -14,52 +15,82 @@ public class StatisticsCalculatorTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    private DoubleTensor xData = DoubleTensor.create(1., 2., 3., 4.);
-    private DoubleTensor yData = DoubleTensor.create(0., 1., 2., 3.);
-    private StatisticsCalculator stats = StatisticsCalculator.forData(xData, yData);
+    private static final DoubleTensor X_DATA = DoubleTensor.create(1., 2., 3., 4.);
+    private static final DoubleTensor Y_DATA = DoubleTensor.create(0.1, 1.4, 2.2, 3.1);
+    SummaryStatistics xStats = createStatistics(X_DATA);
+    SummaryStatistics yStats = createStatistics(Y_DATA);
+    SummaryStatistics xyStats = createCombinedStatistics(X_DATA, Y_DATA);
+
+    private StatisticsCalculator stats = StatisticsCalculator.forData(X_DATA, Y_DATA);
+
+    private static SummaryStatistics createStatistics(DoubleTensor data) {
+        SummaryStatistics stats = new SummaryStatistics();
+        double[] values = data.asFlatDoubleArray();
+        for (int i = 0; i < values.length; i++) {
+            stats.addValue(values[i]);
+        }
+        return stats;
+    }
+
+    private static SummaryStatistics createCombinedStatistics(DoubleTensor xData, DoubleTensor yData) {
+        TensorShapeValidation.checkAllShapesMatch(xData.getShape(), yData.getShape());
+        SummaryStatistics stats = new SummaryStatistics();
+        double[] x = xData.asFlatDoubleArray();
+        double[] y = yData.asFlatDoubleArray();
+        for (int i = 0; i < x.length; i++) {
+            stats.addValue(x[i] * y[i]);
+        }
+        return stats;
+    }
 
     @Test
     public void ifYouConstructItWithTensorsTheirShapesMustMatch() {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Shapes must match");
 
+        DoubleTensor biggerData = DoubleTensor.create(1., 2., 3., 4., 5., 6.);
         DoubleTensor smallerData = DoubleTensor.create(1., 2., 3.);
-        StatisticsCalculator.forData(xData, smallerData);
+        StatisticsCalculator.forData(biggerData, smallerData);
     }
 
     @Test
     public void youCanGetTheNumberOfSamples() {
-        assertThat(stats.size(), equalTo(xData.getLength()));
+        assertThat(stats.size(), equalTo(xStats.getN()));
     }
 
     @Test
     public void youCanGetTheMeanOfX() {
-        assertThat(stats.xMean(), equalTo(xData.average()));
+        assertThat(stats.xMean(), equalTo(xStats.getMean()));
     }
 
     @Test
     public void youCanGetTheMeanOfY() {
-        assertThat(stats.yMean(), equalTo(yData.average()));
+        assertThat(stats.yMean(), equalTo(yStats.getMean()));
     }
 
     @Test
     public void youCanGetTheRegressionCoefficients() {
-        long n = xData.getLength();
-        double sum_x = xData.sum();
-        double xBar = xData.average();
-        double sum_xsq = xData.times(xData).sum();
-        double s_xx = sum_xsq - (sum_x * sum_x / n);
 
-        double sum_y = yData.sum();
-        double yBar = yData.average();
-        double sum_xy = xData.times(yData).sum();
-        double s_xy = sum_xy - (sum_x * sum_y / n);
+        double s_xy = xyStats.getSum() - (xStats.getSum() * yStats.getSum() / xStats.getN());
+        double s_xx = xStats.getSecondMoment();
 
         double expectedGradient = s_xy / s_xx;
-        assertThat(stats.estimatedGradient(), equalTo(expectedGradient));
+        assertThat(stats.estimateGradient(), equalTo(expectedGradient));
 
-        double expectedIntercept = yBar - expectedGradient * xBar;
+        double expectedIntercept = yStats.getMean() - expectedGradient * xStats.getMean();
         assertThat(stats.estimateIntercept(), equalTo(expectedIntercept));
 
     }
+
+//    @Test
+//    public void youCanGetTheMeanSquaredError() {
+//        double estimatedIntercept = stats.estimateIntercept();
+//        double estimatedGradient = stats.estimateGradient();
+//        DoubleTensor calculatedY = X_DATA.times(estimatedGradient).plusInPlace(estimatedIntercept);
+//        DoubleTensor residuals = Y_DATA.minus(calculatedY);
+//        StatisticsCalculator residualStats = StatisticsCalculator.forData(residuals, residuals);
+//        double meanSquaredError = residuals.times(residuals).sum() / (stats.size() - 2);
+//        assertThat(stats.getMeanSquaredError(X_DATA, Y_DATA), equalTo(meanSquaredError));
+//
+//    }
 }
