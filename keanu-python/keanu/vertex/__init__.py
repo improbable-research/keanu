62f5beecdfb45afc78624f4aba5fb71869f1f8d3
Joanne Hong <joannehong@improbable.io>
2018-11-21 12:35:14 +0000

    Apply more formatting.

--- keanu-python/keanu/vertex/__init__.py
+++ keanu-python/keanu/vertex/__init__.py
@@ -1,15 +1,7 @@
 ## This is a generated file. DO NOT EDIT.
 
 # This allows us to make these vertices visible externally so they can be picked up for our python docs
-__all__ = [
-    "ConstantBool", "Equals", "GreaterThanOrEqual", "GreaterThan", "LessThanOrEqual", "LessThan", "NotEquals",
-    "CastDouble", "ConstantDouble", "DoubleIf", "Addition", "ArcTan2", "Difference", "Division", "MatrixMultiplication",
-    "Max", "Min", "Multiplication", "Power", "Abs", "ArcCos", "ArcSin", "ArcTan", "Ceil", "Cos", "Exp", "Floor",
-    "LogGamma", "Log", "MatrixDeterminant", "MatrixInverse", "Reshape", "Round", "Sigmoid", "Sin", "Sum", "Take", "Tan",
-    "Beta", "Cauchy", "ChiSquared", "Dirichlet", "Exponential", "Gamma", "Gaussian", "HalfCauchy", "HalfGaussian",
-    "InverseGamma", "Laplace", "LogNormal", "Logistic", "MultivariateGaussian", "Pareto", "SmoothUniform", "StudentT",
-    "Triangular", "Uniform", "ConstantInteger", "IntegerDivision", "Poisson", "UniformInt"
-]
+__all__ = ["ConstantBool", "Equals", "GreaterThanOrEqual", "GreaterThan", "LessThanOrEqual", "LessThan", "NotEquals", "CastDouble", "ConstantDouble", "DoubleIf", "Addition", "ArcTan2", "Difference", "Division", "MatrixMultiplication", "Max", "Min", "Multiplication", "Power", "Abs", "ArcCos", "ArcSin", "ArcTan", "Ceil", "Cos", "Exp", "Floor", "LogGamma", "Log", "MatrixDeterminant", "MatrixInverse", "Reshape", "Round", "Sigmoid", "Sin", "Sum", "Take", "Tan", "Beta", "Cauchy", "ChiSquared", "Dirichlet", "Exponential", "Gamma", "Gaussian", "HalfCauchy", "HalfGaussian", "InverseGamma", "Laplace", "LogNormal", "Logistic", "MultivariateGaussian", "Pareto", "SmoothUniform", "StudentT", "Triangular", "Uniform", "ConstantInteger", "IntegerDivision", "Poisson", "UniformInt"]
 
 from .generated import *
 from .const import Const
