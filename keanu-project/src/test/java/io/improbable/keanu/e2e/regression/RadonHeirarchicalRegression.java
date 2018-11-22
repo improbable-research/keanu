b550265d6dc27fb7a9a151374facec9d4b265c7c
George Nash <georgenash@improbable.io>
2018-11-22 15:14:29 +0000

    Tidy up test

--- keanu-project/src/test/java/io/improbable/keanu/e2e/regression/RadonHeirarchicalRegression.java
+++ keanu-project/src/test/java/io/improbable/keanu/e2e/regression/RadonHeirarchicalRegression.java
@@ -1,13 +1,17 @@
 package io.improbable.keanu.e2e.regression;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
-import io.improbable.keanu.algorithms.variational.optimizer.gradient.GradientOptimizer;
+import io.improbable.keanu.algorithms.NetworkSamples;
+import io.improbable.keanu.algorithms.mcmc.MetropolisHastings;
+import io.improbable.keanu.algorithms.mcmc.proposal.GaussianProposalDistribution;
+import io.improbable.keanu.algorithms.mcmc.proposal.ProposalDistribution;
 import io.improbable.keanu.model.regression.RegressionModel;
 import io.improbable.keanu.model.regression.RegressionRegularization;
 import io.improbable.keanu.network.BayesianNetwork;
@@ -21,46 +25,48 @@ import io.improbable.keanu.vertices.dbl.probabilistic.HalfCauchyVertex;
 public class RadonHeirarchicalRegression {
 
     @Test
-    public void doesHeirarchical() {
+    public void heirarchicalLinearRegressionRadon() {
         List<Data> radonData = ReadCsv.fromResources("data/datasets/radon/radon.csv")
             .asRowsDefinedBy(Data.class)
             .load(true);
 
-        Map<String, List<Data>> countryRadon = new HashMap<>();
+        Map<String, List<Data>> countryRadon = partitionDataOnCounty(radonData);
 
-        for (Data data : radonData) {
-            countryRadon.computeIfAbsent(data.county, k -> new ArrayList<>()).add(data);
-        }
+        BayesianNetwork bayesianNetwork = buildHeirarchicalNetwork(countryRadon);
 
-        BayesianNetwork bayesianNetwork = buildNetwork(countryRadon);
-        bayesianNetwork.probeForNonZeroProbability(500);
-        System.out.println("optimising...");
-        GradientOptimizer.of(bayesianNetwork).maxAPosteriori();
-        System.out.println("optimised");
         Vertex muAlpha = bayesianNetwork.getVertexByLabel(new VertexLabel("MuAlpha"));
         Vertex muBeta = bayesianNetwork.getVertexByLabel(new VertexLabel("MuBeta"));
 
-        System.out.println("Gradient then y");
-        System.out.println(muBeta.getValue());
-        System.out.println(muAlpha.getValue());
-//
-//        ProposalDistribution proposalDistribution = new GaussianProposalDistribution(DoubleTensor.scalar(100.));
-//
-//        NetworkSamples posteriorSamples = MetropolisHastings.builder().proposalDistribution(proposalDistribution).build().
-//            getPosteriorSamples(
-//            bayesianNetwork,
-//            Arrays.asList(muAlpha, muBeta),
-//            75000
-//        ).drop(5000);
-//
-//        double averagePosteriorA = posteriorSamples.getDoubleTensorSamples(muAlpha).getAverages().scalar();
-//        double averagePosteriorB = posteriorSamples.getDoubleTensorSamples(muBeta).getAverages().scalar();
-
-//        System.out.println("Alpha: " + averagePosteriorA);
-//        System.out.println("Beta: " + averagePosteriorB);
+        sample(bayesianNetwork, muAlpha, muBeta);
     }
 
-    private BayesianNetwork buildNetwork(Map<String, List<Data>> countryRadon) {
+    private void sample(BayesianNetwork bayesianNetwork, Vertex muAlpha, Vertex muBeta) {
+        NetworkSamples posteriorSamples = MetropolisHastings.builder().build().
+            getPosteriorSamples(
+                bayesianNetwork,
+                Arrays.asList(muAlpha, muBeta),
+                50000
+            ).drop(5000)
+            .downSample(bayesianNetwork.getContinuousLatentVertices().size());
+
+        double averagePosteriorA = posteriorSamples.getDoubleTensorSamples(muAlpha).getAverages().scalar();
+        double averagePosteriorB = posteriorSamples.getDoubleTensorSamples(muBeta).getAverages().scalar();
+
+        System.out.println("Alpha: " + averagePosteriorA);
+        System.out.println("Beta: " + averagePosteriorB);
+
+    }
+
+    private Map<String, List<Data>> partitionDataOnCounty(List<Data> radonData) {
+        Map<String, List<Data>> countyRadon = new HashMap<>();
+
+        for (Data data : radonData) {
+            countyRadon.computeIfAbsent(data.county, k -> new ArrayList<>()).add(data);
+        }
+        return countyRadon;
+    }
+
+    private BayesianNetwork buildHeirarchicalNetwork(Map<String, List<Data>> countryRadon) {
         GaussianVertex muAlpha = new GaussianVertex(0, 100).setLabel("MuAlpha");
         GaussianVertex muBeta = new GaussianVertex(0, 100).setLabel("MuBeta");
 
