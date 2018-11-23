9cefe98e253f417dc844bffdbf3f227f34db1585
Michael Lewin <michaellewin@improbable.io>
2018-11-23 16:38:28 +0000

    temporarily ignore pd.read_csv until we stub pandas io

--- keanu-python/examples/coal_mining.py
+++ keanu-python/examples/coal_mining.py
@@ -8,7 +8,7 @@ class CoalMining():
     __fname = "data/coal-mining-disaster-data.csv"
 
     def __init__(self) -> None:
-        self._data: pd.DataFrame = pd.read_csv(CoalMining.__fname, names=["year", "count"]).set_index("year")
+        self._data: pd.DataFrame = pd.read_csv(CoalMining.__fname, names=["year", "count"]).set_index("year") # type: ignore # TODO: stub pandas io
 
     def model(self) -> Model:
         start_year, end_year = (self._data.index.min(), self._data.index.max())
