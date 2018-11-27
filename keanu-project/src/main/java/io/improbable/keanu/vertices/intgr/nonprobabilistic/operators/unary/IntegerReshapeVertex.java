7b0bec1770741507cfdc32e4e9633eec18bdb10d
Chris North <christophernorth@improbable.io>
2018-11-27 15:04:10 +0000

    Final vertex annotation for now

--- keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerReshapeVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/intgr/nonprobabilistic/operators/unary/IntegerReshapeVertex.java
@@ -1,16 +1,26 @@
 package io.improbable.keanu.vertices.intgr.nonprobabilistic.operators.unary;
 
 import io.improbable.keanu.tensor.intgr.IntegerTensor;
-import io.improbable.keanu.vertices.NonSaveableVertex;
+import io.improbable.keanu.vertices.LoadVertexParam;
+import io.improbable.keanu.vertices.SaveVertexParam;
 import io.improbable.keanu.vertices.intgr.IntegerVertex;
 
-public class IntegerReshapeVertex extends IntegerUnaryOpVertex implements NonSaveableVertex {
-    public IntegerReshapeVertex(IntegerVertex inputVertex, long... proposedShape) {
+public class IntegerReshapeVertex extends IntegerUnaryOpVertex {
+
+    private static final String SHAPE_NAME = "proposedShape";
+
+    public IntegerReshapeVertex(@LoadVertexParam(INPUT_NAME) IntegerVertex inputVertex,
+                                @LoadVertexParam(SHAPE_NAME) long... proposedShape) {
         super(proposedShape, inputVertex);
     }
 
     @Override
     protected IntegerTensor op(IntegerTensor value) {
         return value.reshape(getShape());
+    }
+
+    @SaveVertexParam(SHAPE_NAME)
+    public long[] getShapeParam() {
+        return getShape();
     }
 }
