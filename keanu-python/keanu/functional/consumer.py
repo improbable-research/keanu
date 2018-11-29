a534ec73b61ff91ddfeff29e4553e293f4a2da11
Michael Lewin <michaellewin@improbable.io>
2018-11-29 17:03:40 +0000

    annotated consumer, supplier, function

--- keanu-python/keanu/functional/consumer.py
+++ keanu-python/keanu/functional/consumer.py
@@ -1,9 +1,14 @@
+from typing import Callable
+
+from py4j.java_gateway import JavaObject
+
+
 class Consumer:
 
-    def __init__(self, lambda_function):
+    def __init__(self, lambda_function: Callable) -> None:
         self.lambda_function = lambda_function
 
-    def accept(self, arg):
+    def accept(self, arg: JavaObject) -> None:
         self.lambda_function(arg)
 
     class Java:
--- keanu-python/keanu/functional/supplier.py
+++ keanu-python/keanu/functional/supplier.py
@@ -1,9 +1,14 @@
+from typing import Callable
+
+from py4j.java_gateway import JavaObject
+
+
 class Supplier:
 
-    def __init__(self, lambda_function):
+    def __init__(self, lambda_function: Callable):
         self.lambda_function = lambda_function
 
-    def get(self):
+    def get(self) -> JavaObject:
         return self.lambda_function()
 
     class Java:
