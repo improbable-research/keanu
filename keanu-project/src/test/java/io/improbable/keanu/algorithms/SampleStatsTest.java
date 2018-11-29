9f2db9bdb4c30804400822d347911b0269e9583e
Wesley Williams <wesley@improbable.io>
2018-11-29 12:07:11 +0000

    Add python code for generating expected

--- keanu-project/src/test/java/io/improbable/keanu/algorithms/SampleStatsTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/algorithms/SampleStatsTest.java
@@ -12,25 +12,31 @@ import static org.junit.Assert.assertThat;
 
 public class SampleStatsTest {
     @Test
-    public void randomlyGeneratedSamplesHaveCloseToZeroAutocorrelation() {
-        ContinuousDistribution uniform = Uniform.withParameters(DoubleTensor.ZERO_SCALAR,DoubleTensor.scalar(1000));
-        uniform.sample(new long[]{100}, KeanuRandom.getDefaultRandom());
-        double samples[] = new double[100];
-        for(int  i = 0; i < 100; i++) {
-            samples[i] = uniform.sample(new long[]{1},KeanuRandom.getDefaultRandom()).scalar();
+    public void randomlyGeneratedSamplesHaveCloseToZeroAutocorrelationAtLowLags() {
+        ContinuousDistribution uniform = Uniform.withParameters(DoubleTensor.ZERO_SCALAR, DoubleTensor.scalar(1000));
+        uniform.sample(new long[]{500}, KeanuRandom.getDefaultRandom());
+        double samples[] = new double[500];
+        for (int i = 0; i < 500; i++) {
+            samples[i] = uniform.sample(new long[]{1}, KeanuRandom.getDefaultRandom()).scalar();
         }
         double[] autocorr = SampleStats.acf(samples);
 
-        assertThat(autocorr[80],lessThan(0.1));
+        assertThat(autocorr[80], lessThan(0.1));
     }
 
+    /*
+      import statsmodels.api as sm
+      import numpy as np
+      x = np.array([1,2,3,4,5,6,7,8])
+      print(sm.tsa.stattools.acf(x))
+    */
     @Test
     public void increasingSequenceReturnsCorrectAutocorr() {
-        double[] samples = {1,2,3,4,5,6,7,8};
+        double[] samples = {1, 2, 3, 4, 5, 6, 7, 8};
         double[] autocorr = SampleStats.acf(samples);
-        double[] expected = {1.        ,  0.625     ,  0.27380952, -0.0297619 , -0.26190476,
+        double[] expected = {1., 0.625, 0.27380952, -0.0297619, -0.26190476,
             -0.39880952, -0.41666667, -0.29166667};
-        assertArrayEquals(expected,autocorr,0.0001);
+        assertArrayEquals(expected, autocorr, 0.0001);
 
     }
 
