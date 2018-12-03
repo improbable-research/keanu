7bbf91a3665aca1939f892a10e61abff802270e7
Joanne Hong <joannehong@improbable.io>
2018-12-03 14:02:31 +0000

    Try to resolve timeout issue. Go back to previous backend.

--- keanu-python/keanu/plots/traceplot.py
+++ keanu-python/keanu/plots/traceplot.py
@@ -1,5 +1,5 @@
 import matplotlib
-matplotlib.use('agg')  # see: https://matplotlib.org/faq/usage_faq.html#what-is-a-backend
+matplotlib.use('PS')  # see: https://matplotlib.org/faq/usage_faq.html#what-is-a-backend
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
