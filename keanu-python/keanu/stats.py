766c809dd647a8db6358da7a25c83e3e416c2cfc
Wesley Williams <wesley@improbable.io>
2018-12-04 16:49:00 +0000

    More robust autocorr

--- keanu-python/keanu/stats.py
+++ keanu-python/keanu/stats.py
@@ -1,16 +1,21 @@
-from py4j.java_gateway import java_import
-from .context import KeanuContext
-from numpy import ndarray, fromiter
-import numpy as np
 from typing import List
+
+from numpy import ndarray, fromiter, issubdtype, hstack, floating
+from py4j.java_gateway import java_import
+
+from keanu.vartypes import numpy_types
+from .context import KeanuContext
 
 k = KeanuContext()
 
 java_import(k.jvm_view(), "io.improbable.keanu.algorithms.SampleStats")
 
-def autocorrelation(arg: List[ndarray]) -> ndarray:
-    arg_array = np.hstack(arg).astype(float)
-    if arg_array.ndim != 1:
-        raise ValueError("Autocorrelation must be run on a list of dimension 1 ndarrays.")
+
+def autocorrelation(arg: List[numpy_types]) -> ndarray:
+    if not issubdtype(arg[0].dtype, floating):
+        raise ValueError('Autocorrelation must be run on a list of numpy floating types.')
+    if not arg[0].shape == ():
+        raise ValueError("Autocorrelation must be run on a list of single element ndarrays.")
+    arg_array = hstack(arg)
     autocorr = k.jvm_view().SampleStats.autocorrelation(k.to_java_array(arg_array))
-    return fromiter(autocorr, float)
+    return fromiter(autocorr, arg[0].dtype)
--- keanu-python/tests/test_stats.py
+++ keanu-python/tests/test_stats.py
@@ -1,11 +1,43 @@
-import pytest
-from keanu import stats
 import numpy as np
+import pytest
+
+from keanu.vertex import Uniform
+from keanu.algorithm import sample
+from keanu import Model, stats
+
 
 def test_can_get_autocorrelation():
-    x = np.arange(1,9)
-    x_list = [np.array(a) for a in x]
+    x = np.arange(1, 9)
+    x_list = [np.array(a, float) for a in x]
     autocorr = stats.autocorrelation(x_list)
-    expected = [1., 0.625, 0.27380952, -0.0297619, -0.26190476,
-            -0.39880952, -0.41666667, -0.29166667]
-    np.testing.assert_almost_equal(autocorr,expected)
+    expected = [1., 0.625, 0.27380952, -0.0297619, -0.26190476, -0.39880952, -0.41666667, -0.29166667]
+    np.testing.assert_almost_equal(autocorr, expected)
+
+
+def test_can_get_autocorrelation_for_samples():
+    with Model() as m:
+        m.gaussian = Uniform(0, 1000)
+    net = m.to_bayes_net()
+    samples = sample(net=net, sample_from=list(net.get_latent_vertices()), algo="metropolis", draws=1000)
+    valid_key = list(samples.keys())[0]
+    autocorr = stats.autocorrelation(samples.get(valid_key))
+
+
+def test_cant_get_autocorrelation_of_np_bools():
+    x = [True, False, False]
+    x_list = [np.array(a) for a in x]
+    with pytest.raises(ValueError, match="Autocorrelation must be run on a list of numpy floating types."):
+        stats.autocorrelation(x_list)
+
+
+def test_cant_get_autocorrelation_of_np_ints():
+    x = [1, 2, 3]
+    x_list = [np.array(a, int) for a in x]
+    with pytest.raises(ValueError, match="Autocorrelation must be run on a list of numpy floating types."):
+        stats.autocorrelation(x_list)
+
+
+def test_cant_get_autocorrelation_of_non_scalar_arrays():
+    x_list = [np.array([22.4, 33.3]), np.array([15.4, 11.3])]
+    with pytest.raises(ValueError, match="Autocorrelation must be run on a list of single element ndarrays."):
+        stats.autocorrelation(x_list)
