9418a5c405e022e1fb135b2594faad7a2a75ebbf
Wesley Williams <wesley@improbable.io>
2018-12-05 15:37:58 +0000

    Check samples isn't empty in VertexSamples constructor

--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/DoubleVertexSamples.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/DoubleVertexSamples.java
@@ -17,17 +17,12 @@ public class DoubleVertexSamples extends VertexSamples<DoubleTensor> {
     }
 
     public DoubleTensor getAverages() {
-        Preconditions.checkState(!samples.isEmpty(), "No samples exist for averaging.");
-        Preconditions.checkArgument(!samples.isEmpty(), "No samples provided.");
-
         return this.samples.stream()
             .reduce(DoubleTensor.zeros(sampleShape), DoubleTensor::plusInPlace)
             .divInPlace(samples.size());
     }
 
     public DoubleTensor getVariances() {
-        Preconditions.checkState(!samples.isEmpty(), "No samples exist for averaging.");
-
         DoubleTensor sumOfSquares = this.samples.stream()
             .reduce(DoubleTensor.zeros(sampleShape), (l, r) -> l.plusInPlace(r.pow(2)));
         return sumOfSquares
@@ -44,7 +39,6 @@ public class DoubleVertexSamples extends VertexSamples<DoubleTensor> {
      * @return A tensor of autocorrelation at different lags.
      */
     public DoubleTensor getAutocorrelation(long... index) {
-        Preconditions.checkState(!samples.isEmpty(), "No samples exist for averaging.");
         TensorShapeValidation.checkIndexIsValid(sampleShape, index);
 
         double[] sampleValuesAtIndex = samples.stream()
