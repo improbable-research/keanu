698a8ea3e09730b4e21192d8b9f6125fed167873
Wesley Williams <wesley@improbable.io>
2018-12-06 13:43:44 +0000

    Add type annotations

--- keanu-python/keanu/shape_validation.py
+++ keanu-python/keanu/shape_validation.py
@@ -1,12 +1,12 @@
-from typing import Tuple, List
+from typing import Tuple, List, Union
 
-def check_index_is_valid(shape: Tuple[int], index: Tuple[int]):
+def check_index_is_valid(shape: Tuple[int, ...], index: Union[Tuple[int],Tuple]) -> None:
     if len(shape) != len(index):
         raise ValueError("Length of desired index {} must match the length of the shape {}.".format(index, shape))
     for i in range(len(index)):
       if index[i] >= shape[i]:
           raise ValueError("Invalid index {} for shape {}".format(index, shape))
 
-def check_all_shapes_match(shapes: List[Tuple[int]]):
+def check_all_shapes_match(shapes: List[Tuple[int, ...]]) -> None:
     if not len(set(shapes)) == 1:
         raise ValueError("Shapes must match")
--- keanu-python/keanu/stats.py
+++ keanu-python/keanu/stats.py
@@ -1,17 +1,17 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Union
 
-from numpy import ndarray, fromiter, issubdtype, hstack, floating, stack
+from numpy import ndarray, fromiter, issubdtype, floating, stack
 from py4j.java_gateway import java_import
 
-from keanu.vartypes import numpy_types
 from keanu.shape_validation import check_index_is_valid, check_all_shapes_match
+from keanu.vartypes import numpy_types
 from .context import KeanuContext
 
 k = KeanuContext()
 
 java_import(k.jvm_view(), "io.improbable.keanu.algorithms.SampleStats")
 
-def autocorrelation(arg: List[numpy_types], index: Tuple[int] = ()) -> ndarray:
+def autocorrelation(arg: List[numpy_types], index: Union[Tuple[int], Tuple] = ()) -> ndarray:
     check_all_floats(arg)
     check_all_shapes_match([elem.shape for elem in arg])
     check_index_is_valid(arg[0].shape, index)
@@ -19,7 +19,8 @@ def autocorrelation(arg: List[numpy_types], index: Tuple[int] = ()) -> ndarray:
     autocorr = k.jvm_view().SampleStats.autocorrelation(k.to_java_array(arg_array))
     return fromiter(autocorr, arg[0].dtype)
 
-def check_all_floats(arg: List[numpy_types]):
+
+def check_all_floats(arg: List[numpy_types]) -> None:
     all_floats = all(issubdtype(elem.dtype, floating) for elem in arg)
     if not all_floats:
         raise ValueError("Autocorrelation must be run on a list of numpy floating types.")
