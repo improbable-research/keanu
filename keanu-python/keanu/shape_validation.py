a9ea7816e528c0f1d548b0de70be865dbf157879
Wesley Williams <wesley@improbable.io>
2018-12-06 13:27:46 +0000

    Check all shapes match

--- keanu-python/keanu/shape_validation.py
+++ keanu-python/keanu/shape_validation.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 def check_index_is_valid(shape: Tuple[int], index: Tuple[int]):
     if len(shape) != len(index):
@@ -6,3 +6,7 @@ def check_index_is_valid(shape: Tuple[int], index: Tuple[int]):
     for i in range(len(index)):
       if index[i] >= shape[i]:
           raise ValueError("Invalid index {} for shape {}".format(index, shape))
+
+def check_all_shapes_match(shapes: List[Tuple[int]]):
+    if not len(set(shapes)) == 1:
+        raise ValueError("Shapes must match")
--- keanu-python/tests/test_shape_validation.py
+++ keanu-python/tests/test_shape_validation.py
@@ -1,4 +1,4 @@
-from keanu.shape_validation import check_index_is_valid
+from keanu.shape_validation import check_index_is_valid, check_all_shapes_match
 import pytest
 
 def test_doesnt_throw_on_valid_index():
@@ -15,3 +15,19 @@ def test_throws_on_index_shorter_than_shape():
 def test_throws_on_index_out_of_bounds():
     with pytest.raises(ValueError):
         check_index_is_valid((2, 2,6), (1,1,8))
+
+def test_doesnt_throw_on_matching_shapes():
+    shape = (3,3)
+    shape_list = [shape,shape,shape]
+    check_all_shapes_match(shape_list)
+
+def test_throws_on_non_mathcing_shapes():
+    shape_a = (3,4)
+    shape_b = (1,3)
+    shape_list = [shape_a,shape_a,shape_b]
+    with pytest.raises(ValueError):
+        check_all_shapes_match(shape_list)
+
+def test_throws_on_empty_list_of_shapes():
+    with pytest.raises(ValueError):
+        check_all_shapes_match([])
