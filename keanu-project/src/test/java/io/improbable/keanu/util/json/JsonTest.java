2839b7ed231072edc1ad660d4b1d904a40693607
ieva-improbable <ieva@improbable.io>
2018-12-07 17:32:41 +0000

    Add a test for full expected json output, rename other tests to make more sense.

--- keanu-project/src/test/java/io/improbable/keanu/util/json/JsonTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/util/json/JsonTest.java
@@ -1,5 +1,7 @@
 package io.improbable.keanu.util.json;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.google.protobuf.util.JsonFormat;
 import io.improbable.keanu.KeanuSavedBayesNet;
 import io.improbable.keanu.network.BayesianNetwork;
@@ -7,10 +9,13 @@ import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.nd4j.shade.jackson.databind.JsonNode;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,10 +24,11 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.collection.IsIn.isIn;
 import static org.hamcrest.core.Every.everyItem;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 public class JsonTest {
+
+    private static final String JSON_EXPECTED_OUTPUT_FILE = "jsonFiles/JsonModelOutput.json";
 
     private static BayesianNetwork net;
     private static Map<String, String> someMetadata;
@@ -32,6 +38,7 @@ public class JsonTest {
     public static void setup() throws IOException {
         DoubleVertex gaussianVertex = new GaussianVertex(0.0, 1.0);
         gaussianVertex.observe(0.5);
+        gaussianVertex.setLabel("GaussianVertex");
         net = new BayesianNetwork(gaussianVertex.getConnectedGraph());
 
         output = new ByteArrayOutputStream();
@@ -40,12 +47,22 @@ public class JsonTest {
         someMetadata.put("Author", "Some Author");
         someMetadata.put("Tag", "MyBayesNet");
 
-        JsonSaver jasonSaver = new JsonSaver(net, someMetadata);
-        jasonSaver.save(output, true);
+        JsonSaver jsonSaver = new JsonSaver(net, someMetadata);
+        jsonSaver.save(output, true);
     }
 
     @Test
-    public void metadataIsSavedForJsonModels() throws IOException {
+    public void jsonSaverCreatesExpectedJsonOutput() throws IOException {
+        URL url = Resources.getResource(JSON_EXPECTED_OUTPUT_FILE);
+        String expectedOutput = Resources.toString(url, Charsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode expectedJsonObject = mapper.readTree(expectedOutput);
+        JsonNode actualJsonObject = mapper.readTree(output.toString());
+        assertEquals(expectedJsonObject, actualJsonObject);
+    }
+
+    @Test
+    public void jsonSaverSavesMetadata() throws IOException {
         KeanuSavedBayesNet.Metadata.Builder metadataBuilder = KeanuSavedBayesNet.Metadata.newBuilder();
         for (Map.Entry<String, String> entry : someMetadata.entrySet()) {
             metadataBuilder.putMetadataInfo(entry.getKey(), entry.getValue());
@@ -55,16 +72,16 @@ public class JsonTest {
         KeanuSavedBayesNet.Model parsedModel = modelBuilder.build();
 
         assertTrue(parsedModel.hasMetadata());
+        assertEquals(parsedModel.getMetadata().getMetadataInfoMap().size(), (metadataBuilder.getMetadataInfoMap().size()));
         assertThat(parsedModel.getMetadata().getMetadataInfoMap().entrySet(), everyItem(isIn(metadataBuilder.getMetadataInfoMap().entrySet())));
     }
 
     @Test
-    public void modelsCanBeSavedAsJsonAndReadBackIn() throws IOException {
+    public void modelCanBeLoadedFromJson() throws IOException {
         ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
         JsonLoader jsonLoader = new JsonLoader();
         BayesianNetwork loadedNetwork = jsonLoader.loadNetwork(input);
 
-        assertNotEquals(net, loadedNetwork);
         assertEquals(net.getAllVertices().size(), loadedNetwork.getAllVertices().size());
         GaussianVertex gaussianVertex = (GaussianVertex)loadedNetwork.getLatentVertices().get(0);
         assertThat(gaussianVertex.getMu().getValue().scalar(), closeTo(0.0, 1e-10));
