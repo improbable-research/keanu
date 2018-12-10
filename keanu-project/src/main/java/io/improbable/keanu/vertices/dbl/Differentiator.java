7618f5f86898b4cdc38a22a64531bc8cf14cae30
Caleb <caleb@improbable.io>
2018-12-10 20:17:04 +0000

    revert change in differentiator to pluck diffs after reverse mode traverse

--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/Differentiator.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/Differentiator.java
@@ -34,9 +34,15 @@ public class Differentiator {
         Map<Vertex, PartialDerivatives> dwrtOf = new HashMap<>();
         collectPartials(singletonMap(ofVertex, dWrtOfVertex), dwrtOf, dOfShape);
 
+        Map<VertexId, PartialDerivatives> wrtOf = new HashMap<>();
 
         Vertex<?> visiting;
         while ((visiting = priorityQueue.poll()) != null) {
+
+            if (wrt.contains(visiting)) {
+                wrtOf.put(visiting.getId(), dwrtOf.get(visiting));
+                continue;
+            }
 
             if (visiting instanceof Differentiable) {
                 Differentiable visitingDifferentiable = ((Differentiable) visiting);
@@ -51,13 +57,6 @@ public class Differentiator {
                         alreadyQueued.add(parent);
                     }
                 }
-            }
-        }
-
-        Map<VertexId, PartialDerivatives> wrtOf = new HashMap<>();
-        for (Vertex vertex : wrt) {
-            if (dwrtOf.containsKey(vertex)) {
-                wrtOf.put(vertex.getId(), dwrtOf.get(vertex));
             }
         }
 
--- keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/PlaceHolderDoubleVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/PlaceHolderDoubleVertex.java
@@ -2,10 +2,11 @@ package io.improbable.keanu.vertices.dbl.nonprobabilistic;
 
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.NonProbabilistic;
+import io.improbable.keanu.vertices.dbl.Differentiable;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
 
-public class PlaceHolderDoubleVertex extends DoubleVertex implements NonProbabilistic<DoubleTensor> {
+public class PlaceHolderDoubleVertex extends DoubleVertex implements NonProbabilistic<DoubleTensor>, Differentiable {
 
     public PlaceHolderDoubleVertex(long[] initialShape) {
         super(initialShape);
