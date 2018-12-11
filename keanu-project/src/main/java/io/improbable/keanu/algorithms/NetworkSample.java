1c6dccc9962ef20c5693e6a97ffee102a6bcdcc6
Wesley Williams <wesley@improbable.io>
2018-12-11 11:31:48 +0000

    add javadoc

--- keanu-project/src/main/java/io/improbable/keanu/algorithms/NetworkSample.java
+++ keanu-project/src/main/java/io/improbable/keanu/algorithms/NetworkSample.java
@@ -3,31 +3,39 @@ package io.improbable.keanu.algorithms;
 import io.improbable.keanu.network.NetworkState;
 import io.improbable.keanu.vertices.Vertex;
 import io.improbable.keanu.vertices.VertexId;
-import lombok.Getter;
+import lombok.Value;
 
 import java.util.Set;
 
+/**
+ * A network sample contains the state of the network (vertex values) and the logOfMasterP at
+ * a given point in time.
+ */
+@Value
 public class NetworkSample {
 
-    @Getter
     private final NetworkState networkState;
-    @Getter
     private final double logOfMasterP;
 
+    /**
+     * @param vertex the vertex to get the values of
+     * @param <T> the type of the values that the vertex contains
+     * @return the values of the specified vertex
+     */
     public <T> T get(Vertex<T> vertex) {
         return networkState.get(vertex);
     }
 
+    /**
+     * @param vertexId the ID of the vertex to get the values of
+     * @param <T> the type of the values that the vertex contains
+     * @return the values of the specified vertex
+     */
     public <T> T get(VertexId vertexId) {
         return networkState.get(vertexId);
     }
 
     public Set<VertexId> getVertexIds() {
         return networkState.getVertexIds();
-    }
-
-    public NetworkSample(NetworkState networkState, double logOfMasterP) {
-        this.networkState = networkState;
-        this.logOfMasterP = logOfMasterP;
     }
 }
