1026102ef0c95eda94e8ece6972398ac38838276
Michael Lewin <michaellewin@improbable.io>
2018-12-11 09:41:30 +0000

    added first spotless rule for python: no base logging

--- keanu-python/keanu/base.py
+++ keanu-python/keanu/base.py
@@ -23,7 +23,8 @@ class JavaObjectWrapper:
             raise AttributeError("{} has no attribute {}".format(self.__class__, k))
 
         java_name = _to_camel_case_name(k)
-        logging.warning("\"{}\" is not implemented so Java API \"{}\" was called directly instead".format(k, java_name))
+        logging.getLogger("keanu").warning(
+            "\"{}\" is not implemented so Java API \"{}\" was called directly instead".format(k, java_name))
         return self.unwrap().__getattr__(java_name)
 
     def unwrap(self) -> JavaObject:
