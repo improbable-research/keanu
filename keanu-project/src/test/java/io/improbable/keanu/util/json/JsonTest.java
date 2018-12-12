19a89f750e312a093ab219c6d490f87e569a76d2
ieva-improbable <ieva@improbable.io>
2018-12-12 15:01:33 +0000

    Extend the json tests (heavily inspired by the protobuf tests). Currently fails due to a proto bug.

--- keanu-project/src/test/java/io/improbable/keanu/util/json/JsonTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/util/json/JsonTest.java
@@ -8,9 +8,12 @@ import io.improbable.keanu.KeanuSavedBayesNet;
 import io.improbable.keanu.network.BayesianNetwork;
 import io.improbable.keanu.vertices.VertexId;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.nd4j.shade.jackson.databind.JsonNode;
 import org.nd4j.shade.jackson.databind.ObjectMapper;
 
@@ -20,6 +23,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Map;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.collection.IsIn.isIn;
@@ -30,16 +34,27 @@ import static org.junit.Assert.assertTrue;
 public class JsonTest {
 
     private static final String JSON_EXPECTED_OUTPUT_FILE = "jsonFiles/JsonModelOutput.json";
+    private static final String JSON_INVALID_VERTEX_TYPE_FILE = "jsonFiles/InvalidVertexType.json";
+    private static final String JSON_MISSING_PARENT_VERTEX_FILE = "jsonFiles/ParentVertexMissing.json";
+    private static final String JSON_MISSING_HYPERPARAMETER_FILE = "jsonFiles/HyperparameterSpecificationMissing.json";
+    private static final String JSON_WRONG_ARGUMENT_TYPE_FILE = "jsonFiles/WrongArgumentType.json";
+    private static final String JSON_INCOMPATIBLE_LABELS_FILE = "jsonFiles/IncompatibleLabels.json";
+    private static final String JSON_INCOMPATIBLE_VALUE_FILE = "jsonFiles/IncompatibleValueSpecified.json";
 
     private static BayesianNetwork net;
     private static Map<String, String> someMetadata;
     private static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @BeforeClass
     public static void setup() throws IOException {
         VertexId.ID_GENERATOR.set(0);
 
-        DoubleVertex gaussianVertex = new GaussianVertex(0.0, 1.0);
+        DoubleVertex mu = new ConstantDoubleVertex(0);
+        DoubleVertex sigma = new ConstantDoubleVertex(new double[]{3.0, 4.0});
+        DoubleVertex gaussianVertex = new GaussianVertex(mu, sigma);
         gaussianVertex.observe(0.5);
         gaussianVertex.setLabel("GaussianVertex");
         net = new BayesianNetwork(gaussianVertex.getConnectedGraph());
@@ -81,9 +96,64 @@ public class JsonTest {
         BayesianNetwork loadedNetwork = jsonLoader.loadNetwork(input);
 
         assertEquals(net.getAllVertices().size(), loadedNetwork.getAllVertices().size());
+        assertEquals(net.getLatentVertices().size(), loadedNetwork.getLatentVertices().size());
+        assertEquals(net.getObservedVertices().size(), loadedNetwork.getObservedVertices().size());
+        assertThat(net.getLatentVertices().get(0), instanceOf(GaussianVertex.class));
         GaussianVertex gaussianVertex = (GaussianVertex)loadedNetwork.getLatentVertices().get(0);
         assertThat(gaussianVertex.getMu().getValue().scalar(), closeTo(0.0, 1e-10));
         assertThat(gaussianVertex.getSigma().getValue().scalar(), closeTo(1.0, 1e-10));
         assertThat(gaussianVertex.getValue().scalar(), closeTo(0.5, 1e-10));
+    }
+
+    @Test
+    public void loadFailsIfInvalidVertexSpecified() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Unknown Vertex Type Specified: io.improbable.keanu.vertices.dbl.nonprobabilistic.NonExistentVertexType");
+        tryLoadingNetwork(JSON_INVALID_VERTEX_TYPE_FILE);
+    }
+
+    @Test
+    public void loadFailsIfParentVertexInfoIsMissing() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Failed to create vertex due to missing parent: mu");
+        tryLoadingNetwork(JSON_MISSING_PARENT_VERTEX_FILE);
+    }
+
+    @Test
+    public void loadFailsIfHyperparameterIsMissing() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Failed to create vertex due to missing parent: mu");
+        tryLoadingNetwork(JSON_MISSING_HYPERPARAMETER_FILE);
+    }
+
+    @Test
+    public void loadFailsIfWrongArgumentTypeSpecified() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Incorrect Parameter Type specified.  " +
+            "Got: class io.improbable.keanu.tensor.intgr.ScalarIntegerTensor, " +
+            "Expected: interface io.improbable.keanu.tensor.dbl.DoubleTensor");
+        tryLoadingNetwork(JSON_WRONG_ARGUMENT_TYPE_FILE);
+    }
+
+    @Test
+    public void loadFailsWithConflictingVertexInfoInValue() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Label and VertexID don't refer to same Vertex: (SomeLabel) " +
+            "(id: \"[2]\"\n)");
+        tryLoadingNetwork(JSON_INCOMPATIBLE_LABELS_FILE);
+    }
+
+    @Test
+    public void loadFailsIfValueIsWrongType() throws IOException {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Non Double Value specified for Double Vertex");
+        tryLoadingNetwork(JSON_INCOMPATIBLE_VALUE_FILE);
+    }
+
+    private void tryLoadingNetwork(String resourceFileName) throws IOException {
+        URL url = Resources.getResource(resourceFileName);
+        ByteArrayInputStream input = new ByteArrayInputStream(Resources.toByteArray(url));
+        JsonLoader jsonLoader = new JsonLoader();
+        BayesianNetwork loadedNetwork = jsonLoader.loadNetwork(input);
     }
 }
