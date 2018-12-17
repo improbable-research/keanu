1b506827ae42269e7582b70a32ac60c2560aca09
Michael Lewin <michaellewin@improbable.io>
2018-12-17 16:13:41 +0000

    added test that scalars are the expected shape

---
+++ benchmarks/src/test/java/io/improbable/keanu/benchmarks/Nd4jDoubleTensorTimesScalarTest.java
@@ -0,0 +1,18 @@
+package io.improbable.keanu.benchmarks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+public class Nd4jDoubleTensorTimesScalarTest {
+
+    @Test
+    public void theScalarsHaveTheExpectedRank() {
+        assertArrayEquals(Nd4jDoubleTensorTimesScalar.scalars[0].getShape(), new long[] {});
+        assertArrayEquals(Nd4jDoubleTensorTimesScalar.scalars[1].getShape(), new long[] {1});
+        assertArrayEquals(Nd4jDoubleTensorTimesScalar.scalars[2].getShape(), new long[] {1, 1});
+        assertArrayEquals(Nd4jDoubleTensorTimesScalar.scalars[3].getShape(), new long[] {});
+        assertEquals(Nd4jDoubleTensorTimesScalar.scalars.length, 4);
+    }
+}
