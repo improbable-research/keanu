da0009c80379d60f81bda5c782c23c442c3a7609
Wesley Williams <wesley@improbable.io>
2018-12-18 17:02:41 +0000

    missing underscore

--- keanu-python/keanu/plots/autocorrelation.py
+++ keanu-python/keanu/plots/autocorrelation.py
@@ -27,7 +27,7 @@ def __plot_corr(ax: Any, acf_x: ndarray, nlags: int, **kwargs: Any) -> None:
     ax.plot(acf_x[:nlags], **kwargs)
 
 
-def _calc_max_lag(data_len: int) -> int:
+def __calc_max_lag(data_len: int) -> int:
     lim = min(int(floor(10 * log(data_len, 10))), data_len - 1)
     return lim
 
@@ -36,6 +36,6 @@ def plot_acf(data: List[numpy_types], index: Tuple[int, ...] = (), nlags: int = 
     autocorr = stats.autocorrelation(data, index)
     fig, ax = __create_new_mpl()
     if nlags is None:
-        nlags = _calc_max_lag(len(autocorr))
+        nlags = __calc_max_lag(len(autocorr))
     __plot_corr(ax, autocorr, nlags)
     return fig
