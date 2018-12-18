db432fcdd776f1e77d942dc78a46d42557f2ec22
Wesley Williams <wesley@improbable.io>
2018-12-18 18:00:30 +0000

    Reformat code

--- docs/src/test/java/io/improbable/snippet/SelectVertexExample.java
+++ docs/src/test/java/io/improbable/snippet/SelectVertexExample.java
@@ -11,21 +11,21 @@ import static io.improbable.snippet.SelectVertexExample.MyType.*;
 
 public class SelectVertexExample {
 
-//%%SNIPPET_START%% VertexSelectVertexCode
-public enum MyType {
-    A, B, C, D
-}
+    //%%SNIPPET_START%% VertexSelectVertexCode
+    public enum MyType {
+        A, B, C, D
+    }
 
-public CategoricalVertex<MyType, GenericTensor<MyType>> getSelectorForMyType() {
+    public CategoricalVertex<MyType, GenericTensor<MyType>> getSelectorForMyType() {
 
-    LinkedHashMap<MyType, DoubleVertex> frequency = new LinkedHashMap<>();
-    frequency.put(A, new ConstantDoubleVertex(0.25));
-    frequency.put(B, new ConstantDoubleVertex(0.25));
-    frequency.put(C, new ConstantDoubleVertex(0.25));
-    frequency.put(D, new ConstantDoubleVertex(0.25));
+        LinkedHashMap<MyType, DoubleVertex> frequency = new LinkedHashMap<>();
+        frequency.put(A, new ConstantDoubleVertex(0.25));
+        frequency.put(B, new ConstantDoubleVertex(0.25));
+        frequency.put(C, new ConstantDoubleVertex(0.25));
+        frequency.put(D, new ConstantDoubleVertex(0.25));
 
-    return new CategoricalVertex<>(frequency);
-}
-//%%SNIPPET_END%% VertexSelectVertexCode
+        return new CategoricalVertex<>(frequency);
+    }
+    //%%SNIPPET_END%% VertexSelectVertexCode
 
 }
