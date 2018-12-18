95fd02493b7a727521d2df10155460ce9d1b0a44
George Nash <georgenash@improbable.io>
2018-12-18 14:07:21 +0000

    valid for master

--- deployment/buildAndDeploy.sh
+++ deployment/buildAndDeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH != 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
   ./gradlew clean build -x :keanu-python:build publishToNexus -PnexusUser=$NEXUS_USER -PnexusPassword=$NEXUS_PASSWORD -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=../deployment/secret-keys-keanu.gpg --info --stacktrace
 else
   ./gradlew clean build -x :keanu-python:build --info --stacktrace
