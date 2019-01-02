2c7a561239157529ec7ddf77a0697b3758f1089e
Chris North <christophernorth@improbable.io>
2019-01-02 12:11:29 +0000

    Updated Network IO code to use base classes that actually handle loading/saving

--- keanu-python/keanu/network_io.py
+++ keanu-python/keanu/network_io.py
@@ -1,4 +1,5 @@
 from py4j.java_gateway import java_import
+from py4j.java_gateway import JavaObject
 from .context import KeanuContext
 from .base import JavaObjectWrapper
 from .net import BayesNet
@@ -15,60 +16,55 @@ java_import(k.jvm_view(), "java.io.FileOutputStream")
 java_import(k.jvm_view(), "java.io.FileInputStream")
 
 
-def _save_network(saver, filename: str, save_values: bool = False, metadata: Optional[Dict[str, str]] = None):
-    output_stream = k.jvm_view().FileOutputStream(filename)
-    if metadata is None:
-        saver.unwrap().save(output_stream, save_values)
-    else:
-        saver.unwrap().save(output_stream, save_values, k.to_java_map(metadata))
+class NetworkSaver(JavaObjectWrapper):
 
-
-class ProtobufSaver(JavaObjectWrapper):
-
-    def __init__(self, net: BayesNet):
-        super(ProtobufSaver, self).__init__(k.jvm_view().ProtobufSaver(net.unwrap()))
+    def __init__(self, saver_object: JavaObject):
+        super().__init__(saver_object)
 
     def save(self, filename: str, save_values: bool = False, metadata: Optional[Dict[str, str]] = None):
-        _save_network(self, filename, save_values, metadata)
+        output_stream = k.jvm_view().FileOutputStream(filename)
+        if metadata is None:
+            self.unwrap().save(output_stream, save_values)
+        else:
+            self.unwrap().save(output_stream, save_values, k.to_java_map(metadata))
 
 
-class DotSaver(JavaObjectWrapper):
-
-    def __init__(self, net: BayesNet):
-        super(DotSaver, self).__init__(k.jvm_view().DotSaver(net.unwrap()))
-
-    def save(self, filename: str, save_values: bool = False, metadata: Optional[Dict[str, str]] = None):
-        _save_network(self, filename, save_values, metadata)
-
-
-class JsonSaver(JavaObjectWrapper):
+class ProtobufSaver(NetworkSaver):
 
     def __init__(self, net: BayesNet):
-        super(JsonSaver, self).__init__(k.jvm_view().JsonSaver(net.unwrap()))
-
-    def save(self, filename: str, save_values: bool = False, metadata: Optional[Dict[str, str]] = None):
-        _save_network(self, filename, save_values, metadata)
+        super().__init__(k.jvm_view().ProtobufSaver(net.unwrap()))
 
 
-def _load_network(loader, filename: str) -> BayesNet:
-    input_stream = k.jvm_view().FileInputStream(filename)
-    read_net = loader.unwrap().loadNetwork(input_stream)
-    return BayesNet(read_net.getAllVertices())
+class DotSaver(NetworkSaver):
+
+    def __init__(self, net: BayesNet):
+        super().__init__(k.jvm_view().DotSaver(net.unwrap()))
 
 
-class ProtobufLoader(JavaObjectWrapper):
+class JsonSaver(NetworkSaver):
 
-    def __init__(self) -> None:
-        super(ProtobufLoader, self).__init__(k.jvm_view().ProtobufLoader())
+    def __init__(self, net: BayesNet):
+        super().__init__(k.jvm_view().JsonSaver(net.unwrap()))
+
+
+class NetworkLoader(JavaObjectWrapper):
+
+    def __init__(self, loader_object: JavaObject):
+        super().__init__(loader_object)
 
     def load(self, filename: str) -> BayesNet:
-        return _load_network(self, filename)
+        input_stream = k.jvm_view().FileInputStream(filename)
+        read_net = self.unwrap().loadNetwork(input_stream)
+        return BayesNet(read_net.getAllVertices())
 
 
-class JsonLoader(JavaObjectWrapper):
+class ProtobufLoader(NetworkLoader):
 
     def __init__(self) -> None:
-        super(JsonLoader, self).__init__(k.jvm_view().JsonLoader())
+        super().__init__(k.jvm_view().ProtobufLoader())
 
-    def load(self, filename: str) -> BayesNet:
-        return _load_network(self, filename)
+
+class JsonLoader(NetworkLoader):
+
+    def __init__(self) -> None:
+        super().__init__(k.jvm_view().JsonLoader())
