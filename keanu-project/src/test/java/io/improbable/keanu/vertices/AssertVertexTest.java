f2b7955f4389090cb4610d7d7047aa4ae8b7467b
Wesley Williams <wesley@improbable.io>
2019-01-07 11:09:38 +0000

    Test optimizers work as intended with assert

--- keanu-project/src/test/java/io/improbable/keanu/vertices/AssertVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/AssertVertexTest.java
@@ -3,16 +3,26 @@ package io.improbable.keanu.vertices;
 import io.improbable.keanu.DeterministicRule;
 import io.improbable.keanu.algorithms.mcmc.MetropolisHastings;
 import io.improbable.keanu.algorithms.variational.optimizer.KeanuOptimizer;
+import io.improbable.keanu.algorithms.variational.optimizer.Optimizer;
+import io.improbable.keanu.algorithms.variational.optimizer.gradient.GradientOptimizer;
 import io.improbable.keanu.network.BayesianNetwork;
 import io.improbable.keanu.tensor.bool.BooleanTensor;
 import io.improbable.keanu.vertices.bool.BoolVertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.ConstantBoolVertex;
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertEquals;
+
 
 public class AssertVertexTest {
 
@@ -135,15 +145,58 @@ public class AssertVertexTest {
         UniformVertex temperature = new UniformVertex(20., 30.);
         temperature.lessThan(new ConstantDoubleVertex(30)).assertTrue();
         temperature.greaterThan(new ConstantDoubleVertex(20)).assertTrue();
+
         GaussianVertex firstThermometer = new GaussianVertex(temperature, 2.5);
         GaussianVertex secondThermometer = new GaussianVertex(temperature, 5.);
         firstThermometer.observe(25.);
         secondThermometer.observe(30.);
+
         BayesianNetwork bayesNet = new BayesianNetwork(temperature.getConnectedGraph());
         MetropolisHastings.withDefaultConfig().getPosteriorSamples(
             bayesNet,
             bayesNet.getLatentVertices(),
             100
         );
+    }
+
+    @Test
+    public void optimizerWithAssertThatShoudlntFire() {
+        UniformVertex temperature = new UniformVertex(20., 30.);
+        temperature.lessThan(new ConstantDoubleVertex(35)).assertTrue();
+        temperature.greaterThan(new ConstantDoubleVertex(15)).assertTrue();
+
+        GaussianVertex firstThermometer = new GaussianVertex(temperature, 2.5);
+        GaussianVertex secondThermometer = new GaussianVertex(temperature, 5.);
+        firstThermometer.observe(25.);
+        secondThermometer.observe(30.);
+
+        BayesianNetwork bayesNet = new BayesianNetwork(temperature.getConnectedGraph());
+        KeanuOptimizer.of(bayesNet).maxAPosteriori();
+        assertEquals(26, temperature.getValue().scalar(), 0.1);
+    }
+
+    @Test
+    public void canUseGradientOptimizerWithAssertVertex() {
+        DoubleVertex A = new GaussianVertex(20.0, 1.0);
+        DoubleVertex B = new GaussianVertex(20.0, 1.0);
+        A.setValue(21.5);
+        B.setAndCascade(21.5);
+
+        A.greaterThan(new ConstantDoubleVertex(20)).assertTrue();
+        B.greaterThan(new ConstantDoubleVertex(20)).assertTrue();
+
+        DoubleVertex Cobserved = new GaussianVertex(A.plus(B), 1.0);
+        Cobserved.observe(46.0);
+
+        BayesianNetwork bayesNet = new BayesianNetwork(Arrays.asList(A, B, Cobserved));
+        Optimizer optimizer = KeanuOptimizer.of(bayesNet);
+        assertThat(optimizer, instanceOf(GradientOptimizer.class));
+
+        optimizer.maxAPosteriori();
+        double maxA = A.getValue().scalar();
+        double maxB = B.getValue().scalar();
+
+        assertEquals(22, maxA, 0.1);
+        assertEquals(22, maxB, 0.1);
     }
 }
