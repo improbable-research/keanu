769422d8124218409a8cbc562be5950ca7942447
Wesley Williams <wesley@improbable.io>
2019-01-07 11:28:33 +0000

    Spotless

--- keanu-project/src/main/java/io/improbable/keanu/vertices/bool/BoolVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/bool/BoolVertex.java
@@ -6,7 +6,6 @@ import io.improbable.keanu.network.NetworkLoader;
 import io.improbable.keanu.network.NetworkSaver;
 import io.improbable.keanu.tensor.Tensor;
 import io.improbable.keanu.tensor.bool.BooleanTensor;
-import io.improbable.keanu.vertices.utility.AssertVertex;
 import io.improbable.keanu.vertices.Vertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.ConstantBoolVertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.binary.AndBinaryVertex;
@@ -20,6 +19,7 @@ import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.unary.BoolRe
 import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.unary.BoolSliceVertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.unary.BoolTakeVertex;
 import io.improbable.keanu.vertices.bool.nonprobabilistic.operators.unary.NotVertex;
+import io.improbable.keanu.vertices.utility.AssertVertex;
 
 import java.util.Arrays;
 import java.util.List;
