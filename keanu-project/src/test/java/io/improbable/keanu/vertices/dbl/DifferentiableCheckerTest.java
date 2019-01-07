0759d43ee1cab8476062c2b85e12e4e15456d061
Wesley Williams <wesley@improbable.io>
2019-01-07 17:32:34 +0000

    remove Path from name

--- keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/DifferentiableCheckerTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/DifferentiableCheckerTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class DifferentiablePathCheckerTest {
+public class DifferentiableCheckerTest {
 
     @Test
     public void simpleDiffable() {
