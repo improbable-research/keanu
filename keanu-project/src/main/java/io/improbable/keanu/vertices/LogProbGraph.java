6f65f6291856bb0c601544f5ed2d600ae4b1cf80
Joanne Hong <joannehong@improbable.io>
2019-01-08 11:01:59 +0000

    Add LogProbGraph.

---
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/LogProbGraph.java
@@ -0,0 +1,24 @@
+package io.improbable.keanu.vertices;
+
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Singular;
+
+import java.util.Map;
+
+@Builder
+public class LogProbGraph {
+
+    @Getter
+    @Singular
+    private final Map<Vertex<?>, Vertex<?>> inputs;
+
+    @Getter
+    private final DoubleVertex logProbOutput;
+
+    public <T> Vertex<T> getInput(Vertex<T> input) {
+        return (Vertex<T>) inputs.get(input);
+    }
+
+}
