d2664b9a05c55767f103de701cf6569a8eb3807e
Caleb <caleb@improbable.io>
2019-01-08 10:15:09 +0000

    clean up probabilistic graph converter to be way functional

--- keanu-project/src/main/java/io/improbable/keanu/backend/ProbabilisticGraphConverter.java
+++ keanu-project/src/main/java/io/improbable/keanu/backend/ProbabilisticGraphConverter.java
@@ -6,9 +6,10 @@ import io.improbable.keanu.vertices.LogProbGraph;
 import io.improbable.keanu.vertices.Vertex;
 import lombok.experimental.UtilityClass;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class ProbabilisticGraphConverter {
@@ -20,43 +21,34 @@ public class ProbabilisticGraphConverter {
         VariableReference priorLogProbReference = addLogProbCalculation(
             graphBuilder,
             network.getLatentVertices()
+        ).orElseThrow(() -> new IllegalArgumentException("Network must contain latent variables"));
+
+        Optional<VariableReference> logLikelihoodReference = addLogProbCalculation(
+            graphBuilder,
+            network.getObservedVertices()
         );
 
-        VariableReference logProbReference;
-        VariableReference logLikelihoodReference = null;
-
-        if (!network.getObservedVertices().isEmpty()) {
-            logLikelihoodReference = addLogProbCalculation(
-                graphBuilder,
-                network.getObservedVertices()
-            );
-
-            logProbReference = graphBuilder.add(logLikelihoodReference, priorLogProbReference);
-        } else {
-            logProbReference = priorLogProbReference;
-        }
+        VariableReference logProbReference = logLikelihoodReference
+            .map(ll -> graphBuilder.add(ll, priorLogProbReference))
+            .orElse(priorLogProbReference);
 
         graphBuilder.logProb(logProbReference);
-        graphBuilder.logLikelihood(logLikelihoodReference);
+        logLikelihoodReference.ifPresent(graphBuilder::logLikelihood);
 
         return graphBuilder.build();
     }
 
-    private static <T extends ProbabilisticGraph> VariableReference addLogProbCalculation(ProbabilisticGraphBuilder<T> graphBuilder,
-                                                                                          List<Vertex> probabilisticVertices) {
-        List<VariableReference> logProbOps = new ArrayList<>();
-
-        for (Vertex visiting : probabilisticVertices) {
-
-            if (visiting instanceof LogProbAsAGraphable) {
-                LogProbGraph logProbGraph = ((LogProbAsAGraphable) visiting).logProbGraph();
-                addLogProbFrom(logProbGraph, graphBuilder);
-                logProbOps.add(logProbGraph.getLogProbOutput().getReference());
-
-            } else {
-                throw new IllegalArgumentException("Vertex type " + visiting.getClass() + " logProb as a graph not supported");
-            }
-        }
+    private static <T extends ProbabilisticGraph> Optional<VariableReference> addLogProbCalculation(ProbabilisticGraphBuilder<T> graphBuilder,
+                                                                                                    List<Vertex> probabilisticVertices) {
+        List<VariableReference> logProbOps = probabilisticVertices.stream()
+            .map(visiting -> {
+                if (visiting instanceof LogProbAsAGraphable) {
+                    LogProbGraph logProbGraph = ((LogProbAsAGraphable) visiting).logProbGraph();
+                    return addLogProbGraph(logProbGraph, graphBuilder);
+                } else {
+                    throw new IllegalArgumentException("Vertex type " + visiting.getClass() + " logProb as a graph not supported");
+                }
+            }).collect(Collectors.toList());
 
         return addLogProbSumTotal(logProbOps, graphBuilder);
     }
@@ -65,30 +57,26 @@ public class ProbabilisticGraphConverter {
      * @param logProbGraph the graph to add that represents the logProb calculation
      * @param graphBuilder the builder that contains state for the probabilistic graph building so far.
      */
-    private static void addLogProbFrom(LogProbGraph logProbGraph,
-                                       ProbabilisticGraphBuilder graphBuilder) {
+    private static VariableReference addLogProbGraph(LogProbGraph logProbGraph,
+                                                     ProbabilisticGraphBuilder graphBuilder) {
 
         Map<Vertex<?>, Vertex<?>> inputs = logProbGraph.getInputs();
         graphBuilder.connect(inputs);
         graphBuilder.convert(((Vertex) logProbGraph.getLogProbOutput()).getConnectedGraph());
+        return logProbGraph.getLogProbOutput().getReference();
     }
 
-    private static <T extends ProbabilisticGraph> VariableReference addLogProbSumTotal(List<VariableReference> logProbOps,
-                                                                                       ProbabilisticGraphBuilder<T> graphBuilder) {
-
-        VariableReference totalLogProb = logProbOps.get(0);
-
-        if (logProbOps.size() == 1) {
-            return totalLogProb;
-        }
-
-        for (int i = 1; i < logProbOps.size() - 1; i++) {
-            VariableReference logProbContrib = logProbOps.get(i);
-            totalLogProb = graphBuilder.add(totalLogProb, logProbContrib);
-        }
-
-        VariableReference lastLogProbContrib = logProbOps.get(logProbOps.size() - 1);
-        return graphBuilder.add(totalLogProb, lastLogProbContrib);
+    private static <T extends ProbabilisticGraph> Optional<VariableReference> addLogProbSumTotal(List<VariableReference> logProbOps,
+                                                                                                 ProbabilisticGraphBuilder<T> graphBuilder) {
+        return logProbOps.stream()
+            .reduce((a, b) -> {
+                if (a == null) {
+                    return b;
+                } else {
+                    return graphBuilder.add(a, b);
+                }
+            });
     }
+
 
 }
