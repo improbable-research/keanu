80022318997e0c394474b960860c5ab8c5aff7de
Chris North <christophernorth@improbable.io>
2019-01-09 22:22:45 +0000

    Coal mining test failing on CI - increasing sample count to make it more resilient

--- keanu-python/tests/test_coal_mining.py
+++ keanu-python/tests/test_coal_mining.py
@@ -15,7 +15,7 @@ def test_coalmining() -> None:
     model.disasters.observe(coal_mining.training_data())
 
     net = BayesNet(model.switchpoint.get_connected_graph())
-    samples = sample(net=net, sample_from=net.get_latent_vertices(), draws=500, drop=100, down_sample_interval=5)
+    samples = sample(net=net, sample_from=net.get_latent_vertices(), draws=700, drop=100, down_sample_interval=5)
 
     vertex_samples: List[numpy_types] = samples["switchpoint"]
     vertex_samples_primitive: List[List[primitive_types]] = list(map(
