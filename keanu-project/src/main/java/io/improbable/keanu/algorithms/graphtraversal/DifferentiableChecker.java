2deff063a63d7065bfa64d554fcdd521aa0def4a
Wesley Williams <wesley@improbable.io>
2019-01-11 14:19:34 +0000

    Algorithm changes

--- keanu-project/src/main/java/io/improbable/keanu/algorithms/graphtraversal/DifferentiableChecker.java
+++ keanu-project/src/main/java/io/improbable/keanu/algorithms/graphtraversal/DifferentiableChecker.java
@@ -2,9 +2,11 @@ package io.improbable.keanu.algorithms.graphtraversal;
 
 import io.improbable.keanu.vertices.ConstantVertex;
 import io.improbable.keanu.vertices.Vertex;
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import lombok.experimental.UtilityClass;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -13,14 +15,31 @@ import java.util.Set;
 @UtilityClass
 public class DifferentiableChecker {
 
-    /**
-     * Checks whether a set of vertices are differentiable
-     * w.r.t the latents in a graph.
-     *
-     * @param vertices the collection of vertices to check
-     * @return true if the vertices are differentiable w.r.t latents
-     */
     public boolean isDifferentiable(Collection<Vertex> vertices) {
+        if(!allProbabilisticAreDoubleOrObserved(vertices)) {
+            return false;
+        }
+        Set<Vertex> allParents = getSetOfAllParents(vertices);
+        return diffableOrConstantUptoNextRV(allParents);
+    }
+    private boolean allProbabilisticAreDoubleOrObserved(Collection<Vertex> vertices) {
+        return vertices.stream().filter(Vertex::isProbabilistic)
+            .allMatch(DifferentiableChecker::isDoubleOrObserved);
+    }
+
+    private boolean isDoubleOrObserved(Vertex v) {
+        return (v instanceof DoubleVertex || v.isObserved());
+    }
+
+    public Set<Vertex> getSetOfAllParents(Collection<Vertex> vertices) {
+        Set<Vertex> allParents = new HashSet<>();
+        for (Vertex vertex : vertices) {
+            allParents.addAll(vertex.getParents());
+        }
+        return allParents;
+    }
+
+    private boolean diffableOrConstantUptoNextRV(Collection<Vertex> vertices) {
         Queue<Vertex> queue = new LinkedList<>(vertices);
         Set<Vertex> queued = new HashSet<>(vertices);
         Set<Vertex> constantValueVerticesCache = new HashSet<>();
@@ -32,10 +51,10 @@ public class DifferentiableChecker {
                 return false;
             }
 
-            if (visiting.isDifferentiable()) {
+            if (!visiting.isProbabilistic()) {
                 Collection<Vertex> nextVertices = visiting.getParents();
                 for (Vertex next : nextVertices) {
-                    if (!queued.contains(next) && !isValueKnownToBeConstant(next, constantValueVerticesCache)) {
+                    if (!queued.contains(next)) {
                         queue.offer(next);
                         queued.add(next);
                     }
@@ -46,21 +65,11 @@ public class DifferentiableChecker {
     }
 
     private boolean isNonDiffableAndNotConstant(Vertex vertex, Set<Vertex> constantValueVerticesCache) {
-        return !vertex.isDifferentiable() && !isVertexConstant(vertex, constantValueVerticesCache);
+        return !vertex.isDifferentiable() && !isVertexValueConstant(vertex, constantValueVerticesCache);
     }
 
-    private boolean isVertexConstant(Vertex vertex, Set<Vertex> constantValueVerticesCache) {
-        if (vertex.isProbabilistic() && !vertex.isObserved()) {
-            return false;
-        }
-        if (!isVertexParentsValueConstant(vertex, constantValueVerticesCache)) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean isVertexParentsValueConstant(Vertex vertex, Set<Vertex> constantValueVerticesCache) {
-        Collection<Vertex> initialNext = vertex.getParents();
+    private boolean isVertexValueConstant(Vertex vertex, Set<Vertex> constantValueVerticesCache) {
+        Collection<Vertex> initialNext = Collections.singletonList(vertex);
         Queue<Vertex> queue = new LinkedList<>(initialNext);
         Set<Vertex> queued = new HashSet<>(initialNext);
 
@@ -71,7 +80,7 @@ public class DifferentiableChecker {
                 return false;
             }
 
-            if (!isValueKnownToBeConstant(vertex, constantValueVerticesCache)) {
+            if (!isValueKnownToBeConstant(visiting, constantValueVerticesCache)) {
                 Collection<Vertex> nextVertices = visiting.getParents();
                 for (Vertex next : nextVertices) {
                     if (!queued.contains(next)) {
