d00b96f56b9f0d01a3227dfcca26435288cbf813
Michael Lewin <michaellewin@improbable.io>
2019-01-15 15:46:22 +0000

    renamed Model to PredictiveModel

--- keanu-project/src/main/java/io/improbable/keanu/model/regression/RegressionModel.java
+++ keanu-project/src/main/java/io/improbable/keanu/model/regression/RegressionModel.java
@@ -1,6 +1,6 @@
 package io.improbable.keanu.model.regression;
 
-import io.improbable.keanu.model.Model;
+import io.improbable.keanu.model.PredictiveModel;
 import io.improbable.keanu.model.ModelFitter;
 import io.improbable.keanu.tensor.bool.BooleanTensor;
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
@@ -22,7 +22,7 @@ import java.util.function.Function;
  *      .build();
  * </pre>
  */
-public class RegressionModel<OUTPUT> implements Model<DoubleTensor, OUTPUT> {
+public class RegressionModel<OUTPUT> implements PredictiveModel<DoubleTensor, OUTPUT> {
     private static final double DEFAULT_OBSERVATION_SIGMA = 1.0;
     private final ModelFitter fitter;
     private final LinearRegressionGraph<OUTPUT> modelGraph;
