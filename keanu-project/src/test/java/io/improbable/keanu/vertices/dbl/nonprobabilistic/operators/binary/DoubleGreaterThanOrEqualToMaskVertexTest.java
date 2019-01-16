4fc89ca8df56b165af0a8437f37b481f3f0531a0
Joanne Hong <joannehong@improbable.io>
2019-01-16 14:46:44 +0000

    add tests for mask operation vertex tests.

---
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/binary/DoubleGreaterThanOrEqualToMaskVertexTest.java
@@ -0,0 +1,30 @@
+package io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.binary;
+
+import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import org.junit.Test;
+
+import static io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.binary.BinaryOperationTestHelpers.operatesOnTwo2x2MatrixVertexValues;
+import static io.improbable.keanu.vertices.dbl.nonprobabilistic.operators.binary.BinaryOperationTestHelpers.operatesOnTwoScalarVertexValues;
+
+public class DoubleGreaterThanOrEqualToMaskVertexTest {
+
+    @Test
+    public void lessThanOrEqualToScalarVertexValues() {
+        operatesOnTwoScalarVertexValues(
+            2.0,
+            3.0,
+            0.0,
+            DoubleVertex::toGreaterThanOrEqualToMask
+        );
+    }
+
+    @Test
+    public void lessThanMatrixVertexValues() {
+        operatesOnTwo2x2MatrixVertexValues(
+            new double[]{1.0, 4.0, 3.0, -3.0},
+            new double[]{2.0, 2.0, 3.0, 3.0},
+            new double[]{0.0, 1.0, 1.0, 0.0},
+            DoubleVertex::toGreaterThanOrEqualToMask
+        );
+    }
+}
