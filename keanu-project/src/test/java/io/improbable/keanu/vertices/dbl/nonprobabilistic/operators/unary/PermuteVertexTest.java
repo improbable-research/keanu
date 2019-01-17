c4b5b5d56de128d6ac098c57c513d0c7969749d3
George Nash <georgenash@improbable.io>
2019-01-17 11:54:46 +0000

    rank 3 test

--- keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/PermuteVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/dbl/nonprobabilistic/operators/unary/PermuteVertexTest.java
@@ -6,9 +6,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 import io.improbable.keanu.tensor.dbl.DoubleTensor;
+import io.improbable.keanu.vertices.dbl.Differentiator;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.diff.PartialsOf;
 import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
 
 public class PermuteVertexTest {
@@ -18,15 +21,25 @@ public class PermuteVertexTest {
         DoubleVertex a = new UniformVertex(0, 10);
         a.setValue(DoubleTensor.create(new double[]{1, 2, 3, 4}, 2, 2));
 
-        PermuteVertex PermuteVertex = new PermuteVertex(a, 1, 0);
-        PermuteVertex.getValue();
+        PermuteVertex permute = new PermuteVertex(a, 1, 0);
+        permute.getValue();
 
-        Assert.assertArrayEquals(new long[]{2, 2}, PermuteVertex.getShape());
-        Assert.assertArrayEquals(a.getValue().transpose().asFlatDoubleArray(), PermuteVertex.getValue().asFlatDoubleArray(), 1e-6);
+        Assert.assertArrayEquals(new long[]{2, 2}, permute.getShape());
+        Assert.assertArrayEquals(a.getValue().transpose().asFlatDoubleArray(), permute.getValue().asFlatDoubleArray(), 1e-6);
     }
 
     @Test
-    public void canPermuteAutoDiff() {
+    public void canPermuteRankThreeVertex() {
+        UniformVertex a = new UniformVertex(0, 10);
+        a.setValue(DoubleTensor.arange(0, 8).reshape(2, 2, 2));
+
+        PermuteVertex permute = new PermuteVertex(a, 2, 0, 1);
+
+        DoubleTensor forwardWrtA = Differentiator.forwardModeAutoDiff(a, permute).of(permute);
+        DoubleTensor backwardWrtA = Differentiator.reverseModeAutoDiff(permute, a).withRespectTo(a);
+
+        System.out.println(forwardWrtA);
+        System.out.println(backwardWrtA);
 
     }
 
