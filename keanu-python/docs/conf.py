2a48da91a9698422951f7031f3510bdf82f1e0d2
Wesley Williams <wesley@improbable.io>
2019-01-17 16:02:37 +0000

    Version/copyright year bump

--- keanu-python/docs/conf.py
+++ keanu-python/docs/conf.py
@@ -19,13 +19,13 @@ sys.path.insert(0, u'../')
 # -- Project information -----------------------------------------------------
 
 project = u'keanu'
-copyright = u'2018, Improbable'
+copyright = u'2019, Improbable'
 author = u'Improbable'
 
 # The short X.Y version
-version = u'0.0.15'
+version = u'0.0.18'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.15'
+release = u'0.0.18'
 
 # -- General configuration ---------------------------------------------------
 
