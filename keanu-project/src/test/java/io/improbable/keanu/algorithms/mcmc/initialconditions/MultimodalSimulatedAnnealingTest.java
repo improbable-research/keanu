86599e3deaf24164926777e63ed88dc0a5ecda4b
Wesley Williams <wesley@improbable.io>
2019-01-17 13:24:00 +0000

    Spotless

--- keanu-project/src/test/java/io/improbable/keanu/algorithms/mcmc/initialconditions/MultimodalSimulatedAnnealingTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/algorithms/mcmc/initialconditions/MultimodalSimulatedAnnealingTest.java
@@ -8,7 +8,6 @@ import io.improbable.keanu.vertices.bool.BooleanVertex;
 import io.improbable.keanu.vertices.bool.probabilistic.BernoulliVertex;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.KeanuRandom;
-import io.improbable.keanu.vertices.dbl.nonprobabilistic.CastToDoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
 import io.improbable.keanu.vertices.generic.nonprobabilistic.If;
