f053030af9fae54933402e3ca1b33ecacdce5871
Joanne Hong <joannehong@improbable.io>
2019-01-22 18:18:42 +0000

    For VertexLabel equality, check against both unqualified name and qualified name.

--- keanu-python/tests/test_label.py
+++ keanu-python/tests/test_label.py
@@ -23,6 +23,7 @@ def test_inequality(label1: VertexLabel) -> None:
 
 def test_equality_str(label1: VertexLabel) -> None:
     assert label1 == "label1"
+    assert label1 == "outer.inner.label1"
 
 
 def test_inequality_str(label1: VertexLabel) -> None:
