15b0175b6b9e4c7504c6df9529258f3e1a6e2cc2
Michael Lewin <michaellewin@improbable.io>
2019-01-23 08:58:06 +0000

    Made If work with raw types as well as Vertex types

--- keanu-python/keanu/vertex/iff.py
+++ keanu-python/keanu/vertex/iff.py
@@ -1,10 +1,19 @@
 from keanu.infer_type import infer_type_and_execute, get_type_of_value
 from keanu.vartypes import tensor_arg_types
-from .base import Vertex
-from .generated import BooleanIf, DoubleIf, IntegerIf
+from keanu.vertex import do_inferred_vertex_cast
+from .base import Vertex, vertex_constructor_param_types
+from .generated import BooleanIf, DoubleIf, IntegerIf, ConstantBoolean, ConstantInteger, ConstantDouble
+
+
+def __cast_to_vertex(input: vertex_constructor_param_types) -> Vertex:
+    return do_inferred_vertex_cast({bool: ConstantBoolean, int: ConstantInteger, float: ConstantDouble}, input)
 
 
 def If(predicate: Vertex, thn: Vertex, els: Vertex) -> Vertex:
+    predicate = __cast_to_vertex(predicate)
+    thn = __cast_to_vertex(thn)
+    els = __cast_to_vertex(els)
+
     type_ = get_type_of_value(thn.get_value())
 
     if type_ == bool:
--- keanu-python/tests/test_if.py
+++ keanu-python/tests/test_if.py
@@ -4,32 +4,46 @@ import pandas as pd
 from keanu.vertex import If, Bernoulli, Gaussian, Const, Double, Poisson, Integer, Boolean
 
 
-@pytest.fixture
-def predicate():
-    return Bernoulli(0.5)
-
-@pytest.mark.parametrize("data", [1., np.array([1., 2.]), pd.Series([1., 2.])])
+@pytest.mark.parametrize("predicate", [
+    True, np.array([True, False]), pd.Series([True, False]),
+    Bernoulli(0.5), Const(np.array([True, False]))])
+@pytest.mark.parametrize("data", [
+    1., np.array([1., 2.]), pd.Series([1., 2.]),
+    Const(1.), Const(np.array([1., 2.]))
+])
 def test_you_can_create_a_double_valued_if(data, predicate) -> None:
-    thn = Gaussian(data, data)
-    els = Const(data)
+    thn = data
+    els = data
     result = If(predicate, thn, els)
     assert type(result) == Double
     assert result.unwrap().getClass().getSimpleName() == "DoubleIfVertex"
 
 
-@pytest.mark.parametrize("data", [1, np.array([1, 2]), pd.Series([1, 2])])
+@pytest.mark.parametrize("predicate", [
+    True, np.array([True, False]), pd.Series([True, False]),
+    Bernoulli(0.5), Const(np.array([True, False]))])
+@pytest.mark.parametrize("data", [
+    1, np.array([1, 2]), pd.Series([1, 2]),
+    Const(1), Const(np.array([1, 2]))
+])
 def test_you_can_create_an_integer_valued_if(data, predicate) -> None:
-    thn = Poisson(data)
-    els = Const(data)
+    thn = data
+    els = data
     result = If(predicate, thn, els)
     assert type(result) == Integer
     assert result.unwrap().getClass().getSimpleName() == "IntegerIfVertex"
 
 
-@pytest.mark.parametrize("data", [True, np.array([True, False]), pd.Series([True, False])])
+@pytest.mark.parametrize("predicate", [
+    True, np.array([True, False]), pd.Series([True, False]),
+    Bernoulli(0.5), Const(np.array([True, False]))])
+@pytest.mark.parametrize("data", [
+    True, np.array([True, False]), pd.Series([True, False]),
+    Const(True), Const(np.array([True, False]))
+])
 def test_you_can_create_a_boolean_valued_if(data, predicate) -> None:
-    thn = Const(data)
-    els = Const(data)
+    thn = data
+    els = data
     result = If(predicate, thn, els)
     assert type(result) == Boolean
     assert result.unwrap().getClass().getSimpleName() == "BooleanIfVertex"
