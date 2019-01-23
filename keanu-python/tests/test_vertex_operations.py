51bfb2fb39d13fd9c5abb194b07b8dd389e8bbe7
Wesley Williams <wesley@improbable.io>
2019-01-23 13:35:15 +0000

    Format

--- keanu-python/tests/test_vertex_operations.py
+++ keanu-python/tests/test_vertex_operations.py
@@ -250,12 +250,10 @@ def test_can_do_integer_division(lhs: Vertex, rhs: Union[Vertex, numpy_types, fl
     assert (result.get_value() == expected_result).all()
 
 
-@pytest.mark.parametrize("lhs, rhs, expected_result, vertex_type", [
-    (np.array([15, 10]), Const(np.array([2, 4])), np.array([7, 2]), Integer),
-    (15, Const(np.array([2, 4])), np.array([7, 3]), Integer),
-    (15., Const(2), 7, Double),
-    (np.array([15., 10.]), Const(2), np.array([7,5]), Double)
-])
+@pytest.mark.parametrize("lhs, rhs, expected_result, vertex_type",
+                         [(np.array([15, 10]), Const(np.array([2, 4])), np.array([7, 2]), Integer),
+                          (15, Const(np.array([2, 4])), np.array([7, 3]), Integer), (15., Const(2), 7, Double),
+                          (np.array([15., 10.]), Const(2), np.array([7, 5]), Double)])
 # yapf: enable
 def test_can_do_integer_division_with_vertex_on_rhs(lhs: Union[Vertex, numpy_types, float], rhs: Vertex,
                                                     expected_result: numpy_types, vertex_type: Any) -> None:
