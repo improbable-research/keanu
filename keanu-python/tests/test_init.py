25481d1a75d1de8920fabf3e0c5b76b77af3552d
Wesley Williams <wesley@improbable.io>
2019-01-29 10:05:33 +0000

    remove hyphen from regex

--- keanu-python/tests/test_init.py
+++ keanu-python/tests/test_init.py
@@ -7,5 +7,5 @@ def test_version_is_correctly_formatted() -> None:
     assert isinstance(version, str)
     # Example versions that pass: 0.0.18.dev1 0.0.18
     # Example versions that fail: 0.0.dev1 0.0
-    pattern = re.compile(r'^(\d+\.)(\d+\.)(\d+)(\.(\w|-)+)?$')
+    pattern = re.compile(r'^(\d+\.)(\d+\.)(\d+)(\.\w+)?$')
     assert pattern.match(version)
