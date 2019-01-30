bba90ca70b4d5d03f7e3487f5e2cf3fe12db577a
Joanne Hong <joannehong@improbable.io>
2019-01-30 18:14:52 +0000

    comment edit

--- codegen/src/main/java/io/improbable/keanu/codegen/python/vertex/JavaVertexToPythonConverter.java
+++ codegen/src/main/java/io/improbable/keanu/codegen/python/vertex/JavaVertexToPythonConverter.java
@@ -30,7 +30,7 @@ class JavaVertexToPythonConverter {
     /**
      * Extra parameters are added:
      * - as optional parameters to pythonTypedParameters, at the end
-     * - as required parameters to pythonParameters, before other parameters
+     * - as required parameters to pythonParameters, at the beginning
      */
     private static final List<PythonParam> EXTRA_PARAMS = ImmutableList.of(
         new PythonParam("label", String.class, "None")
