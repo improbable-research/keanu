2b37ec2ba478b126ddc8df3bb6b807994bfe6230
Joanne Hong <joannehong@improbable.io>
2019-02-01 16:20:46 +0000

    remove getter annotation for classes with lombok value annotation.

--- codegen/src/main/java/io/improbable/keanu/codegen/python/datamodel/Import.java
+++ codegen/src/main/java/io/improbable/keanu/codegen/python/datamodel/Import.java
@@ -1,10 +1,9 @@
 package io.improbable.keanu.codegen.python.datamodel;
 
-import lombok.Getter;
 import lombok.Value;
 
 @Value
 public class Import {
-    @Getter
+
     private String packageName;
 }
--- codegen/src/main/java/io/improbable/keanu/codegen/python/datamodel/VertexConstructor.java
+++ codegen/src/main/java/io/improbable/keanu/codegen/python/datamodel/VertexConstructor.java
@@ -1,22 +1,15 @@
 package io.improbable.keanu.codegen.python.datamodel;
 
-import lombok.Getter;
 import lombok.Value;
 
 @Value
 public class VertexConstructor {
 
-    @Getter
     private String javaClass;
-    @Getter
     private String pythonClass;
-    @Getter
     private String pythonVertexClass;
-    @Getter
     private String pythonTypedParameters;
-    @Getter
     private String pythonParameters;
-    @Getter
     private String docString;
 
 }
