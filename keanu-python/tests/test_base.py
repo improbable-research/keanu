d1e311ca79e916228b3dd4d11172dfc1ee3ef365
Joanne Hong <joannehong@improbable.io>
2019-02-05 12:02:41 +0000

    reorder tests

--- keanu-python/tests/test_base.py
+++ keanu-python/tests/test_base.py
@@ -26,14 +26,14 @@ def test_you_can_call_a_java_method_on_the_unwrapped_object(java_list_wrapper) -
     assert not java_list_wrapper.unwrap().isEmpty()
 
 
-def test_you_cannot_call_a_java_method_on_the_wrapped_object(java_list_wrapper) -> None:
-    with pytest.raises(AttributeError, match="{} has no attribute isEmpty".format(type(java_list_wrapper))):
-        java_list_wrapper.isEmpty()
-
-
 def test_you_cannot_call_a_java_method_with_snake_case_on_the_unwrapped_object(java_list_wrapper) -> None:
     with pytest.raises(Py4JError, match="Method is_empty\(\[\]\) does not exist"):
         java_list_wrapper.unwrap().is_empty()
+
+
+def test_you_cannot_call_a_java_method_with_snake_case_on_the_wrapped_object(java_list_wrapper) -> None:
+    with pytest.raises(AttributeError, match="{} has no attribute is_empty".format(type(java_list_wrapper))):
+        java_list_wrapper.is_empty()
 
 
 def test_you_can_overload_a_java_method_in_python(java_list_wrapper) -> None:
@@ -42,12 +42,7 @@ def test_you_can_overload_a_java_method_in_python(java_list_wrapper) -> None:
 
 def test_you_cannot_call_a_java_method_that_hasnt_been_overloaded(java_list_wrapper) -> None:
     with pytest.raises(AttributeError, match="{} has no attribute size".format(type(java_list_wrapper))):
-        java_list_wrapper.size()
-
-
-def test_throws_if_there_is_no_java_api(java_list_wrapper) -> None:
-    with pytest.raises(AttributeError, match="{} has no attribute missingMethod".format(type(java_list_wrapper))):
-        java_list_wrapper.missingMethod()
+        java_list_wrapper.isEmpty()
 
 
 def test_throws_if_not_unwrapped_and_passed_to_java_object(java_list_wrapper) -> None:
