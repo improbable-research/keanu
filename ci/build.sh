7ebdef6fe33024c67d2b597a140b80ff0fd6d4e2
George Nash <georgenash@improbable.io>
2019-02-05 16:51:30 +0000

    buildkite config

---
+++ ci/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# https://brevi.link/shell-style
+# https://explainshell.com
+
+if [[ ! `git status` ]]; then
+  echo 'Not in git repo, exiting'
+  exit 1
+fi
+
+repo_top_level_dir=`git rev-parse --show-toplevel`
+cd ${repo_top_level_dir}/java
+
+# clone dependencies
+dependency='applied-core'
+rm -rf ./var/${dependency}
+git clone "git@github.com:improbable/${dependency}.git" "./var/${dependency}"
+
+docker build -t gradle-image .
+exec docker run \
+    --rm \
+    gradle-image \
+    gradle -x test -s build
