2bf3da68578e71dd68499f9241c24b61a85cce22
Michael Lewin <michaellewin@improbable.io>
2019-02-06 12:54:41 +0000

    test_theres_a_factory_for_when_you_dont_know_if_the_string_is_namespaced

--- keanu-python/keanu/vertex/label.py
+++ keanu-python/keanu/vertex/label.py
@@ -10,6 +10,8 @@ java_import(k.jvm_view(), "io.improbable.keanu.vertices.VertexLabel")
 
 class _VertexLabel(JavaObjectWrapper):
 
+    __separator = "."
+
     def __init__(self, first: str, *remainder: str):
         if len(remainder) == 0:
             java_object = k.jvm_view().VertexLabel(first)
@@ -18,10 +20,35 @@ class _VertexLabel(JavaObjectWrapper):
         super(_VertexLabel, self).__init__(java_object)
 
     @staticmethod
+    def create_maybe_with_namespace(label: str) -> '_VertexLabel':
+        """
+        >>> l1 = _VertexLabel.create_maybe_with_namespace("foo")
+        >>> l1.get_unqualified_name()
+        'foo'
+        >>> l1.get_qualified_name()
+        'foo'
+        >>> l2 = _VertexLabel.create_maybe_with_namespace("outer.inner.foo")
+        >>> l2.get_unqualified_name()
+        'foo'
+        >>> l2.get_qualified_name()
+        'outer.inner.foo'
+        """
+        if _VertexLabel.__separator in label:
+            return _VertexLabel.create_with_namespace(label)
+        else:
+            return _VertexLabel(label)
+
+    @staticmethod
     def create_with_namespace(label: str) -> '_VertexLabel':
-        separator = "."
-        if separator not in label:
-            raise ValueError('No namespace separator "{}" found in {}'.format(separator, label))
-        name_array = label.split(separator)
+        """
+        >>> l = _VertexLabel.create_with_namespace("outer.inner.foo")
+        >>> l.get_unqualified_name()
+        'foo'
+        >>> l.get_qualified_name()
+        'outer.inner.foo'
+        """
+        if _VertexLabel.__separator not in label:
+            raise ValueError('No namespace separator "{}" found in {}'.format(_VertexLabel.__separator, label))
+        name_array = label.split(_VertexLabel.__separator)
         return _VertexLabel(name_array[0], *name_array[1:])
 
--- keanu-python/tests/test_labels.py
+++ keanu-python/tests/test_labels.py
@@ -21,6 +21,15 @@ def test_you_can_create_a_label_from_a_dot_separated_string() -> None:
     assert label.get_qualified_name() == "outer.inner.foo"
 
 
+def test_theres_a_factory_for_when_you_dont_know_if_the_string_is_namespaced() -> None:
+    label = _VertexLabel.create_maybe_with_namespace("outer.inner.foo")
+    assert label.get_unqualified_name() == "foo"
+    assert label.get_qualified_name() == "outer.inner.foo"
+    label = _VertexLabel.create_maybe_with_namespace("foo")
+    assert label.get_unqualified_name() == "foo"
+    assert label.get_qualified_name() == "foo"
+
+
 def test_it_throws_if_you_use_the_wrong_separator() -> None:
     with pytest.raises(ValueError, match='No namespace separator "." found in outer/inner/foo'):
         _VertexLabel.create_with_namespace("outer/inner/foo")
