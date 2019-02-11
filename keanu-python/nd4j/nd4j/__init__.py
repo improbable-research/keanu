c3d93829d8e78a0b79b813456c8474c8197628f5
Wesley Williams <wesley@improbable.io>
2019-02-11 12:13:48 +0000

    Build populates nd4j classpath

--- keanu-python/nd4j/nd4j/__init__.py
+++ keanu-python/nd4j/nd4j/__init__.py
@@ -1,5 +1,6 @@
 from pkg_resources import resource_filename
 
+
 def get_classpath():
-  classpath = resource_filename(__name__, 'classpath')
-  return classpath
+    classpath = resource_filename(__name__, 'classpath')
+    return classpath
