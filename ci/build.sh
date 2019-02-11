5a683781ba01662e0dea3993562e548a8b3fe494
George Nash <georgenash@improbable.io>
2019-02-11 15:27:56 +0000

    rename and fix git ignore issue

--- ci/build.sh
+++ ci/build.sh
@@ -10,7 +10,7 @@ fi
 repo_top_level_dir=`git rev-parse --show-toplevel`
 cd ${repo_top_level_dir}/java
 
-docker build -t build-image -f docker/build/Dockerfile .
+docker build -t build-image -f docker/builder/Dockerfile .
 exec docker run \
     --rm \
     build-image \
