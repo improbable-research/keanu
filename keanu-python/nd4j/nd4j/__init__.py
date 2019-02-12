1cf2ac1564bf66e98bd81ecf909e8a4f15609152
Wesley Williams <wesley@improbable.io>
2019-02-12 11:22:07 +0000

    Add __verison__ attribute

--- keanu-python/nd4j/nd4j/__init__.py
+++ keanu-python/nd4j/nd4j/__init__.py
@@ -1,5 +1,7 @@
 from pkg_resources import resource_filename
 
+from .__version__ import __version__
+
 
 def get_classpath():
     classpath = resource_filename(__name__, 'classpath')
--- keanu-python/nd4j/setup.py
+++ keanu-python/nd4j/setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, 'nd4j', '__version__.py'), 'r', encoding='utf-8') as f:
+    exec (f.read(), about)
 
 setup(
     name='nd4j',
     description='A package of the Nd4j jars for use by packages that use Py4j.',
-    version='1.0.0-beta3',
+    version=about['__version__'],
     author='Improbable Worlds',
     author_email='keanu-engineering@improbable.io',
     url='https://improbable-research.github.io/keanu/python',
