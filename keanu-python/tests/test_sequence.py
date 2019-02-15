3e5b843c047878962e972c67664768e1e7198231
Michael Lewin <michaellewin@improbable.io>
2019-02-15 10:39:29 +0000

    renamed functions and variables in test_sequence

--- keanu-python/tests/test_sequence.py
+++ keanu-python/tests/test_sequence.py
@@ -6,44 +6,44 @@ from keanu.sequence import Sequence, SequenceItem
 from keanu.vertex import Bernoulli, DoubleProxy, Exponential, Poisson, Const, KeanuContext
 
 
-def test_you_can_iterate_over_the_plates() -> None:
-    num_plates = 100
-    plates = Sequence(count=num_plates, factory=lambda p: None)
-    plate_count = sum(1 for _ in plates)
-    assert plate_count == num_plates
+def test_you_can_iterate_over_the_sequence() -> None:
+    num_items = 100
+    sequence = Sequence(count=num_items, factory=lambda p: None)
+    item_count = sum(1 for _ in sequence)
+    assert item_count == num_items
 
 
-def test_you_can_build_plates_with_fixed_count() -> None:
-    num_plates = 100
+def test_you_can_build_a_sequence_with_fixed_count() -> None:
+    num_items = 100
     vertexLabel = "foo"
 
-    def create_vertex(plate: SequenceItem) -> None:
+    def create_vertex(item: SequenceItem) -> None:
         v = Bernoulli(0.5)
         v.set_label(vertexLabel)
-        plate.add(v)
+        item.add(v)
 
-    plates = Sequence(count=num_plates, factory=create_vertex)
-    assert plates.size() == num_plates
+    sequence = Sequence(count=num_items, factory=create_vertex)
+    assert sequence.size() == num_items
 
-    for plate in plates:
-        assert plate.get(vertexLabel) is not None
+    for item in sequence:
+        assert item.get(vertexLabel) is not None
 
 
-def test_you_can_build_plates_from_data() -> None:
-    num_plates = 10
+def test_you_can_build_a_sequence_from_data() -> None:
+    num_items = 10
 
-    data_generator = ({"x": i, "y": -i} for i in range(num_plates))
+    data_generator = ({"x": i, "y": -i} for i in range(num_items))
 
-    def create_vertices(plate: SequenceItem, point: Dict[str, Any]) -> None:
-        plate.add(Const(point["x"], label="x"))
-        plate.add(Const(point["y"], label="y"))
+    def create_vertices(item: SequenceItem, point: Dict[str, Any]) -> None:
+        item.add(Const(point["x"], label="x"))
+        item.add(Const(point["y"], label="y"))
 
-    plates = Sequence(data_generator=data_generator, factory=create_vertices)
-    assert plates.size() == num_plates
+    sequence = Sequence(data_generator=data_generator, factory=create_vertices)
+    assert sequence.size() == num_items
 
-    for i, plate in enumerate(plates):
-        assert plate.get("x").get_value() == i
-        assert plate.get("y").get_value() == -i
+    for index, item in enumerate(sequence):
+        assert item.get("x").get_value() == index
+        assert item.get("y").get_value() == -index
 
 
 def test_you_must_pass_count_or_data_generator() -> None:
@@ -72,29 +72,29 @@ def test_you_can_build_a_time_series() -> None:
     y_label = "y"
     x_previous_label = Sequence.proxy_for(x_label)
 
-    num_plates = 10
+    num_items = 10
     initial_x = 1.
 
-    def create_time_step(plate):
+    def create_time_step(sequence_item):
         x_previous = DoubleProxy((), x_previous_label)
         x = Exponential(x_previous)
         y = Poisson(x)
-        plate.add(x_previous)
-        plate.add(x, label=x_label)
-        plate.add(y, label=y_label)
+        sequence_item.add(x_previous)
+        sequence_item.add(x, label=x_label)
+        sequence_item.add(y, label=y_label)
 
-    plates = Sequence(initial_state={x_label: initial_x}, count=num_plates, factory=create_time_step)
-    assert plates.size() == num_plates
+    sequence = Sequence(initial_state={x_label: initial_x}, count=num_items, factory=create_time_step)
+    assert sequence.size() == num_items
 
-    x_from_previous_plate = None
-    for plate in plates:
-        x_previous_proxy = plate.get(x_previous_label)
-        x = plate.get(x_label)
-        y = plate.get(y_label)
-        if x_from_previous_plate is None:
+    x_from_previous_step = None
+    for item in sequence:
+        x_previous_proxy = item.get(x_previous_label)
+        x = item.get(x_label)
+        y = item.get(y_label)
+        if x_from_previous_step is None:
             assert [p.get_value() for p in x_previous_proxy.get_parents()] == [initial_x]
         else:
-            assert [p.get_id() for p in x_previous_proxy.get_parents()] == [x_from_previous_plate.get_id()]
+            assert [p.get_id() for p in x_previous_proxy.get_parents()] == [x_from_previous_step.get_id()]
         assert [p.get_id() for p in x.get_parents()] == [x_previous_proxy.get_id()]
         assert [p.get_id() for p in y.get_parents()] == [x.get_id()]
-        x_from_previous_plate = x
+        x_from_previous_step = x
