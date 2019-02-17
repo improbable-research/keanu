1c37feb1a12108c64e1a75ac648dd7a870a33e67
Wesley Williams <wesley@improbable.io>
2019-02-17 20:26:55 +0000

    bump keanu-python versions

--- docs/bin/up.sh
+++ docs/bin/up.sh
@@ -2,13 +2,18 @@
 set -e -u -o pipefail
 CURRENT_VERSION=0.0.21
 NEXT_VERSION=0.0.22
+DEV_NAME='dev1'
  if [[ -n "${DEBUG-}" ]]; then
   set -x
 fi
 
 cd "$(dirname "$0")/../"
 python3 bin/freezeAtVersion.py --version ${CURRENT_VERSION}
-sed -ri 's/^(\s*)(current_version\s*:\s*'"${CURRENT_VERSION}"'\s*$)/\1current_version: '"${NEXT_VERSION}"'/' _config.yml
+sed -i 's/\(current_version:\).*/\1 '"${NEXT_VERSION}"'/' _config.yml
 sed -i '1i- title: '"${CURRENT_VERSION}" _data/previous_versions.yml
 UNDER=`echo $CURRENT_VERSION | tr . _`
 sed -i '2i\ \ url: /docs/'"${UNDER}" _data/previous_versions.yml
+
+sed -i 's/'"\(version = u\).*/\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
+sed -i 's/'"\(release = u\).*/\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
+sed -i "s/\(__version__ = '\).*/\1${NEXT_VERSION}.${DEV_NAME}'/" ../keanu-python/keanu/__version__.py
