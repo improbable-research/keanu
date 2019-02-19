5a3a82dbfa06dcaa14ea4e9670f2fd05815f6597
Wesley Williams <wesley@improbable.io>
2019-02-19 11:44:11 +0000

    fix shellcheck issues

--- docs/bin/up.sh
+++ docs/bin/up.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 set -e -u -o pipefail
 NEXT_VERSION=0.0.22
 DEV_NAME='dev1'
@@ -21,16 +20,16 @@ python3 bin/freezeAtVersion.py --version "${CURRENT_VERSION}"
 sed -i 's/\(current_version:\).*/\1 '"${NEXT_VERSION}"'/' _config.yml
 
 # Update previous_versions.yml
-UNDERSCORED_CURRENT_VERSION=$(echo $CURRENT_VERSION | tr . _)
+UNDERSCORED_CURRENT_VERSION=$(echo "${CURRENT_VERSION}" | tr . _)
 sed -i '1i- title: '"${CURRENT_VERSION}" _data/previous_versions.yml
 sed -i '2i\ \ url: /docs/'"${UNDERSCORED_CURRENT_VERSION}" _data/previous_versions.yml
 
 # Update conf.py version
-sed -i 's/'"\(version = u\).*/\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
-sed -i 's/'"\(release = u\).*/\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
+sed -i 's/'"\\(version = u\\).*/\\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
+sed -i 's/'"\\(release = u\\).*/\\1'${NEXT_VERSION}'/" ../keanu-python/docs/conf.py
 
 # Update Python __version__
-sed -i "s/\(__version__ = '\).*/\1${NEXT_VERSION}.${DEV_NAME}'/" ../keanu-python/keanu/__version__.py
+sed -i "s/\\(__version__ = '\\).*/\\1${NEXT_VERSION}.${DEV_NAME}'/" ../keanu-python/keanu/__version__.py
 
 # Add new version number to release notes
-sed -i "1i # Version ${NEXT_VERSION}\n" ../release_notes.md
+sed -i "1i # Version ${NEXT_VERSION}\\n" ../release_notes.md
