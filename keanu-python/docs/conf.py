021fea9755fa4f5addd2bc82dcc1330b58a3cbf0
Joanne Hong <joannehong@improbable.io>
2019-02-25 12:33:25 +0000

    bump docs to 21.

--- keanu-python/docs/conf.py
+++ keanu-python/docs/conf.py
@@ -23,9 +23,9 @@ copyright = u'2018, Improbable'
 author = u'Improbable'
 
 # The short X.Y version
-version = u'0.0.21'
+version = u'0.0.22'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.21'
+release = u'0.0.22'
 
 # -- General configuration ---------------------------------------------------
 
