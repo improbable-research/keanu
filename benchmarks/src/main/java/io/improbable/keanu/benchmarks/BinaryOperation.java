6bb045e4e79e209a5bd4e31f3fc8818cc1a239aa
Caleb Walker <caleb@improbable.io>
2019-03-01 10:31:20 +0000

    remove the primitive op in binary operation perf tests

--- benchmarks/src/main/java/io/improbable/keanu/benchmarks/BinaryOperation.java
+++ benchmarks/src/main/java/io/improbable/keanu/benchmarks/BinaryOperation.java
@@ -7,43 +7,27 @@ public enum BinaryOperation {
         public DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs) {
             return lhs.plus(rhs);
         }
-        public double apply(double lhs, double rhs) {
-            return lhs + rhs;
-        }
     },
     MINUS {
         public DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs) {
             return lhs.minus(rhs);
-        }
-        public double apply(double lhs, double rhs) {
-            return lhs - rhs;
         }
     },
     TIMES {
         public DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs) {
             return lhs.times(rhs);
         }
-        public double apply(double lhs, double rhs) {
-            return lhs * rhs;
-        }
     },
     DIVIDE {
         public DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs) {
             return lhs.div(rhs);
         }
-        public double apply(double lhs, double rhs) {
-            return lhs / rhs;
-        }
     },
-    MATRIX_MULTIPLY{
+    MATRIX_MULTIPLY {
         public DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs) {
             return lhs.matrixMultiply(rhs);
-        }
-        public double apply(double lhs, double rhs) {
-            return lhs / rhs;
         }
     };
 
     public abstract DoubleTensor apply(DoubleTensor lhs, DoubleTensor rhs);
-    public abstract double apply(double lhs, double rhs);
 }
