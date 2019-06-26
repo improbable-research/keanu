30db284e197ab062d0d1be0e0be0335b290e8476
Caleb Walker <caleb@improbable.io>
2019-06-26 17:01:50 +0100

    move argCompare up to JVMTensor class because it doesn't have to only apply to numbers

--- keanu-project/src/main/java/io/improbable/keanu/tensor/JVMNumberTensor.java
+++ keanu-project/src/main/java/io/improbable/keanu/tensor/JVMNumberTensor.java
@@ -14,41 +14,4 @@ public abstract class JVMNumberTensor<T extends Number, TENSOR extends NumberTen
         super(buffer, shape, stride);
     }
 
-    public IntegerTensor argCompare(BiFunction<T, T, Boolean> compareOp, int axis) {
-        return argCompare(getFactory(), buffer, compareOp, shape, stride, axis);
-    }
-
-    public static <T extends Number, B extends JVMBuffer.PrimitiveArrayWrapper<T, B>>
-    IntegerTensor argCompare(JVMBuffer.ArrayWrapperFactory<T, B> factory,
-                             B buffer,
-                             BiFunction<T, T, Boolean> compareOp,
-                             long[] shape, long[] stride, int axis) {
-
-        if (axis >= shape.length) {
-            throw new IllegalArgumentException("Cannot take arg max of axis " + axis + " on a " + shape.length + " rank tensor.");
-        }
-
-        int[] rearrange = getPermutationForDimensionToDimensionZero(axis, shape);
-
-        B permutedBuffer = JVMTensor.permute(factory, buffer, shape, stride, rearrange).outputBuffer;
-
-        int dimLength = (int) (buffer.getLength() / shape[axis]);
-
-        B maxBuffer = factory.createNew(dimLength);
-        int[] maxIndex = new int[dimLength];
-
-        for (int i = 0; i < permutedBuffer.getLength(); i++) {
-
-            final int bufferIndex = i % dimLength;
-            final T value = permutedBuffer.get(i);
-
-            if (compareOp.apply(value, maxBuffer.get(bufferIndex))) {
-                maxBuffer.set(value, bufferIndex);
-                maxIndex[bufferIndex] = i / dimLength;
-            }
-
-        }
-
-        return IntegerTensor.create(maxIndex, ArrayUtils.remove(shape, axis));
-    }
 }
--- keanu-project/src/main/java/io/improbable/keanu/tensor/JVMTensor.java
+++ keanu-project/src/main/java/io/improbable/keanu/tensor/JVMTensor.java
@@ -3,6 +3,7 @@ package io.improbable.keanu.tensor;
 import com.google.common.base.Preconditions;
 import com.google.common.primitives.Ints;
 import io.improbable.keanu.tensor.buffer.JVMBuffer;
+import io.improbable.keanu.tensor.intgr.IntegerTensor;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.ArrayList;
@@ -280,6 +281,44 @@ public abstract class JVMTensor<T, TENSOR extends Tensor<T, TENSOR>, B extends J
         JVMTensorBroadcast.broadcast(buffer, shape, stride, outputBuffer, outputStride);
 
         return create(outputBuffer, toShape, outputStride);
+    }
+
+    public IntegerTensor argCompare(BiFunction<T, T, Boolean> compareOp, int axis) {
+        return argCompare(getFactory(), buffer, compareOp, shape, stride, axis);
+    }
+
+    public static <T, B extends JVMBuffer.PrimitiveArrayWrapper<T, B>>
+    IntegerTensor argCompare(JVMBuffer.ArrayWrapperFactory<T, B> factory,
+                             B buffer,
+                             BiFunction<T, T, Boolean> compareOp,
+                             long[] shape, long[] stride, int axis) {
+
+        if (axis >= shape.length) {
+            throw new IllegalArgumentException("Cannot take arg max of axis " + axis + " on a " + shape.length + " rank tensor.");
+        }
+
+        int[] rearrange = getPermutationForDimensionToDimensionZero(axis, shape);
+
+        B permutedBuffer = JVMTensor.permute(factory, buffer, shape, stride, rearrange).outputBuffer;
+
+        int dimLength = (int) (buffer.getLength() / shape[axis]);
+
+        B maxBuffer = factory.createNew(dimLength);
+        int[] maxIndex = new int[dimLength];
+
+        for (int i = 0; i < permutedBuffer.getLength(); i++) {
+
+            final int bufferIndex = i % dimLength;
+            final T value = permutedBuffer.get(i);
+
+            if (compareOp.apply(value, maxBuffer.get(bufferIndex))) {
+                maxBuffer.set(value, bufferIndex);
+                maxIndex[bufferIndex] = i / dimLength;
+            }
+
+        }
+
+        return IntegerTensor.create(maxIndex, ArrayUtils.remove(shape, axis));
     }
 
     private TENSOR createFromResultWrapper(ResultWrapper<T, B> wrapper) {
