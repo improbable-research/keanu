edfbe9d1e4e60352fbf5096ffd2b9f3a0c5c6fd5
Caleb Walker <caleb@improbable.io>
2019-07-01 16:17:44 +0100

    remove commented nd4j dbl tensor factory

--- keanu-project/src/main/java/io/improbable/keanu/tensor/TensorFactories.java
+++ keanu-project/src/main/java/io/improbable/keanu/tensor/TensorFactories.java
@@ -6,6 +6,4 @@ import io.improbable.keanu.tensor.dbl.JVMDoubleTensorFactory;
 public class TensorFactories {
 
     public static DoubleTensorFactory doubleTensorFactory = new JVMDoubleTensorFactory();
-//    public static DoubleTensorFactory doubleTensorFactory = new Nd4jDoubleTensorFactory();
-
 }
