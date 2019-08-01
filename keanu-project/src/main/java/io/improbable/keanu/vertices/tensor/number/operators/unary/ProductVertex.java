43588dfabb4826f0b4911b38832fd4ca31668d48
Caleb Walker <caleb@improbable.io>
2019-08-01 16:55:08 +0100

    add forward and reverse mode autodiff for product vertex

--- keanu-project/src/main/java/io/improbable/keanu/vertices/tensor/number/operators/unary/ProductVertex.java
+++ keanu-project/src/main/java/io/improbable/keanu/vertices/tensor/number/operators/unary/ProductVertex.java
@@ -2,15 +2,25 @@ package io.improbable.keanu.vertices.tensor.number.operators.unary;
 
 import io.improbable.keanu.annotation.ExportVertexToPythonBindings;
 import io.improbable.keanu.tensor.NumberTensor;
+import io.improbable.keanu.tensor.TensorShape;
+import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.LoadVertexParam;
 import io.improbable.keanu.vertices.NonProbabilisticVertex;
 import io.improbable.keanu.vertices.SaveVertexParam;
+import io.improbable.keanu.vertices.Vertex;
+import io.improbable.keanu.vertices.dbl.Differentiable;
+import io.improbable.keanu.vertices.dbl.nonprobabilistic.diff.PartialDerivative;
 import io.improbable.keanu.vertices.tensor.TensorVertex;
 import io.improbable.keanu.vertices.tensor.UnaryTensorOpVertex;
 import io.improbable.keanu.vertices.tensor.number.NumberTensorVertex;
 
+import java.util.Collections;
+import java.util.Map;
+
+import static io.improbable.keanu.tensor.TensorShape.getReductionResultShape;
+
 public class ProductVertex<T extends Number, TENSOR extends NumberTensor<T, TENSOR>, VERTEX extends NumberTensorVertex<T, TENSOR, VERTEX>>
-    extends UnaryTensorOpVertex<T, TENSOR, VERTEX> implements NonProbabilisticVertex<TENSOR, VERTEX> {
+    extends UnaryTensorOpVertex<T, TENSOR, VERTEX> implements NonProbabilisticVertex<TENSOR, VERTEX>, Differentiable {
 
     private final static String OVER_DIMENSIONS = "overDimensions";
     private final int[] overDimensions;
@@ -18,7 +28,7 @@ public class ProductVertex<T extends Number, TENSOR extends NumberTensor<T, TENS
     @ExportVertexToPythonBindings
     public ProductVertex(@LoadVertexParam(INPUT_NAME) TensorVertex<T, TENSOR, VERTEX> inputVertex,
                          @LoadVertexParam(OVER_DIMENSIONS) int[] overDimensions) {
-        super(inputVertex, inputVertex.ofType());
+        super(getReductionResultShape(inputVertex.getShape(), overDimensions), inputVertex, inputVertex.ofType());
         this.overDimensions = overDimensions;
     }
 
@@ -29,11 +39,47 @@ public class ProductVertex<T extends Number, TENSOR extends NumberTensor<T, TENS
 
     @Override
     protected TENSOR op(TENSOR value) {
-        if (overDimensions != null) {
+        if (overDimensions == null) {
             return value.product();
         } else {
             return value.product(overDimensions);
         }
+    }
+
+    @Override
+    public PartialDerivative forwardModeAutoDifferentiation(Map<Vertex, PartialDerivative> derivativeOfParentsWithRespectToInput) {
+
+        final PartialDerivative partial = derivativeOfParentsWithRespectToInput.get(inputVertex);
+
+        final long[] ofShapeWithoutRankLoss = TensorShape.getReductionResultShapeWithoutRankLoss(inputVertex.getShape(), overDimensions);
+        int[] sumOver = overDimensions == null ? TensorShape.dimensionRange(0, inputVertex.getRank()) : overDimensions;
+
+        final DoubleTensor result = partial
+            .multiplyAlongOfDimensions(getValue().toDouble().reshape(ofShapeWithoutRankLoss))
+            .divideByAlongOfDimensions(inputVertex.getValue().toDouble())
+            .get().sum(sumOver);
+
+        return new PartialDerivative(result);
+    }
+
+    @Override
+    public Map<Vertex, PartialDerivative> reverseModeAutoDifferentiation(PartialDerivative partial) {
+
+        final long[] wrtShapeWithoutRankLoss = TensorShape.getReductionResultShapeWithoutRankLoss(inputVertex.getShape(), overDimensions);
+        final long[] ofShape = partial.getOfShape(this.getShape());
+
+        final long[] newPartialShape = TensorShape.concat(
+            ofShape,
+            wrtShapeWithoutRankLoss
+        );
+
+        final DoubleTensor partialDueToShapeChange = partial.get().reshape(newPartialShape);
+
+        final DoubleTensor result = partialDueToShapeChange
+            .times(getValue().toDouble().reshape(wrtShapeWithoutRankLoss))
+            .div(inputVertex.getValue().toDouble());
+
+        return Collections.singletonMap(inputVertex, new PartialDerivative(result));
     }
 
     @SaveVertexParam(OVER_DIMENSIONS)
--- keanu-project/src/test/java/io/improbable/keanu/vertices/tensor/number/operators/unary/ProductVertexTest.java
+++ keanu-project/src/test/java/io/improbable/keanu/vertices/tensor/number/operators/unary/ProductVertexTest.java
@@ -5,7 +5,6 @@ import io.improbable.keanu.tensor.dbl.DoubleTensor;
 import io.improbable.keanu.vertices.dbl.DoubleVertex;
 import io.improbable.keanu.vertices.dbl.nonprobabilistic.ConstantDoubleVertex;
 import io.improbable.keanu.vertices.dbl.probabilistic.UniformVertex;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static io.improbable.keanu.vertices.tensor.number.TensorTestOperations.finiteDifferenceMatchesForwardAndReverseModeGradient;
@@ -13,7 +12,6 @@ import static io.improbable.keanu.vertices.tensor.number.TensorTestOperations.fi
 public class ProductVertexTest {
 
     @Test
-    @Ignore
     public void changesMatchGradientProductAll() {
         UniformVertex inputA = new UniformVertex(new long[]{3}, -10.0, 10.0);
         DoubleVertex product = inputA.product();
@@ -28,12 +26,25 @@ public class ProductVertexTest {
     }
 
     @Test
-    @Ignore
     public void changesMatchGradientProductDim1() {
-        UniformVertex inputA = new UniformVertex(new long[]{3, 3}, -10.0, 10.0);
+        UniformVertex inputA = new UniformVertex(new long[]{2, 3}, -10.0, 10.0);
         DoubleVertex product = inputA.product(1);
         DoubleVertex outputVertex = product.times(
             new ConstantDoubleVertex(DoubleTensor.arange(2))
+        );
+
+        final double INCREMENT = 10;
+        final double DELTA = 1e-10;
+
+        finiteDifferenceMatchesForwardAndReverseModeGradient(ImmutableList.of(inputA), outputVertex, INCREMENT, DELTA);
+    }
+
+    @Test
+    public void changesMatchGradientProductDim0() {
+        UniformVertex inputA = new UniformVertex(new long[]{2, 3}, -10.0, 10.0);
+        DoubleVertex product = inputA.product(0);
+        DoubleVertex outputVertex = product.times(
+            new ConstantDoubleVertex(DoubleTensor.arange(3))
         );
 
         final double INCREMENT = 10;
